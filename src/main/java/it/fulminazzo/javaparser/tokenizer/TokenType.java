f2a8e4b920c29c0f5b19f5f89da9d4ab327f6b88
Fulminazzo <fulminazzo21@gmail.com>
2024-12-13 19:44:24 +0100

    Created TokenType enum class

---
+++ src/main/java/it/fulminazzo/javaparser/tokenizer/TokenType.java
@@ -0,0 +1,40 @@
+package it.fulminazzo.javaparser.tokenizer;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Contains all the tokens utilized by {@link it.fulminazzo.javaparser.parser.JavaParser}.
+ */
+public enum TokenType {
+    ;
+
+    private final @NotNull String regex;
+
+    TokenType(final @NotNull String regex) {
+        this.regex = regex;
+    }
+
+    /**
+     * Verifies if the given token matches with the whole regex.
+     *
+     * @param token the token
+     * @return true if it matches
+     */
+    public boolean matches(final @NotNull String token) {
+        return token.matches(this.regex);
+    }
+
+    /**
+     * Gets the corresponding {@link TokenType} from the given token.
+     * If none is found, an {@link IllegalArgumentException} is thrown.
+     *
+     * @param token the token
+     * @return the token type
+     */
+    public static @NotNull TokenType fromString(final @NotNull String token) {
+        for (final TokenType tokenType : TokenType.values())
+            if (tokenType.matches(token)) return tokenType;
+        throw new IllegalArgumentException("Unknown token type: " + token);
+    }
+
+}
