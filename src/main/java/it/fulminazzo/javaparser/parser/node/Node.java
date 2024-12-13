3c0756954349f4e49cb805df90e6ea9a7759ffa5
Fulminazzo <fulminazzo21@gmail.com>
2024-12-13 22:28:13 +0100

    Implemented hashCode method

--- src/main/java/it/fulminazzo/javaparser/parser/node/Node.java
+++ src/main/java/it/fulminazzo/javaparser/parser/node/Node.java
@@ -1,8 +1,22 @@
 package it.fulminazzo.javaparser.parser.node;
 
+import it.fulminazzo.fulmicollection.objects.Refl;
+
+import java.util.Objects;
+
 /**
  * Represents a general node of the parser.
  */
 public abstract class Node {
+
+    @Override
+    public int hashCode() {
+        Refl<?> refl = new Refl<>(this);
+        return refl.getNonStaticFields().stream()
+                .map(refl::getFieldObject)
+                .filter(Objects::nonNull)
+                .mapToInt(Object::hashCode)
+                .sum();
+    }
 
 }
