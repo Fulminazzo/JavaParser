0cb8619f0038f0f3eaef0138da31668d99018323
Fulminazzo <fulminazzo21@gmail.com>
2024-12-13 22:34:37 +0100

    Implemented equals method

--- src/main/java/it/fulminazzo/javaparser/parser/node/Node.java
+++ src/main/java/it/fulminazzo/javaparser/parser/node/Node.java
@@ -1,7 +1,9 @@
 package it.fulminazzo.javaparser.parser.node;
 
 import it.fulminazzo.fulmicollection.objects.Refl;
+import org.jetbrains.annotations.Nullable;
 
+import java.lang.reflect.Field;
 import java.util.Objects;
 
 /**
@@ -17,6 +19,19 @@ public abstract class Node {
                 .filter(Objects::nonNull)
                 .mapToInt(Object::hashCode)
                 .sum();
+    }
+
+    @Override
+    public boolean equals(final @Nullable Object o) {
+        if (o != null && getClass() == o.getClass()) {
+            Refl<?> refl = new Refl<>(this);
+            Refl<?> other = new Refl<>(o);
+            for (final Field field : refl.getNonStaticFields())
+                if (!Objects.equals(refl.getFieldObject(field), other.getFieldObject(field)))
+                    return false;
+            return true;
+        }
+        return false;
     }
 
 }
