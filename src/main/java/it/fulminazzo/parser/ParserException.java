cc7b817740dc53d05d3bb45932e732906589e0da
Fulminazzo <fulminazzo21@gmail.com>
2024-12-13 21:59:00 +0100

    Created ParserException

---
+++ src/main/java/it/fulminazzo/parser/ParserException.java
@@ -0,0 +1,19 @@
+package it.fulminazzo.parser;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Exception used to notify about errors coming from {@link JavaParser}.
+ */
+public class ParserException extends RuntimeException {
+
+    /**
+     * Instantiates a new Parser exception.
+     *
+     * @param message the message of the exception
+     */
+    public ParserException(final @NotNull String message) {
+        super(message);
+    }
+
+}
