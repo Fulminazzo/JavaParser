156b4c6d03544cd56826f436b81d9c9d08540d16
Fulminazzo <fulminazzo21@gmail.com>
2024-12-14 22:46:06 +0100

    Created IfStatement class

---
+++ src/main/java/it/fulminazzo/javaparser/parser/node/statements/IfStatement.java
@@ -0,0 +1,30 @@
+package it.fulminazzo.javaparser.parser.node.statements;
+
+import it.fulminazzo.javaparser.parser.node.Node;
+import it.fulminazzo.javaparser.parser.node.container.CodeBlock;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Represents an if statement.
+ */
+public class IfStatement extends Statement {
+    private final @NotNull CodeBlock code;
+    private final @Nullable Node thenBranch;
+
+    /**
+     * Instantiates a new If statement.
+     *
+     * @param condition  the condition
+     * @param code       the code that will be executed
+     * @param thenBranch the alternative code
+     */
+    public IfStatement(final @NotNull Node condition,
+                       final @NotNull CodeBlock code,
+                       final @Nullable Node thenBranch) {
+        super(condition);
+        this.code = code;
+        this.thenBranch = thenBranch;
+    }
+
+}
