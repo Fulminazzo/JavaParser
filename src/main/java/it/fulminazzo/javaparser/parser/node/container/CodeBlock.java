08b958b6e0ac3cdcab53707b513e24b60b590bfb
Fulminazzo <fulminazzo21@gmail.com>
2024-12-14 22:38:08 +0100

    Added arrays constructor

--- src/main/java/it/fulminazzo/javaparser/parser/node/container/CodeBlock.java
+++ src/main/java/it/fulminazzo/javaparser/parser/node/container/CodeBlock.java
@@ -3,12 +3,17 @@ package it.fulminazzo.javaparser.parser.node.container;
 import it.fulminazzo.javaparser.parser.node.statements.Statement;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
 /**
  * Represents a general code block contained in braces.
  */
 public class CodeBlock extends StatementContainer {
+
+    public CodeBlock(final Statement @NotNull ... statements) {
+        super(new LinkedList<>(Arrays.asList(statements)));
+    }
 
     /**
      * Instantiates a new Code block.
