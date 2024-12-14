0d17a8ad20fac9e21e17323a6abc241b8f080281
Fulminazzo <fulminazzo21@gmail.com>
2024-12-14 15:23:02 +0100

    Created MethodCall class

---
+++ src/main/java/it/fulminazzo/javaparser/parser/node/MethodCall.java
@@ -0,0 +1,25 @@
+package it.fulminazzo.javaparser.parser.node;
+
+import it.fulminazzo.javaparser.parser.node.types.Literal;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Represents a method call invocation.
+ */
+public class MethodCall extends Node {
+    private final @NotNull Literal executor;
+    private final @NotNull MethodInvocation invocation;
+
+    /**
+     * Instantiates a new Method call.
+     *
+     * @param executor   the executor
+     * @param invocation the invocation
+     */
+    public MethodCall(final @NotNull Literal executor,
+                      final @NotNull MethodInvocation invocation) {
+        this.executor = executor;
+        this.invocation = invocation;
+    }
+
+}
