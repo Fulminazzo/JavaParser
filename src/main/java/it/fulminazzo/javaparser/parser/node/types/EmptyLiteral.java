7b7df0b2987bce98dce6c74c6091b1b9599f5346
Fulminazzo <fulminazzo21@gmail.com>
2024-12-14 22:26:22 +0100

    Created EmptyLiteral class

---
+++ src/main/java/it/fulminazzo/javaparser/parser/node/types/EmptyLiteral.java
@@ -0,0 +1,9 @@
+package it.fulminazzo.javaparser.parser.node.types;
+
+import it.fulminazzo.javaparser.parser.node.Node;
+
+/**
+ * Represents an empty literal.
+ */
+public class EmptyLiteral extends Node {
+}
