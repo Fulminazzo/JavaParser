e9a7c3f4739f272ffadfae2d2c02025182685d9f
Fulminazzo <fulminazzo21@gmail.com>
2024-12-14 22:26:54 +0100

    Fixed Statement class not supporting empty literals

--- src/main/java/it/fulminazzo/javaparser/parser/node/statements/Statement.java
+++ src/main/java/it/fulminazzo/javaparser/parser/node/statements/Statement.java
@@ -1,6 +1,7 @@
 package it.fulminazzo.javaparser.parser.node.statements;
 
 import it.fulminazzo.javaparser.parser.node.Node;
+import it.fulminazzo.javaparser.parser.node.types.EmptyLiteral;
 import it.fulminazzo.javaparser.parser.node.types.Literal;
 import org.jetbrains.annotations.NotNull;
 
@@ -10,10 +11,18 @@ import org.jetbrains.annotations.NotNull;
 public class Statement extends Node {
     private final @NotNull Node expr;
 
+    /**
+     * Instantiates a new Statement with expression {@link EmptyLiteral}.
+     */
     public Statement() {
-        this(new Literal(""));
+        this(new EmptyLiteral());
     }
 
+    /**
+     * Instantiates a new Statement.
+     *
+     * @param expr the expression
+     */
     public Statement(final @NotNull Node expr) {
         this.expr = expr;
     }
