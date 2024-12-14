d5df1282f5893609fa211dfb20328921c398e3b8
Fulminazzo <fulminazzo21@gmail.com>
2024-12-14 21:19:03 +0100

    Created Statement class

---
+++ src/main/java/it/fulminazzo/javaparser/parser/node/statements/Statement.java
@@ -0,0 +1,10 @@
+package it.fulminazzo.javaparser.parser.node.statements;
+
+import it.fulminazzo.javaparser.parser.node.Node;
+
+/**
+ * Represents a general statement.
+ */
+public abstract class Statement extends Node {
+
+}
