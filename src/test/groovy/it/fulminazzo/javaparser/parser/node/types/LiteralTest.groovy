46b285c035749a012a1d3e514da6c12bfb2d87a5
Fulminazzo <fulminazzo21@gmail.com>
2024-12-14 14:26:42 +0100

    Added test suite for Literal

---
+++ src/test/groovy/it/fulminazzo/javaparser/parser/node/types/LiteralTest.groovy
@@ -0,0 +1,21 @@
+package it.fulminazzo.javaparser.parser.node.types
+
+import spock.lang.Specification
+
+
+class LiteralTest extends Specification {
+
+    def "test literal split"() {
+        given:
+        def string = "hello.dear.world"
+        def literal = new Literal(string)
+
+        when:
+        def split = literal.splitLastDot()
+
+        then:
+        split.getKey() == new Literal("hello.dear")
+        split.getValue() == new Literal("world")
+    }
+
+}
