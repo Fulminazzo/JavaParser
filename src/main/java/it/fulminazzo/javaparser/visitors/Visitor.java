f25bdb66f930cda5f8990758ebb3d37795d73d88
Fulminazzo <fulminazzo21@gmail.com>
2024-12-26 14:52:18 +0100

    Created Visitor interface

---
+++ src/main/java/it/fulminazzo/javaparser/visitors/Visitor.java
@@ -0,0 +1,12 @@
+package it.fulminazzo.javaparser.visitors;
+
+/**
+ * A <b>Visitor</b> is a special object capable of reading and parsing
+ * each {@link it.fulminazzo.javaparser.parser.node.Node} accordingly.
+ * It provides methods for each type of node.
+ *
+ * @param <T> the returned type
+ */
+public interface Visitor<T> {
+
+}
