799fc1bb94cb0410e964e822fb6ba511045b6cf2
Fulminazzo <fulminazzo21@gmail.com>
2024-12-26 19:02:41 +0100

    Created Environment class with enterScope and exitScope methods

---
+++ src/main/java/it/fulminazzo/javaparser/environment/Environment.java
@@ -0,0 +1,65 @@
+package it.fulminazzo.javaparser.environment;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.LinkedList;
+
+/**
+ * Represents a container for {@link Scope}s.
+ *
+ * @param <T> the type of the values
+ */
+public class Environment<T> implements Scoped<T> {
+    private final LinkedList<Scope<T>> scopes;
+
+    /**
+     * Instantiates a new Environment.
+     */
+    public Environment() {
+        this.scopes = new LinkedList<>();
+    }
+
+    /**
+     * Enters a new {@link Scope} with the given {@link ScopeType} as type.
+     *
+     * @param scopeType the scope type
+     * @return this environment
+     */
+    public Environment<T> enterScope(final @NotNull ScopeType scopeType) {
+        this.scopes.push(new Scope<>(scopeType));
+        return this;
+    }
+
+    /**
+     * Exit the latest {@link Scope}.
+     * If one is remaining, throws an {@link IllegalStateException}.
+     *
+     * @return this environment
+     */
+    public Environment<T> exitScope() {
+        if (this.scopes.size() < 2) throw new IllegalStateException("Cannot exit from main scope");
+        this.scopes.pop();
+        return this;
+    }
+
+    @Override
+    public @NotNull T lookup(@NotNull String name) throws ScopeException {
+        return null;
+    }
+
+    @Override
+    public void define(@NotNull String name, @NotNull T value) throws ScopeException {
+
+    }
+
+    @Override
+    public void update(@NotNull String name, @NotNull T value) throws ScopeException {
+
+    }
+
+    @Override
+    public @NotNull ScopeType scopeType() {
+        return this.scopes.getLast().scopeType();
+    }
+
+}
