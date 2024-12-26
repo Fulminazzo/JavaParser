1ad99063a96b60340ae0d1886a5751745a4ec63d
Fulminazzo <fulminazzo21@gmail.com>
2024-12-26 16:34:30 +0100

    Added test for accept method of Node

--- src/test/groovy/it/fulminazzo/javaparser/visitors/VisitorTest.groovy
+++ src/test/groovy/it/fulminazzo/javaparser/visitors/VisitorTest.groovy
@@ -3,12 +3,29 @@ package it.fulminazzo.javaparser.visitors
 import it.fulminazzo.fulmicollection.objects.Refl
 import it.fulminazzo.fulmicollection.utils.ClassUtils
 import it.fulminazzo.fulmicollection.utils.ReflectionUtils
+import it.fulminazzo.javaparser.parser.node.MethodInvocation
+import it.fulminazzo.javaparser.parser.node.MockNode
 import it.fulminazzo.javaparser.parser.node.Node
+import it.fulminazzo.javaparser.parser.node.container.CodeBlock
+import it.fulminazzo.javaparser.parser.node.types.Literal
+import org.jetbrains.annotations.NotNull
 import spock.lang.Specification
 
 import java.lang.reflect.Modifier
 
 class VisitorTest extends Specification {
+
+    def 'test accept mock node'() {
+        given:
+        def node = new MockNode('mock', 1)
+        def visitor = new MockVisitor()
+
+        when:
+        def converted = node.accept(visitor)
+
+        then:
+        converted == "${node.name}${node.version}"
+    }
 
     def "visitor should have method: visit#clazz.simpleName(#parameters.type.simpleName) "() {
         given:
@@ -61,4 +78,255 @@ class VisitorTest extends Specification {
                 .findAll { !it.simpleName.contains('Mock') }
     }
 
+    class MockVisitor implements Visitor<String> {
+
+        String visitMockNode(String name, int version) {
+            return "${name}${version}"
+        }
+
+        @Override
+        String visitAssignment(@NotNull Literal type, @NotNull Node assignment) {
+            return null
+        }
+
+        @Override
+        String visitMethodCall(@NotNull Literal executor, @NotNull MethodInvocation invocation) {
+            return null
+        }
+
+        @Override
+        String visitMethodInvocation(@NotNull List<Node> parameters) {
+            return null
+        }
+
+        @Override
+        String visitDynamicArray(@NotNull List<Node> parameters, @NotNull Node type) {
+            return null
+        }
+
+        @Override
+        String visitStaticArray(int size, @NotNull Node type) {
+            return null
+        }
+
+        @Override
+        String visitCodeBlock(@NotNull LinkedList<Node> statements) {
+            return null
+        }
+
+        @Override
+        String visitAdd(@NotNull Node left, @NotNull Node right) {
+            return null
+        }
+
+        @Override
+        String visitAnd(@NotNull Node left, @NotNull Node right) {
+            return null
+        }
+
+        @Override
+        String visitBitAnd(@NotNull Node left, @NotNull Node right) {
+            return null
+        }
+
+        @Override
+        String visitBitOr(@NotNull Node left, @NotNull Node right) {
+            return null
+        }
+
+        @Override
+        String visitBitXor(@NotNull Node left, @NotNull Node right) {
+            return null
+        }
+
+        @Override
+        String visitDivide(@NotNull Node left, @NotNull Node right) {
+            return null
+        }
+
+        @Override
+        String visitEqual(@NotNull Node left, @NotNull Node right) {
+            return null
+        }
+
+        @Override
+        String visitGreaterThan(@NotNull Node left, @NotNull Node right) {
+            return null
+        }
+
+        @Override
+        String visitGreaterThanEqual(@NotNull Node left, @NotNull Node right) {
+            return null
+        }
+
+        @Override
+        String visitLShift(@NotNull Node left, @NotNull Node right) {
+            return null
+        }
+
+        @Override
+        String visitLessThan(@NotNull Node left, @NotNull Node right) {
+            return null
+        }
+
+        @Override
+        String visitLessThanEqual(@NotNull Node left, @NotNull Node right) {
+            return null
+        }
+
+        @Override
+        String visitModulo(@NotNull Node left, @NotNull Node right) {
+            return null
+        }
+
+        @Override
+        String visitMultiply(@NotNull Node left, @NotNull Node right) {
+            return null
+        }
+
+        @Override
+        String visitNewObject(@NotNull Node left, @NotNull Node right) {
+            return null
+        }
+
+        @Override
+        String visitNotEqual(@NotNull Node left, @NotNull Node right) {
+            return null
+        }
+
+        @Override
+        String visitOr(@NotNull Node left, @NotNull Node right) {
+            return null
+        }
+
+        @Override
+        String visitRShift(@NotNull Node left, @NotNull Node right) {
+            return null
+        }
+
+        @Override
+        String visitReAssign(@NotNull Node left, @NotNull Node right) {
+            return null
+        }
+
+        @Override
+        String visitSubtract(@NotNull Node left, @NotNull Node right) {
+            return null
+        }
+
+        @Override
+        String visitURShift(@NotNull Node left, @NotNull Node right) {
+            return null
+        }
+
+        @Override
+        String visitDecrement(boolean before, @NotNull Node operand) {
+            return null
+        }
+
+        @Override
+        String visitIncrement(boolean before, @NotNull Node operand) {
+            return null
+        }
+
+        @Override
+        String visitMinus(@NotNull Node operand) {
+            return null
+        }
+
+        @Override
+        String visitNot(@NotNull Node operand) {
+            return null
+        }
+
+        @Override
+        String visitBreak(@NotNull Node expr) {
+            return null
+        }
+
+        @Override
+        String visitContinue(@NotNull Node expr) {
+            return null
+        }
+
+        @Override
+        String visitDoStatement(@NotNull CodeBlock code, @NotNull Node expr) {
+            return null
+        }
+
+        @Override
+        String visitEnhancedForStatement(@NotNull Node type, @NotNull Node variable, @NotNull CodeBlock code, @NotNull Node expr) {
+            return null
+        }
+
+        @Override
+        String visitForStatement(@NotNull Node assignment, @NotNull Node increment, @NotNull CodeBlock code, @NotNull Node expr) {
+            return null
+        }
+
+        @Override
+        String visitIfStatement(@NotNull CodeBlock code, Node thenBranch, @NotNull Node expr) {
+            return null
+        }
+
+        @Override
+        String visitReturn(@NotNull Node expr) {
+            return null
+        }
+
+        @Override
+        String visitStatement(@NotNull Node expr) {
+            return null
+        }
+
+        @Override
+        String visitWhileStatement(@NotNull CodeBlock code, @NotNull Node expr) {
+            return null
+        }
+
+        @Override
+        String visitBooleanLiteral(String rawValue) {
+            return null
+        }
+
+        @Override
+        String visitCharLiteral(String rawValue) {
+            return null
+        }
+
+        @Override
+        String visitDoubleLiteral(String rawValue) {
+            return null
+        }
+
+        @Override
+        String visitEmptyLiteral() {
+            return null
+        }
+
+        @Override
+        String visitFloatLiteral(String rawValue) {
+            return null
+        }
+
+        @Override
+        String visitLiteral(String rawValue) {
+            return null
+        }
+
+        @Override
+        String visitLongLiteral(String rawValue) {
+            return null
+        }
+
+        @Override
+        String visitNumberLiteral(String rawValue) {
+            return null
+        }
+
+        @Override
+        String visitStringLiteral(String rawValue) {
+            return null
+        }
+    }
 }
