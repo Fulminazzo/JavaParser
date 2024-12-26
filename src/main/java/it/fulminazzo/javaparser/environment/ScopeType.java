eacba619ac904d00ab8680f9cb8ebb270b9e2726
Fulminazzo <fulminazzo21@gmail.com>
2024-12-26 18:14:08 +0100

    Created ScopeType enum

---
+++ src/main/java/it/fulminazzo/javaparser/environment/ScopeType.java
@@ -0,0 +1,16 @@
+package it.fulminazzo.javaparser.environment;
+
+/**
+ * The type of the scope defines the statement that generated it.
+ * This is useful when checking for <code>continue</code> or
+ * <code>break</code> statements being executed in the correct context.
+ */
+public enum ScopeType {
+    CODE_BLOCK,
+    SWITCH,
+    CASE,
+    FOR,
+    WHILE,
+    DO
+
+}
