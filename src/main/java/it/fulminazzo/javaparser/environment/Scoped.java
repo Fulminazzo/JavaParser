1e8f166b23f0ad98aec900518e437fca912544c0
Fulminazzo <fulminazzo21@gmail.com>
2024-12-26 18:09:34 +0100

    Added javadoc and exception methods

--- src/main/java/it/fulminazzo/javaparser/environment/Scoped.java
+++ src/main/java/it/fulminazzo/javaparser/environment/Scoped.java
@@ -1,4 +1,30 @@
 package it.fulminazzo.javaparser.environment;
 
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * An object that can hold values in the form (name, value) pair.
+ *
+ * @param <T> the type of the values
+ */
 interface Scoped<T> {
+
+    /**
+     * Throws an exception for a variable not declared.
+     *
+     * @param name the name of the variable
+     */
+    default void noSuchVariable(final @NotNull String name) {
+        throw new ScopeException("No such variable: " + name);
+    }
+
+    /**
+     * Throws an exception for a variable already declared.
+     *
+     * @param name the name of the variable
+     */
+    default void alreadyDeclaredVariable(final @NotNull String name) {
+        throw new ScopeException("Variable already declared: " + name);
+    }
+
 }
