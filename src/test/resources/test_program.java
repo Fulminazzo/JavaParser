e6cca4ef54107a7d6afbe23056c69c42c15a7a59
Fulminazzo <fulminazzo21@gmail.com>
2024-12-27 19:25:15 +0100

    Fixed binary operations

--- src/test/resources/test_program.java
+++ src/test/resources/test_program.java
@@ -38,8 +38,8 @@ String sW = "Hello world";
 // Binary operations
 i = i + 1 - 1 * 1 / 1 % 1 & 1 | 1 ^ 1 << 2 >> 1 >>> 1;
 l = l + 1 - 1 * 1 / 1 % 1 & 1 | 1 ^ 1 << 2 >> 1 >>> 1;
-f = f + 1 - 1 * 1 / 1 % 1 & 1 | 1 ^ 1 << 2 >> 1 >>> 1;
-d = d + 1 - 1 * 1 / 1 % 1 & 1 | 1 ^ 1 << 2 >> 1 >>> 1;
+f = f + 1 - 1 * 1 / 1 % 1;
+d = d + 1 - 1 * 1 / 1 % 1;
 bo = true == false != true && false || true;
 bo = d == f != i && bo || false;
 bo = i < d || i <= d || f >= d || f > d;
