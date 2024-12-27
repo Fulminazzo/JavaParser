c32acbea21ed88c9d68e82ace54bc4c6640dd8cc
Fulminazzo <fulminazzo21@gmail.com>
2024-12-27 19:54:06 +0100

    Added constructor with TokenType

--- src/main/java/it/fulminazzo/javaparser/parser/ParserException.java
+++ src/main/java/it/fulminazzo/javaparser/parser/ParserException.java
@@ -1,5 +1,6 @@
 package it.fulminazzo.javaparser.parser;
 
+import it.fulminazzo.javaparser.tokenizer.TokenType;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -14,6 +15,15 @@ public class ParserException extends RuntimeException {
      */
     public ParserException(final @NotNull String message) {
         super(message);
+    }
+
+    /**
+     * Instantiates a new Parser exception with message "<i>Unexpected token %tokenType%</i>".
+     *
+     * @param tokenType the token type
+     */
+    public ParserException(final @NotNull TokenType tokenType) {
+        this("Unexpected token: "  + tokenType);
     }
 
 }
