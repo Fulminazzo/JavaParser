71c27a55054e28a736dc0101e82a17bd941ddba8
Fulminazzo <fulminazzo21@gmail.com>
2024-12-27 19:05:13 +0100

    Started rewriting test_program.java Added assignments of all primitive types and re-assignments

--- src/test/resources/test_program.java
+++ src/test/resources/test_program.java
@@ -1,20 +1,20 @@
-double arr = new double[10];
-for (int i = 0; i < arr.length; i++) arr[i] = ++i * 2;
+// Assignment of all primitive types
+byte b = 1;
+short s = 2;
+char c = 'c';
+int i = 4;
+long l = 5L;
+float f = 6.0f;
+double d = 7.0d;
+boolean bo = true;
+String string = "Hello world";
 
-for (double a : arr) {
-    int index = (int) (a % arr.length);
-    arr[index] /= 2;
-
-    if (arr[index] == 0) arr[index]--;
-}
-
-int i = 0;
-while (i < arr.length) {
-    if (arr[i] == 0) throw new RuntimeException("Invalid value arr[" + i + "]");
-    i++;
-}
-
-// Custom class that should be provided by tests
-String output = new it.fulminazzo.javaparser.parser.Printer().print(arr);
-
-return output
+// Re-assignments
+i = -4;
+l = 5;
+f = 6;
+d = 7;
+d = 7.0f;
+d = 7.0;
+bo = false;
+bo = !bo;
