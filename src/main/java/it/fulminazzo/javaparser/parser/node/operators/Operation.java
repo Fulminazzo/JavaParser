2bd6503fd542f9df0029d756c24606e888825e8d
Fulminazzo <fulminazzo21@gmail.com>
2024-12-27 19:00:41 +0100

    Added no arguments constructors

--- src/main/java/it/fulminazzo/javaparser/parser/node/operators/Operation.java
+++ src/main/java/it/fulminazzo/javaparser/parser/node/operators/Operation.java
@@ -1,10 +1,12 @@
 package it.fulminazzo.javaparser.parser.node.operators;
 
 import it.fulminazzo.javaparser.parser.node.Node;
+import lombok.NoArgsConstructor;
 
 /**
  * Represents a general operation.
  */
+@NoArgsConstructor
 public abstract class Operation extends Node {
 
 }
--- src/main/java/it/fulminazzo/javaparser/parser/node/types/EmptyLiteral.java
+++ src/main/java/it/fulminazzo/javaparser/parser/node/types/EmptyLiteral.java
@@ -1,9 +1,11 @@
 package it.fulminazzo.javaparser.parser.node.types;
 
 import it.fulminazzo.javaparser.parser.node.Node;
+import lombok.NoArgsConstructor;
 
 /**
  * Represents an empty literal.
  */
+@NoArgsConstructor
 public class EmptyLiteral extends Node {
 }
