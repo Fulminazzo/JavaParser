48f2937a22f64aadf2f24c410c539107268f81ff
Fulminazzo <fulminazzo21@gmail.com>
2024-12-27 20:15:21 +0100

    Added test for return in parseSingleStatement

--- src/test/groovy/it/fulminazzo/javaparser/parser/JavaParserTest.groovy
+++ src/test/groovy/it/fulminazzo/javaparser/parser/JavaParserTest.groovy
@@ -39,6 +39,7 @@ class JavaParserTest extends Specification {
 
         where:
         code     | expected
+        'return 1;' | new Return(new NumberLiteral('1'))
         'break;' | new Break()
         ';'      | new Statement()
     }
