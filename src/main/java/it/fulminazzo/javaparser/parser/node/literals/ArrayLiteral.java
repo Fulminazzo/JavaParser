7ce638355f020008993d00cae03d4e8a129ba82d
Fulminazzo <fulminazzo21@gmail.com>
2024-12-28 23:11:41 +0100

    Added getType method

--- src/main/java/it/fulminazzo/javaparser/parser/node/literals/ArrayLiteral.java
+++ src/main/java/it/fulminazzo/javaparser/parser/node/literals/ArrayLiteral.java
@@ -2,11 +2,13 @@ package it.fulminazzo.javaparser.parser.node.literals;
 
 import it.fulminazzo.javaparser.parser.node.Node;
 import it.fulminazzo.javaparser.parser.node.NodeImpl;
+import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * Represents an array literal, an expression of the type <code>.*\[\]</code>.
  */
+@Getter
 public class ArrayLiteral extends NodeImpl implements Literal {
     private final @NotNull Node type;
 
