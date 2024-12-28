ad097fcd0c17544e8ce58a5c7487cdcb2d26fcbc
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 00:41:37 +0100

    Added ObjectData class

--- src/main/java/it/fulminazzo/javaparser/environment/Scope.java
+++ src/main/java/it/fulminazzo/javaparser/environment/Scope.java
@@ -47,4 +47,45 @@ class Scope<T> implements Scoped<T> {
         return this.scopeType;
     }
 
+    /**
+     * Represents the information of an object.
+     *
+     * @param <O> the type of the object
+     */
+    static class ObjectData<O> {
+        private final @NotNull Info<O> info;
+        private final @NotNull String name;
+
+        /**
+         * Instantiates a new Object data.
+         *
+         * @param info the info
+         * @param name the name
+         */
+        public ObjectData(final @NotNull Info<O> info, final @NotNull String name) {
+            this.info = info;
+            this.name = name;
+        }
+
+        @Override
+        public int hashCode() {
+            return this.info.hashCode() + this.name.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o instanceof ObjectData) {
+                ObjectData<?> other = (ObjectData<?>) o;
+                return this.info.equals(other.info) && this.name.equals(other.name);
+            }
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s(%s, %s)", getClass().getSimpleName(), this.info, this.name);
+        }
+
+    }
+
 }
