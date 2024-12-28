f38f9c3b01f276cfff1298c83bc4701dc98f6a01
Fulminazzo <fulminazzo21@gmail.com>
2024-12-28 23:45:09 +0100

    Added tests for Scope methods

---
+++ src/test/groovy/it/fulminazzo/javaparser/environment/ScopeTest.groovy
@@ -0,0 +1,31 @@
+package it.fulminazzo.javaparser.environment
+
+import spock.lang.Specification
+
+class ScopeTest extends Specification {
+    private Scope<String> scope
+
+    void setup() {
+        this.scope = new Scope<>(ScopeType.CODE_BLOCK)
+    }
+
+    def 'declare should throw ScopeException on already declared variable'() {
+        when:
+        this.scope.declare('var', '1')
+        this.scope.declare('var', '1')
+
+        then:
+        def e = thrown(ScopeException)
+        e.getMessage() == this.scope.alreadyDeclaredVariable('var').message
+    }
+
+    def 'update should throw ScopeException on not declared variable'() {
+        when:
+        this.scope.update('var', '1')
+
+        then:
+        def e = thrown(ScopeException)
+        e.getMessage() == this.scope.noSuchVariable('var').message
+    }
+
+}
