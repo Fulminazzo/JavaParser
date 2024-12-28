737148256d2328493c85716040906bf48bc5c795
Fulminazzo <fulminazzo21@gmail.com>
2024-12-28 02:12:46 +0100

    Added tests for Parser#match exception

---
+++ src/test/groovy/it/fulminazzo/javaparser/parser/ParserTest.groovy
@@ -0,0 +1,24 @@
+package it.fulminazzo.javaparser.parser
+
+import it.fulminazzo.javaparser.tokenizer.TokenType
+import spock.lang.Specification
+
+class ParserTest extends Specification {
+    private Parser parser
+
+    void setup() {
+        this.parser = new Parser() {}
+    }
+
+    def 'test match method exception'() {
+        given:
+        this.parser.setInput('mock input')
+
+        when:
+        this.parser.match(TokenType.DIVIDE)
+
+        then:
+        thrown(ParserException)
+    }
+
+}
