0dfb5696e913827bcf5e4d18f01f2b5079265383
Fulminazzo <fulminazzo21@gmail.com>
2024-12-28 21:54:36 +0100

    Moved LiteralException to NodeException and refactored package

---
+++ src/main/java/it/fulminazzo/javaparser/parser/node/NodeException.java
@@ -0,0 +1,10 @@
+package it.fulminazzo.javaparser.parser.node;
+
+import lombok.NoArgsConstructor;
+
+/**
+ * An exception thrown by {@link TokenizedNode} when reading invalid value.
+ */
+@NoArgsConstructor
+public final class NodeException extends Exception {
+}
