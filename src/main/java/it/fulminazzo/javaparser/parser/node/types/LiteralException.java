6067d24699df29825a0c1784dac27e0173c06b77
Fulminazzo <fulminazzo21@gmail.com>
2024-12-28 01:23:58 +0100

    Created LiteralException class

---
+++ src/main/java/it/fulminazzo/javaparser/parser/node/types/LiteralException.java
@@ -0,0 +1,10 @@
+package it.fulminazzo.javaparser.parser.node.types;
+
+import lombok.NoArgsConstructor;
+
+/**
+ * An exception thrown by {@link BaseTypeLiteral} when reading invalid value.
+ */
+@NoArgsConstructor
+public final class LiteralException extends Exception {
+}
