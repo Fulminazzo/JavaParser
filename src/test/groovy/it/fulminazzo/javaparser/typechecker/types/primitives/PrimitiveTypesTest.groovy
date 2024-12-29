8df2f5a469269d7c29840295381ac4fe82e5646c
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 16:59:12 +0100

    Added several tests of compatibility

--- src/test/groovy/it/fulminazzo/javaparser/typechecker/types/primitives/PrimitiveTypesTest.groovy
+++ src/test/groovy/it/fulminazzo/javaparser/typechecker/types/primitives/PrimitiveTypesTest.groovy
@@ -2,7 +2,183 @@ package it.fulminazzo.javaparser.typechecker.types.primitives
 
 import spock.lang.Specification
 
+import static it.fulminazzo.javaparser.typechecker.types.values.ValueTypes.*;
+
 class PrimitiveTypesTest extends Specification {
+
+    def 'test BYTE compatible with #type'() {
+        expect:
+        PrimitiveTypes.BYTE.compatibleWith(type)
+
+        where:
+        type << [
+                CHAR_TYPE, NUMBER_TYPE,
+        ]
+    }
+
+    def 'test BYTE incompatible with #type'() {
+        expect:
+        !PrimitiveTypes.BYTE.compatibleWith(type)
+
+        where:
+        type << [
+                LONG_TYPE,
+                DOUBLE_TYPE, FLOAT_TYPE,
+                BOOLEAN_TYPE, STRING_TYPE
+        ]
+    }
+
+    def 'test CHAR compatible with #type'() {
+        expect:
+        PrimitiveTypes.CHAR.compatibleWith(type)
+
+        where:
+        type << [
+                CHAR_TYPE, NUMBER_TYPE
+        ]
+    }
+
+    def 'test CHAR incompatible with #type'() {
+        expect:
+        !PrimitiveTypes.CHAR.compatibleWith(type)
+
+        where:
+        type << [
+                LONG_TYPE,
+                DOUBLE_TYPE, FLOAT_TYPE,
+                BOOLEAN_TYPE, STRING_TYPE
+        ]
+    }
+
+    def 'test SHORT compatible with #type'() {
+        expect:
+        PrimitiveTypes.SHORT.compatibleWith(type)
+
+        where:
+        type << [
+                CHAR_TYPE, NUMBER_TYPE,
+        ]
+    }
+
+    def 'test SHORT incompatible with #type'() {
+        expect:
+        !PrimitiveTypes.SHORT.compatibleWith(type)
+
+        where:
+        type << [
+                LONG_TYPE,
+                DOUBLE_TYPE, FLOAT_TYPE,
+                BOOLEAN_TYPE, STRING_TYPE
+        ]
+    }
+
+    def 'test INT compatible with #type'() {
+        expect:
+        PrimitiveTypes.INT.compatibleWith(type)
+
+        where:
+        type << [
+                CHAR_TYPE, NUMBER_TYPE,
+        ]
+    }
+
+    def 'test INT incompatible with #type'() {
+        expect:
+        !PrimitiveTypes.INT.compatibleWith(type)
+
+        where:
+        type << [
+                LONG_TYPE,
+                DOUBLE_TYPE, FLOAT_TYPE,
+                BOOLEAN_TYPE, STRING_TYPE
+        ]
+    }
+
+    def 'test LONG compatible with #type'() {
+        expect:
+        PrimitiveTypes.LONG.compatibleWith(type)
+
+        where:
+        type << [
+                CHAR_TYPE, NUMBER_TYPE, LONG_TYPE,
+        ]
+    }
+
+    def 'test LONG incompatible with #type'() {
+        expect:
+        !PrimitiveTypes.LONG.compatibleWith(type)
+
+        where:
+        type << [
+                DOUBLE_TYPE, FLOAT_TYPE,
+                BOOLEAN_TYPE, STRING_TYPE
+        ]
+    }
+
+    def 'test FLOAT compatible with #type'() {
+        expect:
+        PrimitiveTypes.FLOAT.compatibleWith(type)
+
+        where:
+        type << [
+                CHAR_TYPE, NUMBER_TYPE, LONG_TYPE,
+                FLOAT_TYPE,
+        ]
+    }
+
+    def 'test FLOAT incompatible with #type'() {
+        expect:
+        !PrimitiveTypes.FLOAT.compatibleWith(type)
+
+        where:
+        type << [
+                DOUBLE_TYPE,
+                BOOLEAN_TYPE, STRING_TYPE
+        ]
+    }
+
+    def 'test DOUBLE compatible with #type'() {
+        expect:
+        PrimitiveTypes.DOUBLE.compatibleWith(type)
+
+        where:
+        type << [
+                CHAR_TYPE, NUMBER_TYPE, LONG_TYPE,
+                FLOAT_TYPE, DOUBLE_TYPE,
+        ]
+    }
+
+    def 'test DOUBLE incompatible with #type'() {
+        expect:
+        !PrimitiveTypes.DOUBLE.compatibleWith(type)
+
+        where:
+        type << [
+                BOOLEAN_TYPE, STRING_TYPE
+        ]
+    }
+
+    def 'test BOOLEAN compatible with #type'() {
+        expect:
+        PrimitiveTypes.BOOLEAN.compatibleWith(type)
+
+        where:
+        type << [
+                BOOLEAN_TYPE
+        ]
+    }
+
+    def 'test BOOLEAN incompatible with #type'() {
+        expect:
+        !PrimitiveTypes.BOOLEAN.compatibleWith(type)
+
+        where:
+        type << [
+                CHAR_TYPE, NUMBER_TYPE, LONG_TYPE,
+                DOUBLE_TYPE, FLOAT_TYPE,
+                STRING_TYPE
+        ]
+    }
 
     def 'test name method of #type'() {
         when:
