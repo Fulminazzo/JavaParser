b990d49de31b166d35dc64b1a70f96019cd2a7fc
Fulminazzo <fulminazzo21@gmail.com>
2024-12-30 00:36:14 +0100

    Implemented toJavaClass method

--- src/main/java/it/fulminazzo/javaparser/typechecker/types/arrays/ArrayClassType.java
+++ src/main/java/it/fulminazzo/javaparser/typechecker/types/arrays/ArrayClassType.java
@@ -5,6 +5,8 @@ import it.fulminazzo.javaparser.typechecker.types.Type;
 import it.fulminazzo.javaparser.typechecker.types.TypeWrapper;
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.reflect.Array;
+
 /**
  * Represents the class type for an array.
  */
@@ -17,6 +19,12 @@ public class ArrayClassType extends TypeWrapper implements ClassType {
      */
     public ArrayClassType(@NotNull ClassType componentType) {
         super(componentType);
+    }
+
+    @Override
+    public Class<?> toJavaClass() {
+        Class<?> componentClass = ((ClassType) this.object).toJavaClass();
+        return Array.newInstance(componentClass, 0).getClass();
     }
 
     @Override
