c4a71bec22e1f5119563486de47c2fbddf4ffb20
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 15:11:52 +0100

    Created ClassType class

---
+++ src/main/java/it/fulminazzo/javaparser/typechecker/types/ClassType.java
@@ -0,0 +1,23 @@
+package it.fulminazzo.javaparser.typechecker.types;
+
+import it.fulminazzo.javaparser.environment.Info;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Represents the class of a {@link Type}.
+ *
+ * @param <I> the type
+ */
+public abstract class ClassType<I extends Type> extends Type implements Info<I> {
+    protected final @NotNull I internalType;
+
+    /**
+     * Instantiates a new Class type.
+     *
+     * @param internalType the internal type
+     */
+    public ClassType(final @NotNull I internalType) {
+        this.internalType = internalType;
+    }
+
+}
