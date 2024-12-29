365853c7430e569a73c3d5efaffc285924f071f5
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 19:20:54 +0100

    Created OperationUtils utility class

---
+++ src/main/java/it/fulminazzo/javaparser/typechecker/OperationUtils.java
@@ -0,0 +1,12 @@
+package it.fulminazzo.javaparser.typechecker;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * A collection of utility functions for handling operations.
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class OperationUtils {
+
+}
