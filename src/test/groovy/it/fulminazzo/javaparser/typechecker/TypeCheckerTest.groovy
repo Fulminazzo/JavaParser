fc21e09ddff8327f7252abb36450dc2508ae5aa0
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 21:57:16 +0100

    Added tests for visitAdd method

--- src/test/groovy/it/fulminazzo/javaparser/typechecker/TypeCheckerTest.groovy
+++ src/test/groovy/it/fulminazzo/javaparser/typechecker/TypeCheckerTest.groovy
@@ -1,5 +1,9 @@
 package it.fulminazzo.javaparser.typechecker
 
+import it.fulminazzo.javaparser.parser.node.values.BooleanValueLiteral
+import it.fulminazzo.javaparser.parser.node.values.NumberValueLiteral
+import it.fulminazzo.javaparser.typechecker.types.Type
+import it.fulminazzo.javaparser.typechecker.types.ValueType
 import spock.lang.Specification
 
 class TypeCheckerTest extends Specification {
@@ -7,6 +11,29 @@ class TypeCheckerTest extends Specification {
 
     void setup() {
         this.typeChecker = new TypeChecker()
+    }
+
+    def 'test valid and'() {
+        given:
+        Type type = this.typeChecker.visitAnd(new BooleanValueLiteral('true'),
+                new BooleanValueLiteral('true'))
+
+        expect:
+        type == ValueType.BOOLEAN
+    }
+
+    def 'test invalid and'() {
+        when:
+        this.typeChecker.visitAnd(first, second)
+
+        then:
+        thrown(TypeCheckerException)
+
+        where:
+        first                           | second
+        new BooleanValueLiteral('true') | new NumberValueLiteral('1')
+        new NumberValueLiteral('1')     | new BooleanValueLiteral('false')
+        new NumberValueLiteral('1')     | new NumberValueLiteral('1')
     }
 
 }
