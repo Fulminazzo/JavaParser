1559798f6497b7b774aa243b71cf89f24519c09f
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 20:26:51 +0100

    Added tests for ObjectWrapper

---
+++ src/test/groovy/it/fulminazzo/javaparser/ObjectWrapperTest.groovy
@@ -0,0 +1,49 @@
+package it.fulminazzo.javaparser
+
+import spock.lang.Specification
+
+class ObjectWrapperTest extends Specification {
+    private String string
+    private ObjectWrapper wrapper
+
+    void setup() {
+        this.string = 'Hello, world!'
+        this.wrapper = new MockWrapper(string)
+    }
+
+    def 'test hashCode'() {
+        expect:
+        this.wrapper.hashCode() == MockWrapper.hashCode() + this.string.hashCode()
+    }
+
+    def 'test equality'() {
+        expect:
+        this.wrapper == new MockWrapper(this.string)
+    }
+
+    def 'test inequality with #object'() {
+        expect:
+        !this.wrapper.equals(object)
+
+        where:
+        object << [
+                null,
+                new ObjectWrapper<>(10),
+                new MockWrapper('Hello, enemy!')
+        ]
+    }
+
+    def 'test toString'() {
+        expect:
+        this.wrapper.toString() == "${MockWrapper.class.simpleName}(${this.string})"
+    }
+
+    static class MockWrapper extends ObjectWrapper<String> {
+
+        MockWrapper(String object) {
+            super(object)
+        }
+
+    }
+
+}
