56e9b1ecd9976d0cd28d24562bd98f4c29e09b20
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 15:34:35 +0100

    Created ValueTypes container class

---
+++ src/main/java/it/fulminazzo/javaparser/typechecker/types/values/ValueTypes.java
@@ -0,0 +1,12 @@
+package it.fulminazzo.javaparser.typechecker.types.values;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * Contains all the {@link ValueType}s representing the values accepted by the language.
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ValueTypes {
+
+}
