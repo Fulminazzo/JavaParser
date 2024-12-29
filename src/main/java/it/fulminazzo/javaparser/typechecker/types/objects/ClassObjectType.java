c606c7fbc249b4c1f709e121e16fd18faa765466
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 18:09:52 +0100

    Added several types

--- src/main/java/it/fulminazzo/javaparser/typechecker/types/objects/ClassObjectType.java
+++ src/main/java/it/fulminazzo/javaparser/typechecker/types/objects/ClassObjectType.java
@@ -2,12 +2,51 @@ package it.fulminazzo.javaparser.typechecker.types.objects;
 
 import it.fulminazzo.javaparser.typechecker.types.ClassType;
 import it.fulminazzo.javaparser.typechecker.types.Type;
+import it.fulminazzo.javaparser.typechecker.types.ValueType;
 import org.jetbrains.annotations.NotNull;
+
+import static it.fulminazzo.javaparser.typechecker.types.ValueType.NUMBER;
 
 /**
  * Represents an {@link Object} class type.
  */
 public enum ClassObjectType implements ClassType {
+    /**
+     * {@link java.lang.Byte}
+     */
+    BYTE(ValueType.CHAR, NUMBER),
+    /**
+     * {@link java.lang.Character}
+     */
+    CHARACTER(ValueType.CHAR, NUMBER),
+    /**
+     * {@link java.lang.Short}
+     */
+    SHORT(ValueType.CHAR, NUMBER),
+    /**
+     * {@link java.lang.Integer}
+     */
+    INTEGER(ValueType.CHAR, NUMBER),
+    /**
+     * {@link java.lang.Long}
+     */
+    LONG(ValueType.CHAR, NUMBER, ValueType.LONG),
+    /**
+     * {@link java.lang.Float}
+     */
+    FLOAT(ValueType.CHAR, NUMBER, ValueType.LONG, ValueType.FLOAT),
+    /**
+     * {@link java.lang.Double}
+     */
+    DOUBLE(ValueType.CHAR, NUMBER, ValueType.LONG, ValueType.FLOAT, ValueType.DOUBLE),
+    /**
+     * {@link java.lang.Boolean}
+     */
+    BOOLEAN(ValueType.BOOLEAN),
+    /**
+     * {@link java.lang.String}
+     */
+    STRING(ValueType.STRING),
     ;
 
     private final Type @NotNull [] compatibleTypes;
