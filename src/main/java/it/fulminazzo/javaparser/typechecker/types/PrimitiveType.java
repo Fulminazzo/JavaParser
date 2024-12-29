3a89f334736be5a7ed551209bc2ec80ae88762de
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 17:07:11 +0100

    Added several primitive types

--- src/main/java/it/fulminazzo/javaparser/typechecker/types/PrimitiveType.java
+++ src/main/java/it/fulminazzo/javaparser/typechecker/types/PrimitiveType.java
@@ -2,7 +2,17 @@ package it.fulminazzo.javaparser.typechecker.types;
 
 import org.jetbrains.annotations.NotNull;
 
+import static it.fulminazzo.javaparser.typechecker.types.values.ValueTypes.*;
+
 public enum PrimitiveType implements ClassType {
+    BYTE(CHAR_TYPE, NUMBER_TYPE),
+    CHAR(CHAR_TYPE, NUMBER_TYPE),
+    SHORT(CHAR_TYPE, NUMBER_TYPE),
+    INT(CHAR_TYPE, NUMBER_TYPE),
+    LONG(CHAR_TYPE, NUMBER_TYPE, LONG_TYPE),
+    FLOAT(CHAR_TYPE, NUMBER_TYPE, LONG_TYPE, FLOAT_TYPE),
+    DOUBLE(CHAR_TYPE, NUMBER_TYPE, LONG_TYPE, FLOAT_TYPE, DOUBLE_TYPE),
+    BOOLEAN(BOOLEAN_TYPE),
     ;
 
     private final Type @NotNull [] compatibleTypes;
