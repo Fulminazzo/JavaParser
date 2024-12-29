47e592e4f899024c49a7285ddfcd31c1459b3480
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 17:11:56 +0100

    Added all types parsed by Java

--- src/main/java/it/fulminazzo/javaparser/typechecker/types/values/ValueTypes.java
+++ src/main/java/it/fulminazzo/javaparser/typechecker/types/values/ValueTypes.java
@@ -1,8 +1,16 @@
 package it.fulminazzo.javaparser.typechecker.types.values;
 
 /**
- * Contains all the {@link ValueType}s representing the values accepted by the language.
+ * Contains all the {@link ValueType}s representing the values accepted by Java.
  */
 public enum ValueTypes {
+    CHAR_TYPE,
+    NUMBER_TYPE,
+    LONG_TYPE,
+    DOUBLE_TYPE,
+    FLOAT_TYPE,
+    BOOLEAN_TYPE,
+    STRING_TYPE
+    ;
 
 }
