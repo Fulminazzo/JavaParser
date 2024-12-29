e781158ce9776dec2c6f82c24b9a431357c5ea3b
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 15:47:54 +0100

    Created EnumType class with valueOf, values and name methods

---
+++ src/main/java/it/fulminazzo/javaparser/typechecker/types/EnumType.java
@@ -0,0 +1,51 @@
+package it.fulminazzo.javaparser.typechecker.types;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Represents a general class which provides various <code>static final</code> fields of the same type.
+ * It provides useful methods for the lookup and retrieval of certain fields, similar to {@link Enum}.
+ */
+public class EnumType {
+
+    /**
+     * Returns the name of the current object based on its field name.
+     *
+     * @return the name
+     */
+    public @NotNull String name() {
+
+    }
+
+    /**
+     * Returns all the static fields of the given type.
+     *
+     * @param <E>             the type of the fields
+     * @param fieldsContainer the class where the fields are stored
+     * @param returnType      the type of the returned fields
+     * @return an array containing all the fields
+     */
+    public static <E extends EnumType> E @NotNull [] values(final @NotNull Class<?> fieldsContainer,
+                                                            final @NotNull Class<E> returnType) {
+
+    }
+
+    /**
+     * Gets the most appropriate field with the given name.
+     * Uses {@link #values(Class, Class)} and then {@link #name()}.
+     * Throws {@link IllegalArgumentException} in case of error.
+     *
+     * @param <E>             the type of the field
+     * @param fieldsContainer the class where the field is stored
+     * @param returnType      the type of the returned fields
+     * @param name            the name of the field
+     * @return the field if found
+     */
+    public static <E extends EnumType> @NotNull E valueOf(final @NotNull Class<?> fieldsContainer,
+                                                          final @NotNull Class<E> returnType,
+                                                          final @NotNull String name) {
+
+    }
+
+
+}
