e6bd198997287e75ea474b17abe151df04888020
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 19:10:01 +0100

    Made CustomClassObjectType extend TypeWrapper

--- src/main/java/it/fulminazzo/javaparser/typechecker/types/objects/CustomClassObjectType.java
+++ src/main/java/it/fulminazzo/javaparser/typechecker/types/objects/CustomClassObjectType.java
@@ -2,21 +2,21 @@ package it.fulminazzo.javaparser.typechecker.types.objects;
 
 import it.fulminazzo.javaparser.typechecker.types.ClassType;
 import it.fulminazzo.javaparser.typechecker.types.Type;
+import it.fulminazzo.javaparser.typechecker.types.TypeWrapper;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * Represents a {@link ClassObjectType} with a class different from the default types.
  */
-class CustomClassObjectType implements ClassType {
-    private final ObjectType internalType;
+class CustomClassObjectType extends TypeWrapper implements ClassType {
 
     /**
      * Instantiates a new Custom class object type.
      *
      * @param internalType the internal type
      */
-    public CustomClassObjectType(final @NotNull ObjectType internalType) {
-        this.internalType = internalType;
+    public CustomClassObjectType(@NotNull ObjectType internalType) {
+        super(internalType);
     }
 
     @Override
@@ -26,16 +26,6 @@ class CustomClassObjectType implements ClassType {
             return this.internalType.equals(objectType);
         }
         return false;
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode() + this.internalType.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        return o instanceof CustomClassObjectType && this.internalType.equals(((CustomClassObjectType) o).internalType);
     }
 
     @Override
