b6e7beb5ce8a5e1893a3b7244b1c922bc583756a
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 19:27:50 +0100

    Added tests for OperationUtils execute binary operations

---
+++ src/test/groovy/it/fulminazzo/javaparser/typechecker/OperationUtilsTest.groovy
@@ -0,0 +1,67 @@
+package it.fulminazzo.javaparser.typechecker
+
+import spock.lang.Specification
+
+import static it.fulminazzo.javaparser.typechecker.types.ValueType.*
+
+class OperationUtilsTest extends Specification {
+
+    def 'test execute binary operation (non-decimal): #a * #b -> #c'() {
+        expect:
+        OperationUtils.executeBinaryOperation(a, b) == c
+
+        where:
+        a      | b      | c
+        // Char
+        CHAR   | CHAR   | NUMBER
+        CHAR   | NUMBER | NUMBER
+        CHAR   | LONG   | LONG
+        // Number
+        NUMBER | CHAR   | NUMBER
+        NUMBER | NUMBER | NUMBER
+        NUMBER | LONG   | LONG
+        // Long
+        LONG   | CHAR   | LONG
+        LONG   | NUMBER | LONG
+        LONG   | LONG   | LONG
+    }
+
+    def 'test execute binary operation decimal: #a * #b -> #c'() {
+        expect:
+        OperationUtils.executeBinaryOperationDecimal(a, b) == c
+
+        where:
+        a      | b      | c
+        // Char
+        CHAR   | CHAR   | NUMBER
+        CHAR   | NUMBER | NUMBER
+        CHAR   | LONG   | LONG
+        CHAR   | FLOAT  | FLOAT
+        CHAR   | DOUBLE | DOUBLE
+        // Number
+        NUMBER | CHAR   | NUMBER
+        NUMBER | NUMBER | NUMBER
+        NUMBER | LONG   | LONG
+        NUMBER | FLOAT  | FLOAT
+        NUMBER | DOUBLE | DOUBLE
+        // Long
+        LONG   | CHAR   | LONG
+        LONG   | NUMBER | LONG
+        LONG   | LONG   | LONG
+        LONG   | FLOAT  | FLOAT
+        LONG   | DOUBLE | DOUBLE
+        // Double
+        DOUBLE | CHAR   | DOUBLE
+        DOUBLE | NUMBER | DOUBLE
+        DOUBLE | LONG   | DOUBLE
+        DOUBLE | FLOAT  | DOUBLE
+        DOUBLE | DOUBLE | DOUBLE
+        // Float
+        FLOAT  | CHAR   | FLOAT
+        FLOAT  | NUMBER | FLOAT
+        FLOAT  | LONG   | LONG
+        FLOAT  | FLOAT  | FLOAT
+        FLOAT  | DOUBLE | DOUBLE
+    }
+
+}
