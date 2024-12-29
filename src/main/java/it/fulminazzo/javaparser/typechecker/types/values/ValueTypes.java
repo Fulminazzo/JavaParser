b18521ed77af8d5a11803e1ef749f08443c598bd
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 16:04:54 +0100

    Added values and valueOf methods

--- src/main/java/it/fulminazzo/javaparser/typechecker/types/values/ValueTypes.java
+++ src/main/java/it/fulminazzo/javaparser/typechecker/types/values/ValueTypes.java
@@ -1,8 +1,10 @@
 package it.fulminazzo.javaparser.typechecker.types.values;
 
+import it.fulminazzo.javaparser.typechecker.types.EnumType;
 import it.fulminazzo.javaparser.typechecker.types.Type;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Contains all the {@link ValueType}s representing the values accepted by the language.
@@ -16,5 +18,25 @@ public final class ValueTypes {
     public static final Type LONG_TYPE = new LongValueType();
     public static final Type NUMBER_TYPE = new NumberValueType();
     public static final Type STRING_TYPE = new StringValueType();
+
+    /**
+     * Gets all the types in this class.
+     *
+     * @return the types
+     */
+    public static Type @NotNull [] values() {
+        return EnumType.values(ValueTypes.class, ValueType.class);
+    }
+
+    /**
+     * Gets the corresponding type from the given name.
+     * Throws {@link IllegalArgumentException} in case of error.
+     *
+     * @param name the name
+     * @return the type
+     */
+    public static @NotNull Type valueOf(final @NotNull String name) {
+        return EnumType.valueOf(ValueTypes.class, ValueType.class, name);
+    }
 
 }
