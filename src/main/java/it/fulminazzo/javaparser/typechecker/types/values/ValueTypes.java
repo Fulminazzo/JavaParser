c839879f633884e7bb6fa743c9571c327901de19
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 15:36:36 +0100

    Added all types parsed by the program

--- src/main/java/it/fulminazzo/javaparser/typechecker/types/values/ValueTypes.java
+++ src/main/java/it/fulminazzo/javaparser/typechecker/types/values/ValueTypes.java
@@ -1,5 +1,6 @@
 package it.fulminazzo.javaparser.typechecker.types.values;
 
+import it.fulminazzo.javaparser.typechecker.types.Type;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -8,5 +9,12 @@ import lombok.NoArgsConstructor;
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ValueTypes {
+    public static final Type BOOLEAN_TYPE = new BooleanValueType();
+    public static final Type CHAR_TYPE = new CharValueType();
+    public static final Type DOUBLE_TYPE = new DoubleValueType();
+    public static final Type FLOAT_TYPE = new FloatValueType();
+    public static final Type LONG_TYPE = new LongValueType();
+    public static final Type NUMBER_TYPE = new NumberValueType();
+    public static final Type STRING_TYPE = new StringValueType();
 
 }
