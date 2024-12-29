e3c66aa21ade1afeabfb992eae17a3395d01250e
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 19:35:07 +0100

    Added executeBinaryOperationDecimal function

--- src/main/java/it/fulminazzo/javaparser/typechecker/OperationUtils.java
+++ src/main/java/it/fulminazzo/javaparser/typechecker/OperationUtils.java
@@ -1,12 +1,33 @@
 package it.fulminazzo.javaparser.typechecker;
 
+import it.fulminazzo.javaparser.typechecker.types.Type;
+import it.fulminazzo.javaparser.typechecker.types.ValueType;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * A collection of utility functions for handling operations.
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class OperationUtils {
+
+    /**
+     * Computes the returned {@link Type} for a binary operation that supports <b>decimal</b> types.
+     *
+     * @param left  the left operand
+     * @param right the right operand
+     * @return the computed type
+     */
+    public static @NotNull Type executeBinaryOperationDecimal(final @NotNull Type left,
+                                                              final @NotNull Type right) {
+        if (left.equals(ValueType.DOUBLE) || right.equals(ValueType.DOUBLE))
+            return ValueType.DOUBLE;
+        else if (left.equals(ValueType.FLOAT) || right.equals(ValueType.FLOAT))
+            return ValueType.FLOAT;
+        else if (left.equals(ValueType.LONG) || right.equals(ValueType.LONG))
+            return ValueType.LONG;
+        else return ValueType.NUMBER;
+    }
 
 }
