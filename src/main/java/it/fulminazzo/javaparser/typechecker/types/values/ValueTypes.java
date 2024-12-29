d73cd4cde090320d4a6f5fa3f8e2ca0e5407d7ab
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 17:12:07 +0100

    Implemented Type interface

--- src/main/java/it/fulminazzo/javaparser/typechecker/types/values/ValueTypes.java
+++ src/main/java/it/fulminazzo/javaparser/typechecker/types/values/ValueTypes.java
@@ -1,9 +1,11 @@
 package it.fulminazzo.javaparser.typechecker.types.values;
 
+import it.fulminazzo.javaparser.typechecker.types.Type;
+
 /**
  * Contains all the {@link ValueType}s representing the values accepted by Java.
  */
-public enum ValueTypes {
+public enum ValueTypes implements Type {
     CHAR_TYPE,
     NUMBER_TYPE,
     LONG_TYPE,
