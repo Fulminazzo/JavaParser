471c0c46855f1337a565da9407773ff7e6ad7ec5
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 19:08:45 +0100

    Created TypeWrapper from CustomClassObjectType

---
+++ src/main/java/it/fulminazzo/javaparser/typechecker/types/TypeWrapper.java
@@ -0,0 +1,30 @@
+package it.fulminazzo.javaparser.typechecker.types;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Represents a wrapper for {@link Type} objects.
+ */
+public abstract class TypeWrapper {
+    protected final Type internalType;
+
+    /**
+     * Instantiates a new Type wrapper.
+     *
+     * @param internalType the internal type
+     */
+    public TypeWrapper(final @NotNull Type internalType) {
+        this.internalType = internalType;
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode() + this.internalType.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof TypeWrapper && this.internalType.equals(((TypeWrapper) o).internalType);
+    }
+
+}
