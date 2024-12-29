527683ba2aa4af7c41c7d62b2ea8f27f4255d85b
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 21:33:08 +0100

    Implemented visitEqual and visitNotEqual methods

--- src/main/java/it/fulminazzo/javaparser/typechecker/TypeChecker.java
+++ src/main/java/it/fulminazzo/javaparser/typechecker/TypeChecker.java
@@ -131,8 +131,7 @@ public final class TypeChecker implements Visitor<Type> {
 
     @Override
     public Type visitEqual(@NotNull Node left, @NotNull Node right) {
-        // TODO:
-        return null;
+        return ValueType.BOOLEAN;
     }
 
     @Override
@@ -185,8 +184,7 @@ public final class TypeChecker implements Visitor<Type> {
 
     @Override
     public Type visitNotEqual(@NotNull Node left, @NotNull Node right) {
-        // TODO:
-        return null;
+        return ValueType.BOOLEAN;
     }
 
     @Override
