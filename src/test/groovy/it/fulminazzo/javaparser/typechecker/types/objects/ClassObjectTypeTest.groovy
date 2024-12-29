e003f6f856cf54b8bf8fb66f9ae49feff8aba4cf
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 18:06:24 +0100

    Added tests for ClassObjectType

---
+++ src/test/groovy/it/fulminazzo/javaparser/typechecker/types/objects/ClassObjectTypeTest.groovy
@@ -0,0 +1,183 @@
+package it.fulminazzo.javaparser.typechecker.types.objects
+
+import spock.lang.Specification
+
+import static it.fulminazzo.javaparser.typechecker.types.ValueType.*
+
+class ClassObjectTypeTest extends Specification {
+
+    def 'test BYTE compatible with #type'() {
+        expect:
+        ClassObjectType.BYTE.compatibleWith(type)
+
+        where:
+        type << [
+                CHAR, NUMBER,
+        ]
+    }
+
+    def 'test BYTE incompatible with #type'() {
+        expect:
+        !ClassObjectType.BYTE.compatibleWith(type)
+
+        where:
+        type << [
+                LONG,
+                DOUBLE, FLOAT,
+                BOOLEAN, STRING
+        ]
+    }
+
+    def 'test CHARACTER compatible with #type'() {
+        expect:
+        ClassObjectType.CHARACTER.compatibleWith(type)
+
+        where:
+        type << [
+                CHAR, NUMBER
+        ]
+    }
+
+    def 'test CHARACTER incompatible with #type'() {
+        expect:
+        !ClassObjectType.CHARACTER.compatibleWith(type)
+
+        where:
+        type << [
+                LONG,
+                DOUBLE, FLOAT,
+                BOOLEAN, STRING
+        ]
+    }
+
+    def 'test SHORT compatible with #type'() {
+        expect:
+        ClassObjectType.SHORT.compatibleWith(type)
+
+        where:
+        type << [
+                CHAR, NUMBER,
+        ]
+    }
+
+    def 'test SHORT incompatible with #type'() {
+        expect:
+        !ClassObjectType.SHORT.compatibleWith(type)
+
+        where:
+        type << [
+                LONG,
+                DOUBLE, FLOAT,
+                BOOLEAN, STRING
+        ]
+    }
+
+    def 'test INTEGER compatible with #type'() {
+        expect:
+        ClassObjectType.INTEGER.compatibleWith(type)
+
+        where:
+        type << [
+                CHAR, NUMBER,
+        ]
+    }
+
+    def 'test INTEGER incompatible with #type'() {
+        expect:
+        !ClassObjectType.INTEGER.compatibleWith(type)
+
+        where:
+        type << [
+                LONG,
+                DOUBLE, FLOAT,
+                BOOLEAN, STRING
+        ]
+    }
+
+    def 'test LONG compatible with #type'() {
+        expect:
+        ClassObjectType.LONG.compatibleWith(type)
+
+        where:
+        type << [
+                CHAR, NUMBER, LONG,
+        ]
+    }
+
+    def 'test LONG incompatible with #type'() {
+        expect:
+        !ClassObjectType.LONG.compatibleWith(type)
+
+        where:
+        type << [
+                DOUBLE, FLOAT,
+                BOOLEAN, STRING
+        ]
+    }
+
+    def 'test FLOAT compatible with #type'() {
+        expect:
+        ClassObjectType.FLOAT.compatibleWith(type)
+
+        where:
+        type << [
+                CHAR, NUMBER, LONG,
+                FLOAT,
+        ]
+    }
+
+    def 'test FLOAT incompatible with #type'() {
+        expect:
+        !ClassObjectType.FLOAT.compatibleWith(type)
+
+        where:
+        type << [
+                DOUBLE,
+                BOOLEAN, STRING
+        ]
+    }
+
+    def 'test DOUBLE compatible with #type'() {
+        expect:
+        ClassObjectType.DOUBLE.compatibleWith(type)
+
+        where:
+        type << [
+                CHAR, NUMBER, LONG,
+                FLOAT, DOUBLE,
+        ]
+    }
+
+    def 'test DOUBLE incompatible with #type'() {
+        expect:
+        !ClassObjectType.DOUBLE.compatibleWith(type)
+
+        where:
+        type << [
+                BOOLEAN, STRING
+        ]
+    }
+
+    def 'test BOOLEAN compatible with #type'() {
+        expect:
+        ClassObjectType.BOOLEAN.compatibleWith(type)
+
+        where:
+        type << [
+                BOOLEAN
+        ]
+    }
+
+    def 'test BOOLEAN incompatible with #type'() {
+        expect:
+        !ClassObjectType.BOOLEAN.compatibleWith(type)
+
+        where:
+        type << [
+                CHAR, NUMBER, LONG,
+                DOUBLE, FLOAT,
+                STRING
+        ]
+    }
+    
+}
