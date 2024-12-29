8a688d386bf269c058d79ce386f9c6f99d45cd68
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 16:01:39 +0100

    Added implementation for name method

--- src/main/java/it/fulminazzo/javaparser/typechecker/types/values/ValueType.java
+++ src/main/java/it/fulminazzo/javaparser/typechecker/types/values/ValueType.java
@@ -3,11 +3,17 @@ package it.fulminazzo.javaparser.typechecker.types.values;
 import it.fulminazzo.javaparser.typechecker.types.EnumType;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Represents a literal value accepted by the language.
  */
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
 abstract class ValueType extends EnumType {
+
+    @Override
+    public @NotNull String name() {
+        return name(ValueTypes.class);
+    }
 
 }
