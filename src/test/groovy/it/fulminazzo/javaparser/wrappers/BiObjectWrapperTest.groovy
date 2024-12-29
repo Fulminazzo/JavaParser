54d8f1ec4f7914099de8a20133c5ca7be52bff44
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 20:38:23 +0100

    Added tests for BiObjectWrapper

---
+++ src/test/groovy/it/fulminazzo/javaparser/wrappers/BiObjectWrapperTest.groovy
@@ -0,0 +1,53 @@
+package it.fulminazzo.javaparser.wrappers
+
+import spock.lang.Specification
+
+class BiObjectWrapperTest extends Specification {
+    private String name
+    private int age
+    private BiObjectWrapper wrapper
+
+    void setup() {
+        this.name = 'Alex'
+        this.age = 22
+        this.wrapper = new MockWrapper(this.name, this.age)
+    }
+
+    def 'test hashCode'() {
+        expect:
+        this.wrapper.hashCode() == MockWrapper.hashCode() + this.name.hashCode() + this.age.hashCode()
+    }
+
+    def 'test equality'() {
+        expect:
+        this.wrapper == new MockWrapper(this.name, this.age)
+    }
+
+    def 'test inequality with #object'() {
+        expect:
+        !this.wrapper.equals(object)
+
+        where:
+        object << [
+                null,
+                new BiObjectWrapper<>(2.0, 10),
+                new MockWrapper('Steve', 22),
+                new MockWrapper('Alex', 23),
+                new MockWrapper('Steve', 21),
+        ]
+    }
+
+    def 'test toString'() {
+        expect:
+        this.wrapper.toString() == "${MockWrapper.class.simpleName}(${this.name})"
+    }
+
+    static class MockWrapper extends BiObjectWrapper<String, Integer> {
+
+        MockWrapper(String first, Integer second) {
+            super(first, second)
+        }
+
+    }
+
+}
