bbd3b395bca92bcf6116e6e4f6113c333ce3efa2
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 17:04:51 +0100

    Replaced PrimitiveTypes with enum and ClassType implementation

--- src/main/java/it/fulminazzo/javaparser/typechecker/types/primitives/PrimitiveTypes.java
+++ src/main/java/it/fulminazzo/javaparser/typechecker/types/primitives/PrimitiveTypes.java
@@ -1,41 +1,23 @@
 package it.fulminazzo.javaparser.typechecker.types.primitives;
 
-import it.fulminazzo.javaparser.typechecker.types.EnumType;
+import it.fulminazzo.javaparser.typechecker.types.ClassType;
 import it.fulminazzo.javaparser.typechecker.types.Type;
-import it.fulminazzo.javaparser.typechecker.types.values.ValueTypes;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class PrimitiveTypes {
-    public static final Type BYTE = new PrimitiveType<>(ValueTypes.NUMBER_TYPE);
-    public static final Type CHAR = new PrimitiveType<>(ValueTypes.CHAR_TYPE);
-    public static final Type SHORT = new PrimitiveType<>(ValueTypes.NUMBER_TYPE);
-    public static final Type INT = new PrimitiveType<>(ValueTypes.NUMBER_TYPE);
-    public static final Type LONG = new PrimitiveType<>(ValueTypes.LONG_TYPE);
-    public static final Type FLOAT = new PrimitiveType<>(ValueTypes.FLOAT_TYPE);
-    public static final Type DOUBLE = new PrimitiveType<>(ValueTypes.DOUBLE_TYPE);
-    public static final Type BOOLEAN = new PrimitiveType<>(ValueTypes.BOOLEAN_TYPE);
+public enum PrimitiveTypes implements ClassType {
+    ;
 
-    /**
-     * Gets all the types in this class.
-     *
-     * @return the types
-     */
-    public static Type @NotNull [] values() {
-        return EnumType.values(PrimitiveTypes.class, PrimitiveType.class);
+    private final Type @NotNull [] compatibleTypes;
+
+    PrimitiveTypes(final Type @NotNull ... compatibleTypes) {
+        this.compatibleTypes = compatibleTypes;
     }
 
-    /**
-     * Gets the corresponding type from the given name.
-     * Throws {@link IllegalArgumentException} in case of error.
-     *
-     * @param name the name
-     * @return the type
-     */
-    public static @NotNull Type valueOf(final @NotNull String name) {
-        return EnumType.valueOf(PrimitiveTypes.class, PrimitiveType.class, name);
+    @Override
+    public boolean compatibleWith(@NotNull Type type) {
+        for (Type compatibleType : this.compatibleTypes)
+            if (compatibleType.equals(type)) return true;
+        return false;
     }
 
 }
