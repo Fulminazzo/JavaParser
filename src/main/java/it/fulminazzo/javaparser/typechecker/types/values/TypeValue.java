1afff4e57b56d1c7c58b35cfcb3526a3f9d27f7e
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 15:29:53 +0100

    Moved TypeValue to values package and converted in final class

---
+++ src/main/java/it/fulminazzo/javaparser/typechecker/types/values/TypeValue.java
@@ -0,0 +1,8 @@
+package it.fulminazzo.javaparser.typechecker.types.values;
+
+import it.fulminazzo.javaparser.typechecker.types.Type;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public final class TypeValue implements Type {
+}
