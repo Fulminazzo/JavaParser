69e37d62558421d7a0cbd5982871df77ba50b7a0
Fulminazzo <fulminazzo21@gmail.com>
2024-12-29 17:53:09 +0100

    Added of and getClass methods

--- src/main/java/it/fulminazzo/javaparser/typechecker/types/ObjectType.java
+++ src/main/java/it/fulminazzo/javaparser/typechecker/types/ObjectType.java
@@ -1,5 +1,6 @@
 package it.fulminazzo.javaparser.typechecker.types;
 
+import it.fulminazzo.fulmicollection.utils.ReflectionUtils;
 import lombok.AccessLevel;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
@@ -40,6 +41,42 @@ public class ObjectType implements Type {
                 break;
             }
         return String.format("Type(%s)", className);
+    }
+
+    /**
+     * Obtains an instance of {@link ObjectType} from the given class name.
+     *
+     * @param className the class name
+     * @return the object type
+     * @throws TypeException the exception thrown in case the class is not found
+     */
+    public static @NotNull ObjectType of(final @NotNull String className) throws TypeException {
+        Class<?> clazz = getClass(className);
+        return new ObjectType(clazz);
+    }
+
+    /**
+     * Searches for a class matching the given class name.
+     * If no match is found, it tries to append the packages in
+     * {@link #IMPLIED_PACKAGES}.
+     * If still nothing is found, a {@link TypeException} is thrown.
+     *
+     * @param className the class name
+     * @return the class
+     * @throws TypeException the exception thrown in case the class is not found
+     */
+    static @NotNull Class<?> getClass(final @NotNull String className) throws TypeException {
+        try {
+            return ReflectionUtils.getClass(className);
+        } catch (IllegalArgumentException e) {
+            for (String impliedPackage : IMPLIED_PACKAGES) {
+                String name = impliedPackage + "." + className;
+                try {
+                    return ReflectionUtils.getClass(name);
+                } catch (IllegalArgumentException ignored) {}
+            }
+            throw new TypeException("Could not find class " + className);
+        }
     }
 
 }
