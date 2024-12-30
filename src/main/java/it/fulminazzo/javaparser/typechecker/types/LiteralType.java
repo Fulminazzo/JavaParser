da6134ed5b59e76579fc2ba4a54de8f1478ebc46
Fulminazzo <fulminazzo21@gmail.com>
2024-12-30 13:35:05 +0100

    Created LiteralType class

---
+++ src/main/java/it/fulminazzo/javaparser/typechecker/types/LiteralType.java
@@ -0,0 +1,37 @@
+package it.fulminazzo.javaparser.typechecker.types;
+
+import it.fulminazzo.javaparser.typechecker.TypeCheckerException;
+import it.fulminazzo.javaparser.wrappers.ObjectWrapper;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+/**
+ * Represents the default conversion for {@link it.fulminazzo.javaparser.tokenizer.TokenType#LITERAL}.
+ */
+public class LiteralType extends ObjectWrapper<String> implements Type {
+
+    /**
+     * Instantiates a new Literal type.
+     *
+     * @param literal the literal
+     */
+    public LiteralType(final @NotNull String literal) {
+        super(Objects.requireNonNull(literal, "Expected literal to be not null"));
+    }
+
+    /**
+     * Gets the inner literal.
+     *
+     * @return the literal
+     */
+    public @NotNull String getLiteral() {
+        return this.object;
+    }
+
+    @Override
+    public @NotNull ClassType toClassType() {
+        throw TypeCheckerException.literalConversion();
+    }
+
+}
