b5bc535733b6e552bf9aec07026b7fa3fc8e5121
Fulminazzo <fulminazzo21@gmail.com>
2024-12-30 12:30:36 +0100

    Added tests with classType

--- src/test/groovy/it/fulminazzo/javaparser/typechecker/types/TypeTest.groovy
+++ src/test/groovy/it/fulminazzo/javaparser/typechecker/types/TypeTest.groovy
@@ -1,13 +1,16 @@
 package it.fulminazzo.javaparser.typechecker.types
 
+import it.fulminazzo.javaparser.typechecker.types.objects.ClassObjectType
 import it.fulminazzo.javaparser.typechecker.types.objects.ObjectType
 import spock.lang.Specification
 
 class TypeTest extends Specification {
     private Type type
+    private ClassType classType
 
     void setup() {
         type = ObjectType.of(TestClass)
+        classType = ClassObjectType.of(TestClass)
     }
 
     def 'test valid getField #field'() {
@@ -42,6 +45,43 @@ class TypeTest extends Specification {
     def 'test field not found'() {
         when:
         this.type.getField('invalid')
+
+        then:
+        def e = thrown(TypeException)
+        e.message == TypeException.fieldNotFound('invalid').message
+    }
+
+    def 'test class valid getField #field'() {
+        when:
+        def tuple = this.classType.getField(field)
+
+        then:
+        tuple.getKey() == expectedType
+        tuple.getValue() == expected
+
+        where:
+        field               | expectedType         | expected
+        'publicField'       | PrimitiveType.DOUBLE | ValueType.DOUBLE
+    }
+
+    def 'test class cannot access field #field from getField'() {
+        when:
+        this.classType.getField(field)
+
+        then:
+        def e = thrown(TypeException)
+        e.message == TypeException.cannotAccessField(TestClass.getDeclaredField(field)).message
+
+        where:
+        field << [
+                'packageStaticField','protectedStaticField','privateStaticField',
+                'packageField','protectedField','privateField'
+        ]
+    }
+
+    def 'test class field not found'() {
+        when:
+        this.classType.getField('invalid')
 
         then:
         def e = thrown(TypeException)
