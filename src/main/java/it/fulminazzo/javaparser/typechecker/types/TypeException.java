e7c11c52d3e9ab866927849dd9eec9b49d0604c5
Fulminazzo <fulminazzo21@gmail.com>
2024-12-30 12:20:27 +0100

    Added cannotAccessField function

--- src/main/java/it/fulminazzo/javaparser/typechecker/types/TypeException.java
+++ src/main/java/it/fulminazzo/javaparser/typechecker/types/TypeException.java
@@ -2,6 +2,8 @@ package it.fulminazzo.javaparser.typechecker.types;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.reflect.Field;
+
 /**
  * An exception thrown by {@link Type} objects.
  */
@@ -36,6 +38,18 @@ public class TypeException extends Exception {
      */
     public static TypeException fieldNotFound(final @NotNull String field) {
         return new TypeException("Could not find field: " + field);
+    }
+
+    /**
+     * Generates a {@link TypeException} with message:
+     * <i>Cannot access field %field% with access-level: %access-level%</i>
+     *
+     * @param field the field
+     * @return the type exception
+     */
+    public static TypeException cannotAccessField(final @NotNull Field field) {
+        return new TypeException(String.format("Cannot access field %s with access level: %s",
+                field.getName(), field.getModifiers()));
     }
 
 }
