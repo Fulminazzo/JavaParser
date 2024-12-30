ea09fcccf0645018d654f01fe0948d128a974a09
Fulminazzo <fulminazzo21@gmail.com>
2024-12-30 17:04:23 +0100

    Implemented Type interface

--- src/main/java/it/fulminazzo/javaparser/typechecker/types/NoType.java
+++ src/main/java/it/fulminazzo/javaparser/typechecker/types/NoType.java
@@ -1,14 +1,21 @@
 package it.fulminazzo.javaparser.typechecker.types;
 
+import it.fulminazzo.javaparser.typechecker.TypeCheckerException;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Represents an absence of {@link Type}.
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class NoType {
+public final class NoType implements Type {
     public static final NoType NO_TYPE = new NoType();
+
+    @Override
+    public @NotNull ClassType toClassType() {
+        throw TypeCheckerException.noClassType(getClass());
+    }
 
     @Override
     public int hashCode() {
