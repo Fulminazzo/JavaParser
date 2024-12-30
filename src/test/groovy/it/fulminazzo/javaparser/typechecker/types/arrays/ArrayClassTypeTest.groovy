7c77384f21bfc4216001e54a448978a432d04375
Fulminazzo <fulminazzo21@gmail.com>
2024-12-30 22:27:02 +0100

    Added tests for cast method

--- src/test/groovy/it/fulminazzo/javaparser/typechecker/types/arrays/ArrayClassTypeTest.groovy
+++ src/test/groovy/it/fulminazzo/javaparser/typechecker/types/arrays/ArrayClassTypeTest.groovy
@@ -3,9 +3,39 @@ package it.fulminazzo.javaparser.typechecker.types.arrays
 import it.fulminazzo.javaparser.typechecker.types.ClassType
 import it.fulminazzo.javaparser.typechecker.types.PrimitiveType
 import it.fulminazzo.javaparser.typechecker.types.ValueType
+import it.fulminazzo.javaparser.typechecker.types.objects.ClassObjectType
+import it.fulminazzo.javaparser.typechecker.types.objects.ObjectType
 import spock.lang.Specification
 
 class ArrayClassTypeTest extends Specification {
+
+    def 'test cast of #cast to #type should return #cast'() {
+        when:
+        def actual = cast.cast(type)
+
+        then:
+        actual == cast.toType()
+
+        where:
+        cast << [
+                PrimitiveType.values().collect { new ArrayClassType(it) },
+                ClassObjectType.values().collect { new ArrayClassType(it) },
+                new ArrayClassType(ClassObjectType.of('Map')),
+                new ArrayClassType(ClassObjectType.of('Collection')),
+                new ArrayClassType(ClassObjectType.of('List'))
+        ].flatten()
+        type << [
+                [
+                        ValueType.BYTE, ValueType.SHORT, ValueType.CHAR,
+                        ValueType.NUMBER, ValueType.LONG, ValueType.FLOAT,
+                        ValueType.DOUBLE, ValueType.BOOLEAN
+                ].collect { new ArrayType(it) },
+                ObjectType.values().collect { new ArrayType(it) },
+                new ArrayType(ObjectType.of('List')),
+                new ArrayType(ObjectType.of('List')),
+                new ArrayType(ObjectType.of('List'))
+        ].flatten()
+    }
 
     def 'test conversion of types'() {
         given:
