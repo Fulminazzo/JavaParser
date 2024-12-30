cc50d5dc7be1787d2453009415e4714e9e546c0b
Fulminazzo <fulminazzo21@gmail.com>
2024-12-30 12:18:32 +0100

    Made constructor private and added classNotFound function

--- src/main/java/it/fulminazzo/javaparser/typechecker/types/TypeException.java
+++ src/main/java/it/fulminazzo/javaparser/typechecker/types/TypeException.java
@@ -12,8 +12,19 @@ public class TypeException extends Exception {
      *
      * @param message the message
      */
-    public TypeException(final @NotNull String message) {
+    private TypeException(final @NotNull String message) {
         super(message);
+    }
+
+    /**
+     * Generates a {@link TypeException} with message:
+     * <i>Could not find class %clazz%</i>
+     *
+     * @param clazz the clazz
+     * @return the type exception
+     */
+    public static TypeException classNotFound(final @NotNull String clazz) {
+        return new TypeException("Could not find class: " + clazz);
     }
 
 }
