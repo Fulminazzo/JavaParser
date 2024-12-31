cb0a604f5e473680d6b4c384b5d7bd3fd01d90e2
Fulminazzo <fulminazzo21@gmail.com>
2024-12-31 16:49:55 +0100

    Added exception message in case of invalid value

--- src/main/java/it/fulminazzo/javaparser/parser/node/TokenizedNode.java
+++ src/main/java/it/fulminazzo/javaparser/parser/node/TokenizedNode.java
@@ -20,7 +20,9 @@ public abstract class TokenizedNode extends NodeImpl {
      */
     public TokenizedNode(final @NotNull String rawValue,
                          final @NotNull TokenType type) throws NodeException {
-        if (!type.matches(rawValue)) throw new NodeException();
+        if (!type.matches(rawValue))
+            throw new NodeException(String.format("Invalid value %s for token %s(%s)",
+                    rawValue, type, type.regex()));
     }
 
 }
