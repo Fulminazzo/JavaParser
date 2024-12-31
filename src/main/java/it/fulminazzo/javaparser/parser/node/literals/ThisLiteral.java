640a7a32355cc06b370fc6b50b2a087177b9b6f0
Fulminazzo <fulminazzo21@gmail.com>
2025-01-01 00:33:16 +0100

    Created ThisLiteral class

---
+++ src/main/java/it/fulminazzo/javaparser/parser/node/literals/ThisLiteral.java
@@ -0,0 +1,11 @@
+package it.fulminazzo.javaparser.parser.node.literals;
+
+import it.fulminazzo.javaparser.parser.node.NodeImpl;
+import lombok.NoArgsConstructor;
+
+/**
+ * Represents the <code>this</code> keyword.
+ */
+@NoArgsConstructor
+public class ThisLiteral extends NodeImpl {
+}
