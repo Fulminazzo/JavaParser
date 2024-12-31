a36470ec54f17e762e79a211bb8dee76c450fa13
Fulminazzo <fulminazzo21@gmail.com>
2024-12-31 18:45:45 +0100

    Refactored field name

--- src/main/java/it/fulminazzo/javaparser/parser/node/statements/IfStatement.java
+++ src/main/java/it/fulminazzo/javaparser/parser/node/statements/IfStatement.java
@@ -8,21 +8,21 @@ import org.jetbrains.annotations.NotNull;
  * Represents an if statement.
  */
 public class IfStatement extends Statement {
-    private final @NotNull CodeBlock code;
+    private final @NotNull CodeBlock then;
     private final @NotNull Node elseBranch;
 
     /**
      * Instantiates a new If statement.
      *
      * @param condition  the condition
-     * @param code       the code that will be executed
+     * @param then       the code that will be executed
      * @param elseBranch the alternative code
      */
     public IfStatement(final @NotNull Node condition,
-                       final @NotNull CodeBlock code,
+                       final @NotNull CodeBlock then,
                        final @NotNull Node elseBranch) {
         super(condition);
-        this.code = code;
+        this.then = then;
         this.elseBranch = elseBranch;
     }
 
