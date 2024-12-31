d85c1e90bfb34becd39bacdd6f51f522500f746b
Fulminazzo <fulminazzo21@gmail.com>
2025-01-01 00:51:32 +0100

    Added tests for ArrayLiteral

---
+++ src/test/groovy/it/fulminazzo/javaparser/parser/node/literals/ArrayLiteralTest.groovy
@@ -0,0 +1,18 @@
+package it.fulminazzo.javaparser.parser.node.literals
+
+import spock.lang.Specification
+
+class ArrayLiteralTest extends Specification {
+
+    def 'test getLiteral should throw exception'() {
+        given:
+        def literal = new ArrayLiteral(Literal.of('a'))
+
+        when:
+        literal.getLiteral()
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+}
