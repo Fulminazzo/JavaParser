0c331ade23cd84b562ad9d8ea7078210e4da38cf
Fulminazzo <fulminazzo21@gmail.com>
2024-12-31 20:29:48 +0100

    Added constructor for test purposes

--- src/test/java/it/fulminazzo/javaparser/typechecker/types/TestClass.java
+++ src/test/java/it/fulminazzo/javaparser/typechecker/types/TestClass.java
@@ -7,8 +7,58 @@ public class TestClass {
     protected static int protectedStaticField = 1;
     private static int privateStaticField = 1;
 
-    public int publicField = 1;
-    int packageField = 1;
-    protected int protectedField = 1;
-    private int privateField = 1;
+    public double publicField = 1.0;
+    double packageField = 1.0;
+    protected double protectedField = 1.0;
+    private double privateField = 1.0;
+
+    public TestClass() {}
+
+    public TestClass(int i, Boolean b) {}
+
+    TestClass(boolean b) {}
+
+    private TestClass(float f) {}
+
+    public static int publicStaticMethod() {
+        return 1;
+    }
+
+    public static int publicStaticMethod(int n, Boolean b) {
+        return b ? n : 1;
+    }
+
+    static int packageStaticMethod() {
+        return 1;
+    }
+
+    protected static int protectedStaticMethod() {
+        return 1;
+    }
+
+    private static int privateStaticMethod() {
+        return 1;
+    }
+
+
+    public double publicMethod() {
+        return 1.0;
+    }
+
+    public double publicMethod(double n, Boolean b) {
+        return b ? n : 1.0;
+    }
+
+    double packageMethod() {
+        return 1.0;
+    }
+
+    protected double protectedMethod() {
+        return 1.0;
+    }
+
+    private double privateMethod() {
+        return 1.0;
+    }
+
 }
