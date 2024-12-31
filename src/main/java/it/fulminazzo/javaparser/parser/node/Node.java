a54e2aac5429592d3336ac9c8d5ba196ed23be3d
Fulminazzo <fulminazzo21@gmail.com>
2024-12-31 22:21:45 +0100

    Made Node Serializable

--- src/main/java/it/fulminazzo/javaparser/parser/node/Node.java
+++ src/main/java/it/fulminazzo/javaparser/parser/node/Node.java
@@ -2,10 +2,12 @@ package it.fulminazzo.javaparser.parser.node;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.Serializable;
+
 /**
  * Represents a general node of the parser.
  */
-public interface Node {
+public interface Node extends Serializable {
 
     /**
      * Checks whether the current node is of the specified type.
