237099104c5e5e29d1ced26624ae942d1a1ed5f1
Fulminazzo <fulminazzo21@gmail.com>
2024-12-31 22:04:51 +0100

    Added Javadoc

--- src/main/java/it/fulminazzo/javaparser/typechecker/types/Types.java
+++ src/main/java/it/fulminazzo/javaparser/typechecker/types/Types.java
@@ -2,8 +2,13 @@ package it.fulminazzo.javaparser.typechecker.types;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 
+/**
+ * A collection of static immutable {@link Type}s.
+ */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class Types {
-    public static final NoType NO_TYPE = new NoType();
+    public static final @NotNull NoType NO_TYPE = new NoType();
+
 }
