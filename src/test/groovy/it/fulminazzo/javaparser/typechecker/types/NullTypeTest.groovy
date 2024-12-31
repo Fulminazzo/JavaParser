ce74ccea438665f50b19c4db689d1e2e9f1f9d8c
Fulminazzo <fulminazzo21@gmail.com>
2024-12-31 22:06:24 +0100

    Added tests for NullType

---
+++ src/test/groovy/it/fulminazzo/javaparser/typechecker/types/NullTypeTest.groovy
@@ -0,0 +1,56 @@
+package it.fulminazzo.javaparser.typechecker.types
+
+import it.fulminazzo.javaparser.typechecker.TypeCheckerException
+import spock.lang.Specification
+
+class NullTypeTest extends Specification {
+    private NullType type
+
+    void setup() {
+        this.type = Types.NULL_TYPE
+    }
+
+    def 'test toClassType'() {
+        when:
+        this.type.toClassType()
+
+        then:
+        def e = thrown(TypeCheckerException)
+        e.message == TypeCheckerException.noClassType(NullType).message
+    }
+
+    def 'test hashCode'() {
+        given:
+        def code = this.type.hashCode()
+
+        expect:
+        code == NullType.hashCode()
+    }
+
+    def 'test equality'() {
+        given:
+        def second = Types.NULL_TYPE
+
+        expect:
+        this.type == second
+    }
+
+    def 'test inequality of #other'() {
+        expect:
+        !this.type.equals(other)
+
+        where:
+        other << [PrimitiveType.BOOLEAN, ValueType.STRING, null, 'NULL_TYPE']
+    }
+
+    def 'test toString'() {
+        given:
+        def string = this.type.toString()
+
+        expect:
+        string == 'NULL_TYPE'
+    }
+
+
+
+}
