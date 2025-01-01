3e788f28d1209a5fe9794d55e003ab4f64137d92
Fulminazzo <fulminazzo21@gmail.com>
2025-01-01 01:05:33 +0100

    Added getStatements method

--- src/main/java/it/fulminazzo/javaparser/parser/node/container/StatementContainer.java
+++ src/main/java/it/fulminazzo/javaparser/parser/node/container/StatementContainer.java
@@ -2,6 +2,7 @@ package it.fulminazzo.javaparser.parser.node.container;
 
 import it.fulminazzo.javaparser.parser.node.NodeImpl;
 import it.fulminazzo.javaparser.parser.node.statements.Statement;
+import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.LinkedList;
@@ -9,6 +10,7 @@ import java.util.LinkedList;
 /**
  * Represents a general class that contains one or more statements.
  */
+@Getter
 abstract class StatementContainer extends NodeImpl {
     protected final @NotNull LinkedList<Statement> statements;
 
