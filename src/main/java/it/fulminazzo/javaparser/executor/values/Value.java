2ab6767a3a16a32101d13379aa1687e5cbd83607
Fulminazzo <fulminazzo21@gmail.com>
2025-01-02 00:02:42 +0100

    Added several binary operations and comparison default methods

--- src/main/java/it/fulminazzo/javaparser/executor/values/Value.java
+++ src/main/java/it/fulminazzo/javaparser/executor/values/Value.java
@@ -1,7 +1,100 @@
 package it.fulminazzo.javaparser.executor.values;
 
+import it.fulminazzo.javaparser.executor.values.primitive.BooleanValue;
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Represents a general value.
  */
 public interface Value {
+
+    /*
+        BINARY COMPARISONS
+     */
+
+    default @NotNull BooleanValue and(final @NotNull Value other) {
+        throw new UnsupportedOperationException();
+    }
+
+    default @NotNull BooleanValue or(final @NotNull Value other) {
+        throw new UnsupportedOperationException();
+    }
+
+    default @NotNull BooleanValue equal(final @NotNull Value other) {
+        throw new UnsupportedOperationException();
+    }
+
+    default @NotNull BooleanValue notEqual(final @NotNull Value other) {
+        throw new UnsupportedOperationException();
+    }
+
+    default @NotNull BooleanValue lessThan(final @NotNull Value other) {
+        throw new UnsupportedOperationException();
+    }
+
+    default @NotNull BooleanValue lessThanEqual(final @NotNull Value other) {
+        throw new UnsupportedOperationException();
+    }
+
+    default @NotNull BooleanValue greaterThan(final @NotNull Value other) {
+        throw new UnsupportedOperationException();
+    }
+
+    default @NotNull BooleanValue greaterThanEqual(final @NotNull Value other) {
+        throw new UnsupportedOperationException();
+    }
+
+    /*
+        BINARY OPERATIONS
+     */
+
+    default @NotNull Value bitAnd(final @NotNull Value other) {
+        throw new UnsupportedOperationException();
+    }
+
+    default @NotNull Value bitOr(final @NotNull Value other) {
+        throw new UnsupportedOperationException();
+    }
+
+    default @NotNull Value bitXor(final @NotNull Value other) {
+        throw new UnsupportedOperationException();
+    }
+
+    default @NotNull Value lshift(final @NotNull Value other) {
+        throw new UnsupportedOperationException();
+    }
+
+    default @NotNull Value rshift(final @NotNull Value other) {
+        throw new UnsupportedOperationException();
+    }
+
+    default @NotNull Value urshift(final @NotNull Value other) {
+        throw new UnsupportedOperationException();
+    }
+
+
+    default @NotNull Value add(final @NotNull Value other) {
+        throw new UnsupportedOperationException();
+    }
+
+    default @NotNull Value subtract(final @NotNull Value other) {
+        throw new UnsupportedOperationException();
+    }
+
+    default @NotNull Value multiply(final @NotNull Value other) {
+        throw new UnsupportedOperationException();
+    }
+
+    default @NotNull Value divide(final @NotNull Value other) {
+        throw new UnsupportedOperationException();
+    }
+
+    default @NotNull Value modulo(final @NotNull Value other) {
+        throw new UnsupportedOperationException();
+    }
+
+    default @NotNull BooleanValue not(final @NotNull Value other) {
+        throw new UnsupportedOperationException();
+    }
+    
 }
