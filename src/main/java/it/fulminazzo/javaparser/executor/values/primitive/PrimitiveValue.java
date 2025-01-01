771ccb4dbd04e2792b4e9dc98ba96ef3013cd4d1
Fulminazzo <fulminazzo21@gmail.com>
2025-01-02 00:16:23 +0100

    Made PrimitiveValue public, constructor package private and added of function

--- src/main/java/it/fulminazzo/javaparser/executor/values/primitive/PrimitiveValue.java
+++ src/main/java/it/fulminazzo/javaparser/executor/values/primitive/PrimitiveValue.java
@@ -1,6 +1,7 @@
 package it.fulminazzo.javaparser.executor.values.primitive;
 
 import it.fulminazzo.javaparser.executor.values.Value;
+import it.fulminazzo.javaparser.executor.values.ValueException;
 import it.fulminazzo.javaparser.wrappers.ObjectWrapper;
 import org.jetbrains.annotations.NotNull;
 
@@ -9,14 +10,14 @@ import org.jetbrains.annotations.NotNull;
  *
  * @param <V> the type of the value
  */
-abstract class PrimitiveValue<V> extends ObjectWrapper<V> implements Value {
+public abstract class PrimitiveValue<V> extends ObjectWrapper<V> implements Value {
 
     /**
      * Instantiates a new Primitive value.
      *
      * @param value the value
      */
-    public PrimitiveValue(@NotNull V value) {
+    PrimitiveValue(@NotNull V value) {
         super(value);
     }
 
@@ -27,6 +28,28 @@ abstract class PrimitiveValue<V> extends ObjectWrapper<V> implements Value {
      */
     public @NotNull V getValue() {
         return this.object;
+    }
+
+    /**
+     * Gets the most appropriate {@link PrimitiveValue} from the given value.
+     *
+     * @param <V>   the type of the value
+     * @param value the value
+     * @return the primitive value
+     */
+    @SuppressWarnings("unchecked")
+    public static @NotNull <V> PrimitiveValue<V> of(@NotNull V value) {
+        Value primitiveValue;
+        if (value instanceof Double) primitiveValue = new DoubleValue((Double) value);
+        else if (value instanceof Float) primitiveValue = new FloatValue((Float) value);
+        else if (value instanceof Long) primitiveValue = new LongValue((Long) value);
+        else if (value instanceof Boolean) primitiveValue = new BooleanValue((Boolean) value);
+        else if (value instanceof String) primitiveValue = new StringValue((String) value);
+        else if (value instanceof Character) primitiveValue = new CharacterValue((Character) value);
+        else if (value instanceof Byte || value instanceof Short || value instanceof Integer)
+            primitiveValue = new IntegerValue(Integer.parseInt(value.toString()));
+        else throw ValueException.invalidPrimitiveValue(value);
+        return (PrimitiveValue<V>) primitiveValue;
     }
 
 }
