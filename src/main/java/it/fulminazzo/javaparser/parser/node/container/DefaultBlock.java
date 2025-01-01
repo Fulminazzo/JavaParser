bbcf97eb05d661b85eab0ff1276d093ec17d7ef1
Fulminazzo <fulminazzo21@gmail.com>
2025-01-01 01:03:16 +0100

    Created DefaultBlock class

---
+++ src/main/java/it/fulminazzo/javaparser/parser/node/container/DefaultBlock.java
@@ -0,0 +1,22 @@
+package it.fulminazzo.javaparser.parser.node.container;
+
+import it.fulminazzo.javaparser.parser.node.statements.Statement;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.LinkedList;
+
+/**
+ * Represents the code block associated with {@link it.fulminazzo.javaparser.tokenizer.TokenType#DEFAULT}.
+ */
+public class DefaultBlock extends StatementContainer {
+
+    /**
+     * Instantiates a new Default block.
+     *
+     * @param statements the statements
+     */
+    public DefaultBlock(final @NotNull LinkedList<Statement> statements) {
+        super(statements);
+    }
+
+}
