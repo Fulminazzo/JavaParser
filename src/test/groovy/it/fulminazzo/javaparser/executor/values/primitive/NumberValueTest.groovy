f50c9e336e656a71d6caa27ec98124e3cbff69e2
Fulminazzo <fulminazzo21@gmail.com>
2025-01-02 03:22:31 +0100

    Refactored code

--- src/test/groovy/it/fulminazzo/javaparser/executor/values/primitive/NumberValueTest.groovy
+++ src/test/groovy/it/fulminazzo/javaparser/executor/values/primitive/NumberValueTest.groovy
@@ -427,25 +427,25 @@ class NumberValueTest extends Specification {
         eval == third
 
         where:
-        first                | second                           | third
+        first                 | second                          | third
         // Integer
-        new IntegerValue(4)  | new CharacterValue('a' as char)  | new IntegerValue((4 + INT_CHAR) as Integer)
-        new IntegerValue(4)  | new IntegerValue(2)              | new IntegerValue((4 + 2) as Integer)
-        new IntegerValue(4)  | new LongValue(2L)                | new LongValue((4 + 2L) as Long)
-        new IntegerValue(4)  | new FloatValue(2.0f)             | new FloatValue((4 + 2.0f) as Float)
-        new IntegerValue(4)  | new DoubleValue(2.0d)            | new DoubleValue((4 + 2.0d) as Double)
+        new IntegerValue(4)   | new CharacterValue('a' as char) | new IntegerValue((4 + INT_CHAR) as Integer)
+        new IntegerValue(4)   | new IntegerValue(2)             | new IntegerValue((4 + 2) as Integer)
+        new IntegerValue(4)   | new LongValue(2L)               | new LongValue((4 + 2L) as Long)
+        new IntegerValue(4)   | new FloatValue(2.0f)            | new FloatValue((4 + 2.0f) as Float)
+        new IntegerValue(4)   | new DoubleValue(2.0d)           | new DoubleValue((4 + 2.0d) as Double)
         // Long
-        new LongValue(4L)    | new CharacterValue('a' as char)  | new LongValue((4L + INT_CHAR) as Long)
-        new LongValue(4L)    | new IntegerValue(2)              | new LongValue((4L + 2) as Long)
-        new LongValue(4L)    | new LongValue(2L)                | new LongValue((4L + 2L) as Long)
-        new LongValue(4L)    | new FloatValue(2.0f)             | new FloatValue((4L + 2.0f) as Float)
-        new LongValue(4L)    | new DoubleValue(2.0d)            | new DoubleValue((4L + 2.0d) as Double)
+        new LongValue(4L)     | new CharacterValue('a' as char) | new LongValue((4L + INT_CHAR) as Long)
+        new LongValue(4L)     | new IntegerValue(2)             | new LongValue((4L + 2) as Long)
+        new LongValue(4L)     | new LongValue(2L)               | new LongValue((4L + 2L) as Long)
+        new LongValue(4L)     | new FloatValue(2.0f)            | new FloatValue((4L + 2.0f) as Float)
+        new LongValue(4L)     | new DoubleValue(2.0d)           | new DoubleValue((4L + 2.0d) as Double)
         // Float
-        new FloatValue(4.0f) | new CharacterValue('a' as char)  | new FloatValue((4.0f + INT_CHAR) as Float)
-        new FloatValue(4.0f) | new IntegerValue(2)              | new FloatValue((4.0f + 2) as Float)
-        new FloatValue(4.0f) | new LongValue(2L)                | new FloatValue((4.0f + 2L) as Float)
-        new FloatValue(4.0f) | new FloatValue(2.0f)             | new FloatValue((4.0f + 2.0f) as Float)
-        new FloatValue(4.0f) | new DoubleValue(2.0d)            | new DoubleValue((4.0f + 2.0d) as Double)
+        new FloatValue(4.0f)  | new CharacterValue('a' as char) | new FloatValue((4.0f + INT_CHAR) as Float)
+        new FloatValue(4.0f)  | new IntegerValue(2)             | new FloatValue((4.0f + 2) as Float)
+        new FloatValue(4.0f)  | new LongValue(2L)               | new FloatValue((4.0f + 2L) as Float)
+        new FloatValue(4.0f)  | new FloatValue(2.0f)            | new FloatValue((4.0f + 2.0f) as Float)
+        new FloatValue(4.0f)  | new DoubleValue(2.0d)           | new DoubleValue((4.0f + 2.0d) as Double)
         // Double
         new DoubleValue(4.0d) | new CharacterValue('a' as char) | new DoubleValue((4.0d + INT_CHAR) as Double)
         new DoubleValue(4.0d) | new IntegerValue(2)             | new DoubleValue((4.0d + 2) as Double)
@@ -462,25 +462,25 @@ class NumberValueTest extends Specification {
         eval == third
 
         where:
-        first                | second                           | third
+        first                 | second                          | third
         // Integer
-        new IntegerValue(4)  | new CharacterValue('a' as char)  | new IntegerValue((4 - INT_CHAR) as Integer)
-        new IntegerValue(4)  | new IntegerValue(2)              | new IntegerValue((4 - 2) as Integer)
-        new IntegerValue(4)  | new LongValue(2L)                | new LongValue((4 - 2L) as Long)
-        new IntegerValue(4)  | new FloatValue(2.0f)             | new FloatValue((4 - 2.0f) as Float)
-        new IntegerValue(4)  | new DoubleValue(2.0d)            | new DoubleValue((4 - 2.0d) as Double)
+        new IntegerValue(4)   | new CharacterValue('a' as char) | new IntegerValue((4 - INT_CHAR) as Integer)
+        new IntegerValue(4)   | new IntegerValue(2)             | new IntegerValue((4 - 2) as Integer)
+        new IntegerValue(4)   | new LongValue(2L)               | new LongValue((4 - 2L) as Long)
+        new IntegerValue(4)   | new FloatValue(2.0f)            | new FloatValue((4 - 2.0f) as Float)
+        new IntegerValue(4)   | new DoubleValue(2.0d)           | new DoubleValue((4 - 2.0d) as Double)
         // Long
-        new LongValue(4L)    | new CharacterValue('a' as char)  | new LongValue((4L - INT_CHAR) as Long)
-        new LongValue(4L)    | new IntegerValue(2)              | new LongValue((4L - 2) as Long)
-        new LongValue(4L)    | new LongValue(2L)                | new LongValue((4L - 2L) as Long)
-        new LongValue(4L)    | new FloatValue(2.0f)             | new FloatValue((4L - 2.0f) as Float)
-        new LongValue(4L)    | new DoubleValue(2.0d)            | new DoubleValue((4L - 2.0d) as Double)
+        new LongValue(4L)     | new CharacterValue('a' as char) | new LongValue((4L - INT_CHAR) as Long)
+        new LongValue(4L)     | new IntegerValue(2)             | new LongValue((4L - 2) as Long)
+        new LongValue(4L)     | new LongValue(2L)               | new LongValue((4L - 2L) as Long)
+        new LongValue(4L)     | new FloatValue(2.0f)            | new FloatValue((4L - 2.0f) as Float)
+        new LongValue(4L)     | new DoubleValue(2.0d)           | new DoubleValue((4L - 2.0d) as Double)
         // Float
-        new FloatValue(4.0f) | new CharacterValue('a' as char)  | new FloatValue((4.0f - INT_CHAR) as Float)
-        new FloatValue(4.0f) | new IntegerValue(2)              | new FloatValue((4.0f - 2) as Float)
-        new FloatValue(4.0f) | new LongValue(2L)                | new FloatValue((4.0f - 2L) as Float)
-        new FloatValue(4.0f) | new FloatValue(2.0f)             | new FloatValue((4.0f - 2.0f) as Float)
-        new FloatValue(4.0f) | new DoubleValue(2.0d)            | new DoubleValue((4.0f - 2.0d) as Double)
+        new FloatValue(4.0f)  | new CharacterValue('a' as char) | new FloatValue((4.0f - INT_CHAR) as Float)
+        new FloatValue(4.0f)  | new IntegerValue(2)             | new FloatValue((4.0f - 2) as Float)
+        new FloatValue(4.0f)  | new LongValue(2L)               | new FloatValue((4.0f - 2L) as Float)
+        new FloatValue(4.0f)  | new FloatValue(2.0f)            | new FloatValue((4.0f - 2.0f) as Float)
+        new FloatValue(4.0f)  | new DoubleValue(2.0d)           | new DoubleValue((4.0f - 2.0d) as Double)
         // Double
         new DoubleValue(4.0d) | new CharacterValue('a' as char) | new DoubleValue((4.0d - INT_CHAR) as Double)
         new DoubleValue(4.0d) | new IntegerValue(2)             | new DoubleValue((4.0d - 2) as Double)
@@ -497,25 +497,25 @@ class NumberValueTest extends Specification {
         eval == third
 
         where:
-        first                | second                           | third
+        first                 | second                          | third
         // Integer
-        new IntegerValue(4)  | new CharacterValue('a' as char)  | new IntegerValue((4 * INT_CHAR) as Integer)
-        new IntegerValue(4)  | new IntegerValue(2)              | new IntegerValue((4 * 2) as Integer)
-        new IntegerValue(4)  | new LongValue(2L)                | new LongValue((4 * 2L) as Long)
-        new IntegerValue(4)  | new FloatValue(2.0f)             | new FloatValue((4 * 2.0f) as Float)
-        new IntegerValue(4)  | new DoubleValue(2.0d)            | new DoubleValue((4 * 2.0d) as Double)
+        new IntegerValue(4)   | new CharacterValue('a' as char) | new IntegerValue((4 * INT_CHAR) as Integer)
+        new IntegerValue(4)   | new IntegerValue(2)             | new IntegerValue((4 * 2) as Integer)
+        new IntegerValue(4)   | new LongValue(2L)               | new LongValue((4 * 2L) as Long)
+        new IntegerValue(4)   | new FloatValue(2.0f)            | new FloatValue((4 * 2.0f) as Float)
+        new IntegerValue(4)   | new DoubleValue(2.0d)           | new DoubleValue((4 * 2.0d) as Double)
         // Long
-        new LongValue(4L)    | new CharacterValue('a' as char)  | new LongValue((4L * INT_CHAR) as Long)
-        new LongValue(4L)    | new IntegerValue(2)              | new LongValue((4L * 2) as Long)
-        new LongValue(4L)    | new LongValue(2L)                | new LongValue((4L * 2L) as Long)
-        new LongValue(4L)    | new FloatValue(2.0f)             | new FloatValue((4L * 2.0f) as Float)
-        new LongValue(4L)    | new DoubleValue(2.0d)            | new DoubleValue((4L * 2.0d) as Double)
+        new LongValue(4L)     | new CharacterValue('a' as char) | new LongValue((4L * INT_CHAR) as Long)
+        new LongValue(4L)     | new IntegerValue(2)             | new LongValue((4L * 2) as Long)
+        new LongValue(4L)     | new LongValue(2L)               | new LongValue((4L * 2L) as Long)
+        new LongValue(4L)     | new FloatValue(2.0f)            | new FloatValue((4L * 2.0f) as Float)
+        new LongValue(4L)     | new DoubleValue(2.0d)           | new DoubleValue((4L * 2.0d) as Double)
         // Float
-        new FloatValue(4.0f) | new CharacterValue('a' as char)  | new FloatValue((4.0f * INT_CHAR) as Float)
-        new FloatValue(4.0f) | new IntegerValue(2)              | new FloatValue((4.0f * 2) as Float)
-        new FloatValue(4.0f) | new LongValue(2L)                | new FloatValue((4.0f * 2L) as Float)
-        new FloatValue(4.0f) | new FloatValue(2.0f)             | new FloatValue((4.0f * 2.0f) as Float)
-        new FloatValue(4.0f) | new DoubleValue(2.0d)            | new DoubleValue((4.0f * 2.0d) as Double)
+        new FloatValue(4.0f)  | new CharacterValue('a' as char) | new FloatValue((4.0f * INT_CHAR) as Float)
+        new FloatValue(4.0f)  | new IntegerValue(2)             | new FloatValue((4.0f * 2) as Float)
+        new FloatValue(4.0f)  | new LongValue(2L)               | new FloatValue((4.0f * 2L) as Float)
+        new FloatValue(4.0f)  | new FloatValue(2.0f)            | new FloatValue((4.0f * 2.0f) as Float)
+        new FloatValue(4.0f)  | new DoubleValue(2.0d)           | new DoubleValue((4.0f * 2.0d) as Double)
         // Double
         new DoubleValue(4.0d) | new CharacterValue('a' as char) | new DoubleValue((4.0d * INT_CHAR) as Double)
         new DoubleValue(4.0d) | new IntegerValue(2)             | new DoubleValue((4.0d * 2) as Double)
@@ -532,25 +532,25 @@ class NumberValueTest extends Specification {
         eval == third
 
         where:
-        first                | second                           | third
+        first                 | second                          | third
         // Integer
-        new IntegerValue(4)  | new CharacterValue('a' as char)  | new IntegerValue((4 / INT_CHAR) as Integer)
-        new IntegerValue(4)  | new IntegerValue(2)              | new IntegerValue((4 / 2) as Integer)
-        new IntegerValue(4)  | new LongValue(2L)                | new LongValue((4 / 2L) as Long)
-        new IntegerValue(4)  | new FloatValue(2.0f)             | new FloatValue((4 / 2.0f) as Float)
-        new IntegerValue(4)  | new DoubleValue(2.0d)            | new DoubleValue((4 / 2.0d) as Double)
+        new IntegerValue(4)   | new CharacterValue('a' as char) | new IntegerValue((4 / INT_CHAR) as Integer)
+        new IntegerValue(4)   | new IntegerValue(2)             | new IntegerValue((4 / 2) as Integer)
+        new IntegerValue(4)   | new LongValue(2L)               | new LongValue((4 / 2L) as Long)
+        new IntegerValue(4)   | new FloatValue(2.0f)            | new FloatValue((4 / 2.0f) as Float)
+        new IntegerValue(4)   | new DoubleValue(2.0d)           | new DoubleValue((4 / 2.0d) as Double)
         // Long
-        new LongValue(4L)    | new CharacterValue('a' as char)  | new LongValue((4 / INT_CHAR) as Long)
-        new LongValue(4L)    | new IntegerValue(2)              | new LongValue((4 / 2) as Long)
-        new LongValue(4L)    | new LongValue(2L)                | new LongValue((4 / 2L) as Long)
-        new LongValue(4L)    | new FloatValue(2.0f)             | new FloatValue((4 / 2.0f) as Float)
-        new LongValue(4L)    | new DoubleValue(2.0d)            | new DoubleValue((4 / 2.0d) as Double)
+        new LongValue(4L)     | new CharacterValue('a' as char) | new LongValue((4 / INT_CHAR) as Long)
+        new LongValue(4L)     | new IntegerValue(2)             | new LongValue((4 / 2) as Long)
+        new LongValue(4L)     | new LongValue(2L)               | new LongValue((4 / 2L) as Long)
+        new LongValue(4L)     | new FloatValue(2.0f)            | new FloatValue((4 / 2.0f) as Float)
+        new LongValue(4L)     | new DoubleValue(2.0d)           | new DoubleValue((4 / 2.0d) as Double)
         // Float
-        new FloatValue(4.0f) | new CharacterValue('a' as char)  | new FloatValue((4.0f / INT_CHAR) as Float)
-        new FloatValue(4.0f) | new IntegerValue(2)              | new FloatValue((4.0f / 2) as Float)
-        new FloatValue(4.0f) | new LongValue(2L)                | new FloatValue((4.0f / 2L) as Float)
-        new FloatValue(4.0f) | new FloatValue(2.0f)             | new FloatValue((4.0f / 2.0f) as Float)
-        new FloatValue(4.0f) | new DoubleValue(2.0d)            | new DoubleValue((4.0f / 2.0d) as Double)
+        new FloatValue(4.0f)  | new CharacterValue('a' as char) | new FloatValue((4.0f / INT_CHAR) as Float)
+        new FloatValue(4.0f)  | new IntegerValue(2)             | new FloatValue((4.0f / 2) as Float)
+        new FloatValue(4.0f)  | new LongValue(2L)               | new FloatValue((4.0f / 2L) as Float)
+        new FloatValue(4.0f)  | new FloatValue(2.0f)            | new FloatValue((4.0f / 2.0f) as Float)
+        new FloatValue(4.0f)  | new DoubleValue(2.0d)           | new DoubleValue((4.0f / 2.0d) as Double)
         // Double
         new DoubleValue(4.0d) | new CharacterValue('a' as char) | new DoubleValue((4.0d / INT_CHAR) as Double)
         new DoubleValue(4.0d) | new IntegerValue(2)             | new DoubleValue((4.0d / 2) as Double)
@@ -567,25 +567,25 @@ class NumberValueTest extends Specification {
         eval == third
 
         where:
-        first                | second                           | third
+        first                 | second                          | third
         // Integer
-        new IntegerValue(4)  | new CharacterValue('a' as char)  | new IntegerValue((4 % INT_CHAR) as Integer)
-        new IntegerValue(4)  | new IntegerValue(2)              | new IntegerValue((4 % 2) as Integer)
-        new IntegerValue(4)  | new LongValue(2L)                | new LongValue((4 % 2L) as Long)
-        new IntegerValue(4)  | new FloatValue(2.0f)             | new FloatValue((4 % 2.0f) as Float)
-        new IntegerValue(4)  | new DoubleValue(2.0d)            | new DoubleValue((4 % 2.0d) as Double)
+        new IntegerValue(4)   | new CharacterValue('a' as char) | new IntegerValue((4 % INT_CHAR) as Integer)
+        new IntegerValue(4)   | new IntegerValue(2)             | new IntegerValue((4 % 2) as Integer)
+        new IntegerValue(4)   | new LongValue(2L)               | new LongValue((4 % 2L) as Long)
+        new IntegerValue(4)   | new FloatValue(2.0f)            | new FloatValue((4 % 2.0f) as Float)
+        new IntegerValue(4)   | new DoubleValue(2.0d)           | new DoubleValue((4 % 2.0d) as Double)
         // Long
-        new LongValue(4L)    | new CharacterValue('a' as char)  | new LongValue((4L % INT_CHAR) as Long)
-        new LongValue(4L)    | new IntegerValue(2)              | new LongValue((4L % 2) as Long)
-        new LongValue(4L)    | new LongValue(2L)                | new LongValue((4L % 2L) as Long)
-        new LongValue(4L)    | new FloatValue(2.0f)             | new FloatValue((4L % 2.0f) as Float)
-        new LongValue(4L)    | new DoubleValue(2.0d)            | new DoubleValue((4L % 2.0d) as Double)
+        new LongValue(4L)     | new CharacterValue('a' as char) | new LongValue((4L % INT_CHAR) as Long)
+        new LongValue(4L)     | new IntegerValue(2)             | new LongValue((4L % 2) as Long)
+        new LongValue(4L)     | new LongValue(2L)               | new LongValue((4L % 2L) as Long)
+        new LongValue(4L)     | new FloatValue(2.0f)            | new FloatValue((4L % 2.0f) as Float)
+        new LongValue(4L)     | new DoubleValue(2.0d)           | new DoubleValue((4L % 2.0d) as Double)
         // Float
-        new FloatValue(4.0f) | new CharacterValue('a' as char)  | new FloatValue((4.0f % INT_CHAR) as Float)
-        new FloatValue(4.0f) | new IntegerValue(2)              | new FloatValue((4.0f % 2) as Float)
-        new FloatValue(4.0f) | new LongValue(2L)                | new FloatValue((4.0f % 2L) as Float)
-        new FloatValue(4.0f) | new FloatValue(2.0f)             | new FloatValue((4.0f % 2.0f) as Float)
-        new FloatValue(4.0f) | new DoubleValue(2.0d)            | new DoubleValue((4.0f % 2.0d) as Double)
+        new FloatValue(4.0f)  | new CharacterValue('a' as char) | new FloatValue((4.0f % INT_CHAR) as Float)
+        new FloatValue(4.0f)  | new IntegerValue(2)             | new FloatValue((4.0f % 2) as Float)
+        new FloatValue(4.0f)  | new LongValue(2L)               | new FloatValue((4.0f % 2L) as Float)
+        new FloatValue(4.0f)  | new FloatValue(2.0f)            | new FloatValue((4.0f % 2.0f) as Float)
+        new FloatValue(4.0f)  | new DoubleValue(2.0d)           | new DoubleValue((4.0f % 2.0d) as Double)
         // Double
         new DoubleValue(4.0d) | new CharacterValue('a' as char) | new DoubleValue((4.0d % INT_CHAR) as Double)
         new DoubleValue(4.0d) | new IntegerValue(2)             | new DoubleValue((4.0d % 2) as Double)
