04f8b4a86fe2d2f8ab14990db28e533c3e17b935
Fulminazzo <fulminazzo21@gmail.com>
2025-01-02 03:10:48 +0100

    Added tests for methods and and or

--- src/test/groovy/it/fulminazzo/javaparser/executor/values/primitive/BooleanValueTest.groovy
+++ src/test/groovy/it/fulminazzo/javaparser/executor/values/primitive/BooleanValueTest.groovy
@@ -6,7 +6,37 @@ import static it.fulminazzo.javaparser.executor.values.primitive.BooleanValue.*
 
 class BooleanValueTest extends Specification {
 
-    def 'test #first ^ #second = #third'() {
+    def 'test #first && #second = #third'() {
+        when:
+        def eval = first.and(second)
+
+        then:
+        eval == third
+
+        where:
+        first | second | third
+        TRUE  | TRUE   | of(true && true)
+        TRUE  | FALSE  | of(true && false)
+        FALSE | TRUE   | of(false && true)
+        FALSE | FALSE  | of(false && false)
+    }
+
+    def 'test #first || #second = #third'() {
+        when:
+        def eval = first.or(second)
+
+        then:
+        eval == third
+
+        where:
+        first | second | third
+        TRUE  | TRUE   | of(true || true)
+        TRUE  | FALSE  | of(true || false)
+        FALSE | TRUE   | of(false || true)
+        FALSE | FALSE  | of(false || false)
+    }
+
+    def 'test #first & #second = #third'() {
         when:
         def eval = first.bitAnd(second)
 
