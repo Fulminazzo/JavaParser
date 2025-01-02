534df37e4ab29201e6b924e1c475028f32172caf
Fulminazzo <fulminazzo21@gmail.com>
2025-01-02 11:40:54 +0100

    Added tests for compatibleWith method

---
+++ src/test/groovy/it/fulminazzo/javaparser/executor/values/classes/PrimitiveClassValueTest.groovy
@@ -0,0 +1,74 @@
+package it.fulminazzo.javaparser.executor.values.classes
+
+import it.fulminazzo.javaparser.executor.values.primitive.PrimitiveValue
+import spock.lang.Specification
+
+class PrimitiveClassValueTest extends Specification {
+
+    def 'test #classValue should be compatible with #value'() {
+        expect:
+        classValue.compatibleWith(value)
+
+        where:
+        //TODO: wrapper objects
+        classValue                  | value
+        PrimitiveClassValue.BYTE    | PrimitiveValue.of(1)
+        PrimitiveClassValue.SHORT   | PrimitiveValue.of(1)
+        PrimitiveClassValue.CHAR    | PrimitiveValue.of('c' as char)
+        PrimitiveClassValue.CHAR    | PrimitiveValue.of(1)
+        PrimitiveClassValue.INT     | PrimitiveValue.of(1)
+        PrimitiveClassValue.INT     | PrimitiveValue.of('c' as char)
+        PrimitiveClassValue.LONG    | PrimitiveValue.of(1)
+        PrimitiveClassValue.LONG    | PrimitiveValue.of(1L)
+        PrimitiveClassValue.LONG    | PrimitiveValue.of('c' as char)
+        PrimitiveClassValue.FLOAT   | PrimitiveValue.of(1)
+        PrimitiveClassValue.FLOAT   | PrimitiveValue.of(1L)
+        PrimitiveClassValue.FLOAT   | PrimitiveValue.of(1.0f)
+        PrimitiveClassValue.FLOAT   | PrimitiveValue.of('c' as char)
+        PrimitiveClassValue.DOUBLE  | PrimitiveValue.of(1)
+        PrimitiveClassValue.DOUBLE  | PrimitiveValue.of(1L)
+        PrimitiveClassValue.DOUBLE  | PrimitiveValue.of(1.0f)
+        PrimitiveClassValue.DOUBLE  | PrimitiveValue.of(1.0d)
+        PrimitiveClassValue.DOUBLE  | PrimitiveValue.of('c' as char)
+        PrimitiveClassValue.BOOLEAN | PrimitiveValue.of(true)
+    }
+
+    def 'test #classValue should not be compatible with #value'() {
+        expect:
+        !classValue.compatibleWith(value)
+
+        where:
+        //TODO: wrapper objects
+        classValue                   | value
+        PrimitiveClassValue.BYTE     | PrimitiveValue.of('a')
+        PrimitiveClassValue.BYTE     | PrimitiveValue.of(1L)
+        PrimitiveClassValue.BYTE     | PrimitiveValue.of(1.0f)
+        PrimitiveClassValue.BYTE     | PrimitiveValue.of(1.0d)
+        PrimitiveClassValue.BYTE     | PrimitiveValue.of(true)
+        PrimitiveClassValue.SHORT    | PrimitiveValue.of('a')
+        PrimitiveClassValue.SHORT    | PrimitiveValue.of(1L)
+        PrimitiveClassValue.SHORT    | PrimitiveValue.of(1.0f)
+        PrimitiveClassValue.SHORT    | PrimitiveValue.of(1.0d)
+        PrimitiveClassValue.SHORT    | PrimitiveValue.of(true)
+        PrimitiveClassValue.CHAR     | PrimitiveValue.of('a')
+        PrimitiveClassValue.CHAR     | PrimitiveValue.of(1L)
+        PrimitiveClassValue.CHAR     | PrimitiveValue.of(1.0f)
+        PrimitiveClassValue.CHAR     | PrimitiveValue.of(1.0d)
+        PrimitiveClassValue.CHAR     | PrimitiveValue.of(true)
+        PrimitiveClassValue.INT      | PrimitiveValue.of(1.0f)
+        PrimitiveClassValue.INT      | PrimitiveValue.of(1.0d)
+        PrimitiveClassValue.INT      | PrimitiveValue.of(true)
+        PrimitiveClassValue.LONG     | PrimitiveValue.of(1.0f)
+        PrimitiveClassValue.LONG     | PrimitiveValue.of(1.0d)
+        PrimitiveClassValue.LONG     | PrimitiveValue.of(true)
+        PrimitiveClassValue.FLOAT    | PrimitiveValue.of(1.0d)
+        PrimitiveClassValue.FLOAT    | PrimitiveValue.of(true)
+        PrimitiveClassValue.DOUBLE   | PrimitiveValue.of(true)
+        PrimitiveClassValue.BOOLEAN  | PrimitiveValue.of(1)
+        PrimitiveClassValue.BOOLEAN  | PrimitiveValue.of('a')
+        PrimitiveClassValue.BOOLEAN  | PrimitiveValue.of(1L)
+        PrimitiveClassValue.BOOLEAN  | PrimitiveValue.of(1.0f)
+        PrimitiveClassValue.BOOLEAN  | PrimitiveValue.of(1.0d)
+    }
+
+}
