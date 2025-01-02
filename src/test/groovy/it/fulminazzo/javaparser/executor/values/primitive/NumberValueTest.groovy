02e90bd1e8bd4db7e2eb94139c6fa4f5e529f4ee
Fulminazzo <fulminazzo21@gmail.com>
2025-01-02 01:21:02 +0100

    Updated tests

--- src/test/groovy/it/fulminazzo/javaparser/executor/values/primitive/NumberValueTest.groovy
+++ src/test/groovy/it/fulminazzo/javaparser/executor/values/primitive/NumberValueTest.groovy
@@ -14,29 +14,29 @@ class NumberValueTest extends Specification {
         where:
         first                | second                           | third
         // Integer
-        new IntegerValue(4)  | new CharacterValue('a' as char)  | new IntegerValue(0)
+        new IntegerValue(4)  | new CharacterValue('a' as char)  | new IntegerValue(4)
         new IntegerValue(4)  | new IntegerValue(2)              | new IntegerValue(0)
-        new IntegerValue(4)  | new LongValue(2L)                | new IntegerValue(0)
-        new IntegerValue(4)  | new FloatValue(2.0f)             | new IntegerValue(0)
-        new IntegerValue(4)  | new DoubleValue(2.0d)            | new IntegerValue(0)
+        new IntegerValue(4)  | new LongValue(2L)                | new LongValue(0)
+        new IntegerValue(4)  | new FloatValue(2.0f)             | new FloatValue(0)
+        new IntegerValue(4)  | new DoubleValue(2.0d)            | new DoubleValue(0)
         // Long
-        new LongValue(4L)    | new CharacterValue('a' as char)  | new IntegerValue(0)
-        new LongValue(4L)    | new IntegerValue(2)              | new IntegerValue(0)
-        new LongValue(4L)    | new LongValue(2L)                | new IntegerValue(0)
-        new LongValue(4L)    | new FloatValue(2.0f)             | new IntegerValue(0)
-        new LongValue(4L)    | new DoubleValue(2.0d)            | new IntegerValue(0)
+        new LongValue(4L)    | new CharacterValue('a' as char)  | new LongValue(4L)
+        new LongValue(4L)    | new IntegerValue(2)              | new LongValue(0)
+        new LongValue(4L)    | new LongValue(2L)                | new LongValue(0)
+        new LongValue(4L)    | new FloatValue(2.0f)             | new FloatValue(0)
+        new LongValue(4L)    | new DoubleValue(2.0d)            | new DoubleValue(0)
         // Float
-        new FloatValue(4.0f) | new CharacterValue('a' as char)  | new IntegerValue(0)
-        new FloatValue(4.0f) | new IntegerValue(2)              | new IntegerValue(0)
-        new FloatValue(4.0f) | new LongValue(2L)                | new IntegerValue(0)
-        new FloatValue(4.0f) | new FloatValue(2.0f)             | new IntegerValue(0)
-        new FloatValue(4.0f) | new DoubleValue(2.0d)            | new IntegerValue(0)
+        new FloatValue(4.0f) | new CharacterValue('a' as char)  | new FloatValue(4.0f)
+        new FloatValue(4.0f) | new IntegerValue(2)              | new FloatValue(0)
+        new FloatValue(4.0f) | new LongValue(2L)                | new FloatValue(0)
+        new FloatValue(4.0f) | new FloatValue(2.0f)             | new FloatValue(0)
+        new FloatValue(4.0f) | new DoubleValue(2.0d)            | new DoubleValue(0)
         // Double
-        new DoubleValue(4.0d) | new CharacterValue('a' as char) | new IntegerValue(0)
-        new DoubleValue(4.0d) | new IntegerValue(2)             | new IntegerValue(0)
-        new DoubleValue(4.0d) | new LongValue(2L)               | new IntegerValue(0)
-        new DoubleValue(4.0d) | new FloatValue(2.0f)            | new IntegerValue(0)
-        new DoubleValue(4.0d) | new DoubleValue(2.0d)           | new IntegerValue(0)
+        new DoubleValue(4.0d) | new CharacterValue('a' as char) | new DoubleValue(4.0d)
+        new DoubleValue(4.0d) | new IntegerValue(2)             | new DoubleValue(0)
+        new DoubleValue(4.0d) | new LongValue(2L)               | new DoubleValue(0)
+        new DoubleValue(4.0d) | new FloatValue(2.0f)            | new DoubleValue(0)
+        new DoubleValue(4.0d) | new DoubleValue(2.0d)           | new DoubleValue(0)
     }
 
 }
