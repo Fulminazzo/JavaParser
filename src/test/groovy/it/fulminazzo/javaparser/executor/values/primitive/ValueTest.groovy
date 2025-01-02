3f8641e3f926d4bfc3558aa32d1fde959f709167
Fulminazzo <fulminazzo21@gmail.com>
2025-01-02 01:33:02 +0100

    Added tests for empty check method

---
+++ src/test/groovy/it/fulminazzo/javaparser/executor/values/primitive/ValueTest.groovy
@@ -0,0 +1,23 @@
+package it.fulminazzo.javaparser.executor.values.primitive
+
+import it.fulminazzo.javaparser.executor.values.Value
+import spock.lang.Specification
+
+class ValueTest extends Specification {
+
+    def 'test empty check'() {
+        given:
+        def value = new MockValue()
+
+        when:
+        value.check()
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    static class MockValue implements Value {
+
+    }
+
+}
