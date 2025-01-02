e0a31b6e2d972c765b394ca8315d286d7e79f476
Fulminazzo <fulminazzo21@gmail.com>
2025-01-02 11:37:22 +0100

    Added compatibleValues field, updated compatibleWith and static fields

--- src/main/java/it/fulminazzo/javaparser/executor/values/classes/PrimitiveClassValue.java
+++ src/main/java/it/fulminazzo/javaparser/executor/values/classes/PrimitiveClassValue.java
@@ -7,28 +7,44 @@ import it.fulminazzo.javaparser.executor.values.Value;
 import it.fulminazzo.javaparser.wrappers.ObjectWrapper;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+
 /**
  * Represents a primitive {@link ClassValue}.
  *
  * @param <V> the type of the primitive
  */
 public final class PrimitiveClassValue<V> extends ObjectWrapper<Class<V>> implements ClassValue<V>, Info {
-    public static final ClassValue<Byte> BYTE = new PrimitiveClassValue<>(byte.class);
-    public static final ClassValue<Short> SHORT = new PrimitiveClassValue<>(short.class);
-    public static final ClassValue<Character> CHAR = new PrimitiveClassValue<>(char.class);
-    public static final ClassValue<Integer> INT = new PrimitiveClassValue<>(int.class);
-    public static final ClassValue<Long> LONG = new PrimitiveClassValue<>(long.class);
-    public static final ClassValue<Float> FLOAT = new PrimitiveClassValue<>(float.class);
-    public static final ClassValue<Double> DOUBLE = new PrimitiveClassValue<>(double.class);
-    public static final ClassValue<Boolean> BOOLEAN = new PrimitiveClassValue<>(boolean.class);
+    public static final ClassValue<Byte> BYTE = new PrimitiveClassValue<>(byte.class,
+            Byte.class, Integer.class);
+    public static final ClassValue<Short> SHORT = new PrimitiveClassValue<>(short.class,
+            Byte.class, Short.class, Integer.class);
+    public static final ClassValue<Character> CHAR = new PrimitiveClassValue<>(char.class,
+            Integer.class, Character.class);
+    public static final ClassValue<Integer> INT = new PrimitiveClassValue<>(int.class,
+            Byte.class, Short.class, Character.class, Integer.class);
+    public static final ClassValue<Long> LONG = new PrimitiveClassValue<>(long.class,
+            Byte.class, Short.class, Character.class, Integer.class, Long.class);
+    public static final ClassValue<Float> FLOAT = new PrimitiveClassValue<>(float.class,
+            Byte.class, Short.class, Character.class, Integer.class, Long.class, Float.class);
+    public static final ClassValue<Double> DOUBLE = new PrimitiveClassValue<>(double.class,
+            Byte.class, Short.class, Character.class, Integer.class, Long.class, Float.class, Double.class);
+    public static final ClassValue<Boolean> BOOLEAN = new PrimitiveClassValue<>(boolean.class,
+            Boolean.class);
+
+    private final Class<?> @NotNull [] compatibleValues;
 
     /**
      * Instantiates a new Primitive class value.
      *
      * @param clazz the clazz
      */
-    public PrimitiveClassValue(final @NotNull Class<V> clazz) {
+    public PrimitiveClassValue(final @NotNull Class<V> clazz, Class<?> @NotNull ... compatibleValues) {
         super(clazz);
+        this.compatibleValues = Arrays.stream(compatibleValues)
+                .map(c -> new Class[]{c, ReflectionUtils.getPrimitiveClass(c)})
+                .flatMap(Arrays::stream)
+                .toArray(Class[]::new);
     }
 
     @Override
@@ -40,7 +56,10 @@ public final class PrimitiveClassValue<V> extends ObjectWrapper<Class<V>> implem
     public boolean compatibleWith(@NotNull Value<?> type) {
         Object actual = type.getValue();
         if (actual == null) return false;
-        return ReflectionUtils.getWrapperClass(getValue()).isAssignableFrom(actual.getClass());
+        else if (getValue().isAssignableFrom(actual.getClass())) return true;
+        else for (Class<?> compatibleValue : this.compatibleValues)
+            if (compatibleValue.isAssignableFrom(actual.getClass())) return true;
+        return false;
     }
 
 }
