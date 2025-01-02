881da7e2207abbefe0791f3d0f5dc828f46982d2
Fulminazzo <fulminazzo21@gmail.com>
2025-01-02 02:12:54 +0100

    Updated tests with actual operations

--- src/test/groovy/it/fulminazzo/javaparser/executor/values/primitive/CharacterValueTest.groovy
+++ src/test/groovy/it/fulminazzo/javaparser/executor/values/primitive/CharacterValueTest.groovy
@@ -14,11 +14,11 @@ class CharacterValueTest extends Specification {
         where:
         first                            | second                           | third
         // Character
-        new CharacterValue('a' as char)  | new CharacterValue('a' as char)  | new IntegerValue(194)
-        new CharacterValue('a' as char)  | new IntegerValue(2)              | new IntegerValue(99)
-        new CharacterValue('a' as char)  | new LongValue(2L)                | new LongValue(99)
-        new CharacterValue('a' as char)  | new FloatValue(2.0f)             | new FloatValue(99)
-        new CharacterValue('a' as char)  | new DoubleValue(2.0d)            | new DoubleValue(99)
+        new CharacterValue('a' as char)  | new CharacterValue('a' as char)  | new IntegerValue((((int) 'a') + ((int) 'a')) as Integer)
+        new CharacterValue('a' as char)  | new IntegerValue(2)              | new IntegerValue((((int) 'a') + 2) as Integer)
+        new CharacterValue('a' as char)  | new LongValue(2L)                | new LongValue((((int) 'a') + 2L) as Long)
+        new CharacterValue('a' as char)  | new FloatValue(2.0f)             | new FloatValue((((int) 'a') + 2.0f) as Float)
+        new CharacterValue('a' as char)  | new DoubleValue(2.0d)            | new DoubleValue((((int) 'a') + 2.0d) as Double)
     }
 
     def 'test #first - #second = #third'() {
@@ -31,11 +31,11 @@ class CharacterValueTest extends Specification {
         where:
         first                            | second                           | third
         // Character
-        new CharacterValue('a' as char)  | new CharacterValue('a' as char)  | new IntegerValue(0)
-        new CharacterValue('a' as char)  | new IntegerValue(2)              | new IntegerValue(95)
-        new CharacterValue('a' as char)  | new LongValue(2L)                | new LongValue(95)
-        new CharacterValue('a' as char)  | new FloatValue(2.0f)             | new FloatValue(95)
-        new CharacterValue('a' as char)  | new DoubleValue(2.0d)            | new DoubleValue(95)
+        new CharacterValue('a' as char)  | new CharacterValue('a' as char)  | new IntegerValue((((int) 'a') - ((int) 'a')) as Integer)
+        new CharacterValue('a' as char)  | new IntegerValue(2)              | new IntegerValue((((int) 'a') - 2) as Integer)
+        new CharacterValue('a' as char)  | new LongValue(2L)                | new LongValue((((int) 'a') - 2L) as Long)
+        new CharacterValue('a' as char)  | new FloatValue(2.0f)             | new FloatValue((((int) 'a') - 2.0f) as Float)
+        new CharacterValue('a' as char)  | new DoubleValue(2.0d)            | new DoubleValue((((int) 'a') - 2.0d) as Double)
     }
 
     def 'test #first * #second = #third'() {
@@ -48,11 +48,11 @@ class CharacterValueTest extends Specification {
         where:
         first                            | second                           | third
         // Character
-        new CharacterValue('a' as char)  | new CharacterValue('a' as char)  | new IntegerValue(9409)
-        new CharacterValue('a' as char)  | new IntegerValue(2)              | new IntegerValue(194)
-        new CharacterValue('a' as char)  | new LongValue(2L)                | new LongValue(194)
-        new CharacterValue('a' as char)  | new FloatValue(2.0f)             | new FloatValue(194)
-        new CharacterValue('a' as char)  | new DoubleValue(2.0d)            | new DoubleValue(194)
+        new CharacterValue('a' as char)  | new CharacterValue('a' as char)  | new IntegerValue((((int) 'a') * ((int) 'a')) as Integer)
+        new CharacterValue('a' as char)  | new IntegerValue(2)              | new IntegerValue((((int) 'a') * 2) as Integer)
+        new CharacterValue('a' as char)  | new LongValue(2L)                | new LongValue((((int) 'a') * 2L) as Long)
+        new CharacterValue('a' as char)  | new FloatValue(2.0f)             | new FloatValue((((int) 'a') * 2.0f) as Float)
+        new CharacterValue('a' as char)  | new DoubleValue(2.0d)            | new DoubleValue((((int) 'a') * 2.0d) as Double)
     }
 
     def 'test #first / #second = #third'() {
@@ -65,11 +65,11 @@ class CharacterValueTest extends Specification {
         where:
         first                            | second                           | third
         // Character
-        new CharacterValue('a' as char)  | new CharacterValue('a' as char)  | new IntegerValue(1)
-        new CharacterValue('a' as char)  | new IntegerValue(2)              | new IntegerValue(48)
-        new CharacterValue('a' as char)  | new LongValue(2L)                | new LongValue(48)
-        new CharacterValue('a' as char)  | new FloatValue(2.0f)             | new FloatValue(48.5)
-        new CharacterValue('a' as char)  | new DoubleValue(2.0d)            | new DoubleValue(48.5)
+        new CharacterValue('a' as char)  | new CharacterValue('a' as char)  | new IntegerValue((((int) 'a') / ((int) 'a')) as Integer)
+        new CharacterValue('a' as char)  | new IntegerValue(2)              | new IntegerValue((((int) 'a') / 2) as Integer)
+        new CharacterValue('a' as char)  | new LongValue(2L)                | new LongValue((((int) 'a') / 2L) as Long)
+        new CharacterValue('a' as char)  | new FloatValue(2.0f)             | new FloatValue((((int) 'a') / 2.0f) as Float)
+        new CharacterValue('a' as char)  | new DoubleValue(2.0d)            | new DoubleValue((((int) 'a') / 2.0d) as Double)
     }
 
     def 'test #first % #second = #third'() {
@@ -82,11 +82,11 @@ class CharacterValueTest extends Specification {
         where:
         first                            | second                           | third
         // Character
-        new CharacterValue('a' as char)  | new CharacterValue('a' as char)  | new IntegerValue(0)
-        new CharacterValue('a' as char)  | new IntegerValue(2)              | new IntegerValue(1)
-        new CharacterValue('a' as char)  | new LongValue(2L)                | new LongValue(1)
-        new CharacterValue('a' as char)  | new FloatValue(2.0f)             | new FloatValue(1)
-        new CharacterValue('a' as char)  | new DoubleValue(2.0d)            | new DoubleValue(1)
+        new CharacterValue('a' as char)  | new CharacterValue('a' as char)  | new IntegerValue((((int) 'a') % ((int) 'a')) as Integer)
+        new CharacterValue('a' as char)  | new IntegerValue(2)              | new IntegerValue((((int) 'a') % 2) as Integer)
+        new CharacterValue('a' as char)  | new LongValue(2L)                | new LongValue((((int) 'a') % 2L) as Long)
+        new CharacterValue('a' as char)  | new FloatValue(2.0f)             | new FloatValue((((int) 'a') % 2.0f) as Float)
+        new CharacterValue('a' as char)  | new DoubleValue(2.0d)            | new DoubleValue((((int) 'a') % 2.0d) as Double)
     }
 
 }
