53c737e9db109a8fb65f00f1d2cf5d0c4c84d96a
Fulminazzo <fulminazzo21@gmail.com>
2025-01-02 13:25:55 +0100

    Created CustomObjectClassValue with toString, compatibleWith and getValue methods

---
+++ src/main/java/it/fulminazzo/javaparser/executor/values/objects/CustomObjectClassValue.java
@@ -0,0 +1,39 @@
+package it.fulminazzo.javaparser.executor.values.objects;
+
+import it.fulminazzo.javaparser.executor.values.ClassValue;
+import it.fulminazzo.javaparser.executor.values.Value;
+import it.fulminazzo.javaparser.wrappers.ObjectWrapper;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Represents a {@link ObjectClassValue} with a class different from the default types.
+ */
+class CustomObjectClassValue<V> extends ObjectWrapper<Class<V>> implements ClassValue<V> {
+
+    /**
+     * Instantiates a new Object value.
+     *
+     * @param clazz the class
+     */
+    public CustomObjectClassValue(@NotNull Class<V> clazz) {
+        super(clazz);
+    }
+
+    @Override
+    public @NotNull Class<V> getValue() {
+        return this.object;
+    }
+
+    @Override
+    public boolean compatibleWith(@NotNull Value<?> value) {
+        if (value instanceof ObjectValue) 
+            return getValue().isAssignableFrom(value.getValue().getClass());
+        else return false; //TODO: check null
+    }
+
+    @Override
+    public String toString() {
+        return ClassValue.print(ObjectValue.getClassName(this.object));
+    }
+
+}
