6e78caf79997520bdf33649177af8728ac967b8c
Fulminazzo <fulminazzo21@gmail.com>
2025-01-02 19:43:15 +0100

    Separated ValueException in ValueRuntimeException

---
+++ src/main/java/it/fulminazzo/javaparser/executor/values/ValueRuntimeException.java
@@ -0,0 +1,31 @@
+package it.fulminazzo.javaparser.executor.values;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * An exception thrown by {@link Value} objects.
+ */
+public class ValueRuntimeException extends RuntimeException {
+
+    /**
+     * Instantiates a new Value runtime exception.
+     *
+     * @param message the message
+     * @param args    the arguments to add in the message format
+     */
+    private ValueRuntimeException(final @NotNull String message, final Object @NotNull ... args) {
+        super(String.format(message, args));
+    }
+
+    /**
+     * Generates a {@link ValueRuntimeException} with message:
+     * <i>Value %value% is not a valid primitive type</i>
+     *
+     * @param value the value
+     * @return the value runtime exception
+     */
+    public static @NotNull ValueRuntimeException invalidPrimitiveValue(final @NotNull Object value) {
+        return new ValueRuntimeException("Value %s is not a valid primitive type", value);
+    }
+
+}
