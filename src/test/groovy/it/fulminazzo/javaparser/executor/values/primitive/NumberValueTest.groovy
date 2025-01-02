a039979a94cfd6a377c44c6cc49fb21774d6d828
Fulminazzo <fulminazzo21@gmail.com>
2025-01-02 03:06:06 +0100

    Added tests for less than and less than equal

--- src/test/groovy/it/fulminazzo/javaparser/executor/values/primitive/NumberValueTest.groovy
+++ src/test/groovy/it/fulminazzo/javaparser/executor/values/primitive/NumberValueTest.groovy
@@ -5,6 +5,156 @@ import spock.lang.Specification
 class NumberValueTest extends Specification {
     private static int INT_CHAR = (int) 'a'
 
+    def 'test #first < #second = #third'() {
+        when:
+        def eval = first.lessThan(second)
+
+        then:
+        eval == third
+
+        where:
+        first                  | second                           | third
+        // Integer
+        new IntegerValue(100)  | new CharacterValue('a' as char)  | BooleanValue.FALSE
+        new IntegerValue(100)  | new CharacterValue('z' as char)  | BooleanValue.TRUE
+        new IntegerValue(100)  | new CharacterValue('d' as char)  | BooleanValue.FALSE
+        new IntegerValue(100)  | new IntegerValue(2)              | BooleanValue.FALSE
+        new IntegerValue(100)  | new IntegerValue(200)            | BooleanValue.TRUE
+        new IntegerValue(100)  | new IntegerValue(100)            | BooleanValue.FALSE
+        new IntegerValue(100)  | new LongValue(2L)                | BooleanValue.FALSE
+        new IntegerValue(100)  | new LongValue(200L)              | BooleanValue.TRUE
+        new IntegerValue(100)  | new LongValue(100L)              | BooleanValue.FALSE
+        new IntegerValue(100)  | new FloatValue(2.0f)             | BooleanValue.FALSE
+        new IntegerValue(100)  | new FloatValue(200.0f)           | BooleanValue.TRUE
+        new IntegerValue(100)  | new FloatValue(100.0f)           | BooleanValue.FALSE
+        new IntegerValue(100)  | new DoubleValue(2.0d)            | BooleanValue.FALSE
+        new IntegerValue(100)  | new DoubleValue(200.0d)          | BooleanValue.TRUE
+        new IntegerValue(100)  | new DoubleValue(100.0d)          | BooleanValue.FALSE
+        // Long
+        new LongValue(100L)    | new CharacterValue('a' as char)  | BooleanValue.FALSE
+        new LongValue(100L)    | new CharacterValue('z' as char)  | BooleanValue.TRUE
+        new LongValue(100L)    | new CharacterValue('d' as char)  | BooleanValue.FALSE
+        new LongValue(100L)    | new IntegerValue(2)              | BooleanValue.FALSE
+        new LongValue(100L)    | new IntegerValue(200)            | BooleanValue.TRUE
+        new LongValue(100L)    | new IntegerValue(100)            | BooleanValue.FALSE
+        new LongValue(100L)    | new LongValue(2L)                | BooleanValue.FALSE
+        new LongValue(100L)    | new LongValue(200L)              | BooleanValue.TRUE
+        new LongValue(100L)    | new LongValue(100L)              | BooleanValue.FALSE
+        new LongValue(100L)    | new FloatValue(2.0f)             | BooleanValue.FALSE
+        new LongValue(100L)    | new FloatValue(200.0f)           | BooleanValue.TRUE
+        new LongValue(100L)    | new FloatValue(100.0f)           | BooleanValue.FALSE
+        new LongValue(100L)    | new DoubleValue(2.0d)            | BooleanValue.FALSE
+        new LongValue(100L)    | new DoubleValue(200.0d)          | BooleanValue.TRUE
+        new LongValue(100L)    | new DoubleValue(100.0d)          | BooleanValue.FALSE
+        // Float
+        new FloatValue(100.0f) | new CharacterValue('a' as char)  | BooleanValue.FALSE
+        new FloatValue(100.0f) | new CharacterValue('z' as char)  | BooleanValue.TRUE
+        new FloatValue(100.0f) | new CharacterValue('d' as char)  | BooleanValue.FALSE
+        new FloatValue(100.0f) | new IntegerValue(2)              | BooleanValue.FALSE
+        new FloatValue(100.0f) | new IntegerValue(200)            | BooleanValue.TRUE
+        new FloatValue(100.0f) | new IntegerValue(100)            | BooleanValue.FALSE
+        new FloatValue(100.0f) | new LongValue(2L)                | BooleanValue.FALSE
+        new FloatValue(100.0f) | new LongValue(200L)              | BooleanValue.TRUE
+        new FloatValue(100.0f) | new LongValue(100L)              | BooleanValue.FALSE
+        new FloatValue(100.0f) | new FloatValue(2.0f)             | BooleanValue.FALSE
+        new FloatValue(100.0f) | new FloatValue(200.0f)           | BooleanValue.TRUE
+        new FloatValue(100.0f) | new FloatValue(100.0f)           | BooleanValue.FALSE
+        new FloatValue(100.0f) | new DoubleValue(2.0d)            | BooleanValue.FALSE
+        new FloatValue(100.0f) | new DoubleValue(200.0d)          | BooleanValue.TRUE
+        new FloatValue(100.0f) | new DoubleValue(100.0d)          | BooleanValue.FALSE
+        // Double
+        new DoubleValue(100.0d) | new CharacterValue('a' as char) | BooleanValue.FALSE
+        new DoubleValue(100.0d) | new CharacterValue('z' as char) | BooleanValue.TRUE
+        new DoubleValue(100.0d) | new CharacterValue('d' as char) | BooleanValue.FALSE
+        new DoubleValue(100.0d) | new IntegerValue(2)             | BooleanValue.FALSE
+        new DoubleValue(100.0d) | new IntegerValue(200)           | BooleanValue.TRUE
+        new DoubleValue(100.0d) | new IntegerValue(100)           | BooleanValue.FALSE
+        new DoubleValue(100.0d) | new LongValue(2L)               | BooleanValue.FALSE
+        new DoubleValue(100.0d) | new LongValue(200L)             | BooleanValue.TRUE
+        new DoubleValue(100.0d) | new LongValue(100L)             | BooleanValue.FALSE
+        new DoubleValue(100.0d) | new FloatValue(2.0f)            | BooleanValue.FALSE
+        new DoubleValue(100.0d) | new FloatValue(200.0f)          | BooleanValue.TRUE
+        new DoubleValue(100.0d) | new FloatValue(100.0f)          | BooleanValue.FALSE
+        new DoubleValue(100.0d) | new DoubleValue(2.0d)           | BooleanValue.FALSE
+        new DoubleValue(100.0d) | new DoubleValue(200.0d)         | BooleanValue.TRUE
+        new DoubleValue(100.0d) | new DoubleValue(100.0d)         | BooleanValue.FALSE
+    }
+
+    def 'test #first <= #second = #third'() {
+        when:
+        def eval = first.lessThanEqual(second)
+
+        then:
+        eval == third
+
+        where:
+        first                  | second                           | third
+        // Integer
+        new IntegerValue(100)  | new CharacterValue('a' as char)  | BooleanValue.FALSE
+        new IntegerValue(100)  | new CharacterValue('z' as char)  | BooleanValue.TRUE
+        new IntegerValue(100)  | new CharacterValue('d' as char)  | BooleanValue.TRUE
+        new IntegerValue(100)  | new IntegerValue(2)              | BooleanValue.FALSE
+        new IntegerValue(100)  | new IntegerValue(200)            | BooleanValue.TRUE
+        new IntegerValue(100)  | new IntegerValue(100)            | BooleanValue.TRUE
+        new IntegerValue(100)  | new LongValue(2L)                | BooleanValue.FALSE
+        new IntegerValue(100)  | new LongValue(200L)              | BooleanValue.TRUE
+        new IntegerValue(100)  | new LongValue(100L)              | BooleanValue.TRUE
+        new IntegerValue(100)  | new FloatValue(2.0f)             | BooleanValue.FALSE
+        new IntegerValue(100)  | new FloatValue(200.0f)           | BooleanValue.TRUE
+        new IntegerValue(100)  | new FloatValue(100.0f)           | BooleanValue.TRUE
+        new IntegerValue(100)  | new DoubleValue(2.0d)            | BooleanValue.FALSE
+        new IntegerValue(100)  | new DoubleValue(200.0d)          | BooleanValue.TRUE
+        new IntegerValue(100)  | new DoubleValue(100.0d)          | BooleanValue.TRUE
+        // Long
+        new LongValue(100L)    | new CharacterValue('a' as char)  | BooleanValue.FALSE
+        new LongValue(100L)    | new CharacterValue('z' as char)  | BooleanValue.TRUE
+        new LongValue(100L)    | new CharacterValue('d' as char)  | BooleanValue.TRUE
+        new LongValue(100L)    | new IntegerValue(2)              | BooleanValue.FALSE
+        new LongValue(100L)    | new IntegerValue(200)            | BooleanValue.TRUE
+        new LongValue(100L)    | new IntegerValue(100)            | BooleanValue.TRUE
+        new LongValue(100L)    | new LongValue(2L)                | BooleanValue.FALSE
+        new LongValue(100L)    | new LongValue(200L)              | BooleanValue.TRUE
+        new LongValue(100L)    | new LongValue(100L)              | BooleanValue.TRUE
+        new LongValue(100L)    | new FloatValue(2.0f)             | BooleanValue.FALSE
+        new LongValue(100L)    | new FloatValue(200.0f)           | BooleanValue.TRUE
+        new LongValue(100L)    | new FloatValue(100.0f)           | BooleanValue.TRUE
+        new LongValue(100L)    | new DoubleValue(2.0d)            | BooleanValue.FALSE
+        new LongValue(100L)    | new DoubleValue(200.0d)          | BooleanValue.TRUE
+        new LongValue(100L)    | new DoubleValue(100.0d)          | BooleanValue.TRUE
+        // Float
+        new FloatValue(100.0f) | new CharacterValue('a' as char)  | BooleanValue.FALSE
+        new FloatValue(100.0f) | new CharacterValue('z' as char)  | BooleanValue.TRUE
+        new FloatValue(100.0f) | new CharacterValue('d' as char)  | BooleanValue.TRUE
+        new FloatValue(100.0f) | new IntegerValue(2)              | BooleanValue.FALSE
+        new FloatValue(100.0f) | new IntegerValue(200)            | BooleanValue.TRUE
+        new FloatValue(100.0f) | new IntegerValue(100)            | BooleanValue.TRUE
+        new FloatValue(100.0f) | new LongValue(2L)                | BooleanValue.FALSE
+        new FloatValue(100.0f) | new LongValue(200L)              | BooleanValue.TRUE
+        new FloatValue(100.0f) | new LongValue(100L)              | BooleanValue.TRUE
+        new FloatValue(100.0f) | new FloatValue(2.0f)             | BooleanValue.FALSE
+        new FloatValue(100.0f) | new FloatValue(200.0f)           | BooleanValue.TRUE
+        new FloatValue(100.0f) | new FloatValue(100.0f)           | BooleanValue.TRUE
+        new FloatValue(100.0f) | new DoubleValue(2.0d)            | BooleanValue.FALSE
+        new FloatValue(100.0f) | new DoubleValue(200.0d)          | BooleanValue.TRUE
+        new FloatValue(100.0f) | new DoubleValue(100.0d)          | BooleanValue.TRUE
+        // Double
+        new DoubleValue(100.0d) | new CharacterValue('a' as char) | BooleanValue.FALSE
+        new DoubleValue(100.0d) | new CharacterValue('z' as char) | BooleanValue.TRUE
+        new DoubleValue(100.0d) | new CharacterValue('d' as char) | BooleanValue.TRUE
+        new DoubleValue(100.0d) | new IntegerValue(2)             | BooleanValue.FALSE
+        new DoubleValue(100.0d) | new IntegerValue(200)           | BooleanValue.TRUE
+        new DoubleValue(100.0d) | new IntegerValue(100)           | BooleanValue.TRUE
+        new DoubleValue(100.0d) | new LongValue(2L)               | BooleanValue.FALSE
+        new DoubleValue(100.0d) | new LongValue(200L)             | BooleanValue.TRUE
+        new DoubleValue(100.0d) | new LongValue(100L)             | BooleanValue.TRUE
+        new DoubleValue(100.0d) | new FloatValue(2.0f)            | BooleanValue.FALSE
+        new DoubleValue(100.0d) | new FloatValue(200.0f)          | BooleanValue.TRUE
+        new DoubleValue(100.0d) | new FloatValue(100.0f)          | BooleanValue.TRUE
+        new DoubleValue(100.0d) | new DoubleValue(2.0d)           | BooleanValue.FALSE
+        new DoubleValue(100.0d) | new DoubleValue(200.0d)         | BooleanValue.TRUE
+        new DoubleValue(100.0d) | new DoubleValue(100.0d)         | BooleanValue.TRUE
+    }
+
     def 'test #first > #second = #third'() {
         when:
         def eval = first.greaterThan(second)
