88e706663458136a75af58f665f2492618c65f66
Fulminazzo <fulminazzo21@gmail.com>
2025-01-02 13:00:39 +0100

    Added several fields

--- src/main/java/it/fulminazzo/javaparser/executor/values/objects/ObjectClassValue.java
+++ src/main/java/it/fulminazzo/javaparser/executor/values/objects/ObjectClassValue.java
@@ -11,6 +11,47 @@ import org.jetbrains.annotations.Nullable;
 
 @Getter
 public final class ObjectClassValue<V> extends EnumObject implements ClassValue<V> {
+    /**
+     * {@link java.lang.Byte}
+     */
+    public static final ObjectClassValue<Byte> BYTE = new ObjectClassValue<>(Byte.class, PrimitiveClassValue.BYTE);
+    /**
+     * {@link java.lang.Short}
+     */
+    public static final ObjectClassValue<Short> SHORT = new ObjectClassValue<>(Short.class, PrimitiveClassValue.SHORT);
+    /**
+     * {@link java.lang.Character}
+     */
+    public static final ObjectClassValue<Character> CHARACTER = new ObjectClassValue<>(Character.class, PrimitiveClassValue.CHAR);
+    /**
+     * {@link java.lang.Integer}
+     */
+    public static final ObjectClassValue<Integer> INTEGER = new ObjectClassValue<>(Integer.class, PrimitiveClassValue.INT);
+    /**
+     * {@link java.lang.Long}
+     */
+    public static final ObjectClassValue<Long> LONG = new ObjectClassValue<>(Long.class, PrimitiveClassValue.LONG);
+    /**
+     * {@link java.lang.Float}
+     */
+    public static final ObjectClassValue<Float> FLOAT = new ObjectClassValue<>(Float.class, PrimitiveClassValue.FLOAT);
+    /**
+     * {@link java.lang.Double}
+     */
+    public static final ObjectClassValue<Double> DOUBLE = new ObjectClassValue<>(Double.class, PrimitiveClassValue.DOUBLE);
+    /**
+     * {@link java.lang.Boolean}
+     */
+    public static final ObjectClassValue<Boolean> BOOLEAN = new ObjectClassValue<>(Boolean.class, PrimitiveClassValue.BOOLEAN);
+    /**
+     * {@link java.lang.String}
+     */
+    public static final ObjectClassValue<String> STRING = new ObjectClassValue<>(String.class);
+    /**
+     * {@link java.lang.Object}
+     */
+    public static final ObjectClassValue<Object> OBJECT = new ObjectClassValue<>(Object.class);
+    
     private final @NotNull Class<V> value;
     private final @Nullable ClassValue<V> associatedValue;
 
