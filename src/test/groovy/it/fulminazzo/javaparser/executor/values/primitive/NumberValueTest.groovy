7dcf93883c3bbe41a7d996d584c8ecc578ce90bc
Fulminazzo <fulminazzo21@gmail.com>
2025-01-02 01:02:06 +0100

    Refactored code

--- src/test/groovy/it/fulminazzo/javaparser/executor/values/primitive/NumberValueTest.groovy
+++ src/test/groovy/it/fulminazzo/javaparser/executor/values/primitive/NumberValueTest.groovy
@@ -12,19 +12,19 @@ class NumberValueTest extends Specification {
         eval == third
 
         where:
-        first | second | third
+        first                | second                           | third
         // Integer
-        new IntegerValue(4) | new CharacterValue('a' as char)   | new IntegerValue(0)
-        new IntegerValue(4) | new IntegerValue(2)               | new IntegerValue(0)
-        new IntegerValue(4) | new LongValue(2L)                 | new IntegerValue(0)
-        new IntegerValue(4) | new FloatValue(2.0f)              | new IntegerValue(0)
-        new IntegerValue(4) | new DoubleValue(2.0d)             | new IntegerValue(0)
+        new IntegerValue(4)  | new CharacterValue('a' as char)  | new IntegerValue(0)
+        new IntegerValue(4)  | new IntegerValue(2)              | new IntegerValue(0)
+        new IntegerValue(4)  | new LongValue(2L)                | new IntegerValue(0)
+        new IntegerValue(4)  | new FloatValue(2.0f)             | new IntegerValue(0)
+        new IntegerValue(4)  | new DoubleValue(2.0d)            | new IntegerValue(0)
         // Long
-        new LongValue(4L) | new CharacterValue('a' as char)     | new IntegerValue(0)
-        new LongValue(4L) | new IntegerValue(2)                 | new IntegerValue(0)
-        new LongValue(4L) | new LongValue(2L)                   | new IntegerValue(0)
-        new LongValue(4L) | new FloatValue(2.0f)                | new IntegerValue(0)
-        new LongValue(4L) | new DoubleValue(2.0d)               | new IntegerValue(0)
+        new LongValue(4L)    | new CharacterValue('a' as char)  | new IntegerValue(0)
+        new LongValue(4L)    | new IntegerValue(2)              | new IntegerValue(0)
+        new LongValue(4L)    | new LongValue(2L)                | new IntegerValue(0)
+        new LongValue(4L)    | new FloatValue(2.0f)             | new IntegerValue(0)
+        new LongValue(4L)    | new DoubleValue(2.0d)            | new IntegerValue(0)
         // Float
         new FloatValue(4.0f) | new CharacterValue('a' as char)  | new IntegerValue(0)
         new FloatValue(4.0f) | new IntegerValue(2)              | new IntegerValue(0)
