7ccf9909fbe7e8a331b07eda41e459cd21c91e54
Fulminazzo <fulminazzo21@gmail.com>
2025-01-03 19:51:06 +0100

    Added tests for array value static constructor

--- src/test/groovy/it/fulminazzo/javaparser/executor/values/arrays/ArrayValueTest.groovy
+++ src/test/groovy/it/fulminazzo/javaparser/executor/values/arrays/ArrayValueTest.groovy
@@ -4,9 +4,23 @@ import spock.lang.Specification
 
 class ArrayValueTest extends Specification {
 
+    def 'test static array initialization should have equal parameters'() {
+        given:
+        def value = new ArrayValue<>(Integer, 3)
+
+        and:
+        def expected = new Integer[3]
+
+        when:
+        def actual = value.getValue()
+
+        then:
+        Arrays.equals(actual, expected)
+    }
+
     def 'test dynamic array initialization should have equal parameters'() {
         given:
-        def value = new ArrayValue<>(String.class, ['hello', 'world'])
+        def value = new ArrayValue<>(String, ['hello', 'world'])
 
         and:
         def expected = new String[]{'hello', 'world'}
@@ -15,7 +29,7 @@ class ArrayValueTest extends Specification {
         def actual = value.getValue()
 
         then:
-        Arrays.deepEquals(actual, expected)
+        Arrays.equals(actual, expected)
     }
 
 }
