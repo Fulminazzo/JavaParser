85dc70bd4f3321bbb670d9035cbf2c7956f8acb4
Fulminazzo <fulminazzo21@gmail.com>
2025-01-03 22:10:51 +0100

    Added tests for compatible and incompatible

--- src/test/groovy/it/fulminazzo/javaparser/executor/values/arrays/ArrayClassValueTest.groovy
+++ src/test/groovy/it/fulminazzo/javaparser/executor/values/arrays/ArrayClassValueTest.groovy
@@ -1,13 +1,37 @@
 package it.fulminazzo.javaparser.executor.values.arrays
 
-
 import it.fulminazzo.javaparser.executor.values.PrimitiveClassValue
+import it.fulminazzo.javaparser.executor.values.Value
+import it.fulminazzo.javaparser.executor.values.Values
 import it.fulminazzo.javaparser.executor.values.objects.ObjectClassValue
 import spock.lang.Specification
 
 import java.lang.reflect.Array
 
 class ArrayClassValueTest extends Specification {
+
+    def 'test #classValue should be compatible with #value'() {
+        expect:
+        classValue.compatibleWith(value)
+
+        where:
+        classValue                                      | value
+        new ArrayClassValue<>(PrimitiveClassValue.INT)  | new ArrayValue<>(PrimitiveClassValue.INT, [])
+        new ArrayClassValue<>(ObjectClassValue.INTEGER) | new ArrayValue<>(ObjectClassValue.INTEGER, [])
+        new ArrayClassValue<>(ObjectClassValue.INTEGER) | Values.NULL_VALUE
+    }
+
+    def 'test #classValue should not be compatible with #value'() {
+        expect:
+        !classValue.compatibleWith(value)
+
+        where:
+        classValue                                      | value
+        new ArrayClassValue<>(ObjectClassValue.INTEGER) | new ArrayValue<>(PrimitiveClassValue.INT, [])
+        new ArrayClassValue<>(PrimitiveClassValue.INT)  | new ArrayValue<>(ObjectClassValue.INTEGER, [])
+        new ArrayClassValue<>(PrimitiveClassValue.INT)  | Values.NULL_VALUE
+        new ArrayClassValue<>(PrimitiveClassValue.INT)  | Value.of('Hello, world!')
+    }
 
     def 'test getValue should return class'() {
         given:
