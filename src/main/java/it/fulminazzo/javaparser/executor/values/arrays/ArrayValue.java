7776db4c3ccde7abee5b75d8ba2d597e14e7e202
Fulminazzo <fulminazzo21@gmail.com>
2025-01-03 20:01:56 +0100

    Added ofPrimitive function

--- src/main/java/it/fulminazzo/javaparser/executor/values/arrays/ArrayValue.java
+++ src/main/java/it/fulminazzo/javaparser/executor/values/arrays/ArrayValue.java
@@ -1,11 +1,13 @@
 package it.fulminazzo.javaparser.executor.values.arrays;
 
+import it.fulminazzo.fulmicollection.utils.ReflectionUtils;
 import it.fulminazzo.javaparser.executor.values.ClassValue;
 import it.fulminazzo.javaparser.executor.values.Value;
 import it.fulminazzo.javaparser.wrappers.ObjectWrapper;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Collection;
 
 /**
@@ -55,6 +57,22 @@ public class ArrayValue<V> extends ObjectWrapper<V[]> implements Value<V[]> {
     @Override
     public V[] getValue() {
         return this.object;
+    }
+
+    /**
+     * Creates a new {@link ArrayValue} for a primitive type
+     * (essentially initializing all its values to 0).
+     *
+     * @param <V>             the primitive type
+     * @param componentsClass the components class
+     * @param size            the size of the array
+     * @return the array value
+     */
+    public static <V> @NotNull ArrayValue<V> ofPrimitive(final Class<V> componentsClass,
+                                                         final int size) {
+        ArrayValue<?> value = new ArrayValue<>(ReflectionUtils.getWrapperClass(componentsClass), size);
+        Arrays.fill(value.object, ClassValue.of(componentsClass).toValue().getValue());
+        return (ArrayValue<V>) value;
     }
 
 }
