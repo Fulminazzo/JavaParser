5a4533b42bd0ed3d3300dea4573032574c8fe857
Fulminazzo <fulminazzo21@gmail.com>
2025-01-03 21:21:38 +0100

    Updated tests

--- src/test/groovy/it/fulminazzo/javaparser/executor/values/arrays/ArrayValueTest.groovy
+++ src/test/groovy/it/fulminazzo/javaparser/executor/values/arrays/ArrayValueTest.groovy
@@ -1,12 +1,15 @@
 package it.fulminazzo.javaparser.executor.values.arrays
 
+import it.fulminazzo.javaparser.executor.values.PrimitiveClassValue
+import it.fulminazzo.javaparser.executor.values.Value
+import it.fulminazzo.javaparser.executor.values.objects.ObjectClassValue
 import spock.lang.Specification
 
 class ArrayValueTest extends Specification {
 
     def 'test static array initialization should have equal parameters'() {
         given:
-        def value = new ArrayValue<>(Integer, 3)
+        def value = new ArrayValue<>(PrimitiveClassValue.INT, 3)
 
         and:
         def expected = new Integer[3]
@@ -20,10 +23,13 @@ class ArrayValueTest extends Specification {
 
     def 'test dynamic array initialization should have equal parameters'() {
         given:
-        def value = new ArrayValue<>(String, ['hello', 'world'])
+        def array = [Value.of('hello'), Value.of('world')]
 
         and:
-        def expected = new String[]{'hello', 'world'}
+        def value = new ArrayValue<>(ObjectClassValue.STRING, array)
+
+        and:
+        def expected = array.toArray(String[]::new)
 
         when:
         def actual = value.getValue()
@@ -34,7 +40,7 @@ class ArrayValueTest extends Specification {
 
     def 'test toClassValue of array should return compatible array class'() {
         given:
-        def value = new ArrayValue(String, 3)
+        def value = new ArrayValue(ObjectClassValue.STRING, 3)
 
         when:
         def classValue = value.toClassValue()
@@ -45,8 +51,8 @@ class ArrayValueTest extends Specification {
 
     def 'test equals and hashCode'() {
         given:
-        def first = new ArrayValue(String, ['Hello', 'world!'])
-        def second = new ArrayValue(String, ['Hello', 'world!'])
+        def first = new ArrayValue<>(ObjectClassValue.STRING, [Value.of('hello'), Value.of('world')])
+        def second = new ArrayValue<>(ObjectClassValue.STRING, [Value.of('hello'), Value.of('world')])
 
         expect:
         first == second
@@ -55,10 +61,10 @@ class ArrayValueTest extends Specification {
 
     def 'test toString'() {
         given:
-        def array = ['Hello', 'world!']
+        def array = [Value.of('Hello'), Value.of(null), Value.of('world!')]
 
         and:
-        def value = new ArrayValue(String, array)
+        def value = new ArrayValue(ObjectClassValue.STRING, array)
 
         when:
         def string = value.toString()
