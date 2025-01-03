c1da5010fc4d3bba3d648e5b20aa90134071af9d
Fulminazzo <fulminazzo21@gmail.com>
2025-01-03 20:13:58 +0100

    Added tests for getValue method

--- src/test/groovy/it/fulminazzo/javaparser/executor/values/arrays/ArrayClassValueTest.groovy
+++ src/test/groovy/it/fulminazzo/javaparser/executor/values/arrays/ArrayClassValueTest.groovy
@@ -1,9 +1,21 @@
 package it.fulminazzo.javaparser.executor.values.arrays
 
+import it.fulminazzo.javaparser.executor.values.ClassValue
 import it.fulminazzo.javaparser.executor.values.PrimitiveClassValue
 import spock.lang.Specification
 
 class ArrayClassValueTest extends Specification {
+
+    def 'test getValue should return class'() {
+        given:
+        def arrayClassValue = new ArrayClassValue(ClassValue.of(String))
+
+        when:
+        def value = arrayClassValue.getValue()
+
+        then:
+        value == String[]
+    }
 
     def 'test toString'() {
         given:
