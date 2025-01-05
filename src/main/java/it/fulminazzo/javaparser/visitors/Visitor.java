cb273fdf6d042271c855f2a6b6a99ef167f2ac3e
Fulminazzo <fulminazzo21@gmail.com>
2025-01-05 01:01:35 +0100

    Updated Javadoc

--- src/main/java/it/fulminazzo/javaparser/visitors/Visitor.java
+++ src/main/java/it/fulminazzo/javaparser/visitors/Visitor.java
@@ -21,7 +21,7 @@ import java.util.concurrent.Callable;
 
 /**
  * A <b>Visitor</b> is a special object capable of reading and parsing
- * each {@link it.fulminazzo.javaparser.parser.node.Node} accordingly.
+ * each {@link Node} accordingly.
  * It provides methods for each type of node.
  *
  * @param <O> the returned object
