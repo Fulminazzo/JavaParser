1a0cfb8aa3e02742edca6f3ef4fbbc668b6a81e6
Fulminazzo <fulminazzo21@gmail.com>
2025-01-05 15:52:56 +0100

    Removed many methods and extends. Updated constructor with Type instead of ClassType

--- src/main/java/it/fulminazzo/javaparser/typechecker/types/ParameterTypes.java
+++ src/main/java/it/fulminazzo/javaparser/typechecker/types/ParameterTypes.java
@@ -12,52 +12,20 @@ import java.util.stream.Stream;
 /**
  * Represents the list of parameters {@link Type}s required during the invocation of a method or constructor.
  */
-public final class ParameterTypes extends ObjectWrapper<List<ClassType>> implements Type, Iterable<ClassType> {
+public final class ParameterTypes implements Type {
 
     /**
      * Instantiates a new Parameter types.
      *
      * @param parameters the parameters
      */
-    public ParameterTypes(final @NotNull List<ClassType> parameters) {
+    public ParameterTypes(final @NotNull List<Type> parameters) {
         super(new LinkedList<>(parameters));
-    }
-
-    /**
-     * Converts this class to an array of Java {@link Class}es.
-     *
-     * @return the classes
-     */
-    public Class<?> @NotNull [] toJavaClassArray() {
-        return this.object.stream().map(ClassType::toJavaClass).toArray(Class[]::new);
-    }
-
-    /**
-     * Returns the number of parameters.
-     *
-     * @return the number of parameters
-     */
-    public int size() {
-        return this.object.size();
     }
 
     @Override
     public @NotNull ClassType toClass() {
         throw TypeCheckerException.noClassType(getClass());
-    }
-
-    /**
-     * Gets a stream of the internal classes.
-     *
-     * @return the stream
-     */
-    public @NotNull Stream<ClassType> stream() {
-        return this.object.stream();
-    }
-
-    @Override
-    public @NotNull Iterator<ClassType> iterator() {
-        return stream().iterator();
     }
 
 }
