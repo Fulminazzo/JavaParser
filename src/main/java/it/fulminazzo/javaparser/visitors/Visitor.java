147cbaed3254070f193336fd2bbd7b591be5edcb
Fulminazzo <fulminazzo21@gmail.com>
2025-01-05 14:07:04 +0100

    Updated newLiteralObject method

--- src/main/java/it/fulminazzo/javaparser/visitors/Visitor.java
+++ src/main/java/it/fulminazzo/javaparser/visitors/Visitor.java
@@ -803,10 +803,10 @@ public interface Visitor<
                     }
             }
             throw cannotResolveSymbol(value);
-        } else return newLiteralObject(value);
+        } else return (O) newLiteralObject(value);
     }
 
-    <L extends LiteralObject<C, O, P>> @NotNull L newLiteralObject(@NotNull String value);
+    @NotNull LiteralObject<C, O, P> newLiteralObject(@NotNull String value);
 
     /**
      * Converts empty literal and its fields to this visitor type.
