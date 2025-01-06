95ac8b3bd04fe18be24bdf4b52b0aafdcb9ca65b
Fulminazzo <fulminazzo21@gmail.com>
2025-01-06 11:55:20 +0100

    Created VariableContainer class

---
+++ src/main/java/it/fulminazzo/javaparser/visitors/visitorobjects/variables/VariableContainer.java
@@ -0,0 +1,90 @@
+package it.fulminazzo.javaparser.visitors.visitorobjects.variables;
+
+import it.fulminazzo.fulmicollection.structures.tuples.Tuple;
+import it.fulminazzo.javaparser.visitors.visitorobjects.ClassVisitorObject;
+import it.fulminazzo.javaparser.visitors.visitorobjects.ParameterVisitorObjects;
+import it.fulminazzo.javaparser.visitors.visitorobjects.VisitorObject;
+import it.fulminazzo.javaparser.visitors.visitorobjects.VisitorObjectException;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * A variable container is an abstract class that contains the information of a variable.
+ * It also provides a {@link #set(VisitorObject)} method to update it.
+ *
+ * @param <C> the type of the {@link ClassVisitorObject}
+ * @param <O> the type of the {@link VisitorObject}
+ * @param <P> the type of the {@link ParameterVisitorObjects}
+ */
+@Getter
+abstract class VariableContainer<
+        C extends ClassVisitorObject<C, O, P>,
+        O extends VisitorObject<C, O, P>,
+        P extends ParameterVisitorObjects<C, O, P>
+        > implements VisitorObject<C, O, P> {
+    private final @NotNull C type;
+    private final @NotNull O value;
+
+    /**
+     * Instantiates a new Variable container.
+     *
+     * @param type  the type
+     * @param value the value
+     */
+    public VariableContainer(final @NotNull C type,
+                             final @NotNull O value) {
+        this.type = type;
+        this.value = value;
+    }
+
+    /**
+     * Updates the value of the variable.
+     *
+     * @param newValue the new value
+     */
+    public abstract void set(final @NotNull O newValue);
+
+    @Override
+    public boolean isPrimitive() {
+        return this.value.isPrimitive();
+    }
+
+    @Override
+    public boolean isNull() {
+        return this.value.isNull();
+    }
+
+    @Override
+    public <T extends VisitorObject<C, O, P>> @NotNull T check(@NotNull Class<T> clazz) {
+        return this.value.check(clazz);
+    }
+
+    @Override
+    public @NotNull Tuple<C, O> getField(@NotNull Field field) throws VisitorObjectException {
+        return this.value.getField(field);
+    }
+
+    @Override
+    public @NotNull O invokeMethod(@NotNull Method method, @NotNull P parameters) throws VisitorObjectException {
+        return this.value.invokeMethod(method, parameters);
+    }
+
+    @Override
+    public @NotNull O toPrimitive() {
+        return this.value.toPrimitive();
+    }
+
+    @Override
+    public @NotNull O toWrapper() {
+        return this.value.toWrapper();
+    }
+
+    @Override
+    public @NotNull C toClass() {
+        return this.type;
+    }
+
+}
