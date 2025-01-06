ef73ad76b9d199a390a9bb1ccc0b55211eba7ffd
Fulminazzo <fulminazzo21@gmail.com>
2025-01-06 13:42:22 +0100

    Overridden several type methods

--- src/main/java/it/fulminazzo/javaparser/typechecker/types/variables/TypeLiteralVariableContainer.java
+++ src/main/java/it/fulminazzo/javaparser/typechecker/types/variables/TypeLiteralVariableContainer.java
@@ -2,12 +2,12 @@ package it.fulminazzo.javaparser.typechecker.types.variables;
 
 import it.fulminazzo.javaparser.environment.Environment;
 import it.fulminazzo.javaparser.typechecker.TypeCheckerException;
-import it.fulminazzo.javaparser.typechecker.types.ClassType;
-import it.fulminazzo.javaparser.typechecker.types.ParameterTypes;
-import it.fulminazzo.javaparser.typechecker.types.Type;
-import it.fulminazzo.javaparser.typechecker.types.Types;
+import it.fulminazzo.javaparser.typechecker.types.*;
 import it.fulminazzo.javaparser.visitors.visitorobjects.variables.LiteralVariableContainer;
 import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 /**
  * An implementation of {@link LiteralVariableContainer} with {@link Type}.
@@ -40,6 +40,36 @@ public final class TypeLiteralVariableContainer extends LiteralVariableContainer
     @Override
     protected @NotNull RuntimeException exceptionWrapper(@NotNull Exception exception) {
         return TypeCheckerException.of(exception);
+    }
+
+    @Override
+    public boolean isClassType() {
+        return this.value.isClassType();
+    }
+
+    @Override
+    public @NotNull Type check(Type @NotNull ... expectedTypes) {
+        return this.value.check(expectedTypes);
+    }
+
+    @Override
+    public @NotNull Type checkNot(Type @NotNull ... expectedTypes) {
+        return this.value.checkNot(expectedTypes);
+    }
+
+    @Override
+    public @NotNull Type checkAssignableFrom(@NotNull ClassType classType) {
+        return this.value.checkAssignableFrom(classType);
+    }
+
+    @Override
+    public @NotNull TypeFieldContainer getField(@NotNull Field field) throws TypeException {
+        return this.value.getField(field);
+    }
+
+    @Override
+    public @NotNull Type invokeMethod(@NotNull Method method, @NotNull ParameterTypes parameterTypes) throws TypeException {
+        return this.value.invokeMethod(method, parameterTypes);
     }
 
 }
