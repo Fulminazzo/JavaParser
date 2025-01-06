841a8c08d34c6289b7953864f69164f2dba05b3c
Fulminazzo <fulminazzo21@gmail.com>
2025-01-06 01:12:39 +0100

    Removed cast method

--- src/main/java/it/fulminazzo/javaparser/executor/values/arrays/ArrayClassValue.java
+++ src/main/java/it/fulminazzo/javaparser/executor/values/arrays/ArrayClassValue.java
@@ -31,15 +31,6 @@ public class ArrayClassValue<A> extends ObjectWrapper<ClassValue<A>> implements 
     }
 
     @Override
-    public @NotNull Value<V[]> cast(@NotNull Value<?> value) {
-        Object[] internal = (Object[]) value.getValue();
-        Value<V[]> returnedValue = ArrayValue.of(getComponentsType(), internal.length);
-        for (int i = 0; i < internal.length; i++)
-            returnedValue.getValue()[i] = getComponentsType().cast(Value.of(internal[i])).getValue();
-        return returnedValue;
-    }
-
-    @Override
     public boolean compatibleWith(@NotNull Value<?> value) {
         if (value.is(Values.NULL_VALUE) && !this.object.isPrimitive()) return true;
         if (value.is(ArrayValue.class)) {
