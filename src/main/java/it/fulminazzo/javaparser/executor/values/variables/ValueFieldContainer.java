a2ce415c02c386fa813ca0b848aa1af1f8e8e62a
Fulminazzo <fulminazzo21@gmail.com>
2025-01-06 16:10:12 +0100

    Made ValueFieldContainer implement ValueVariableContainer interface

--- src/main/java/it/fulminazzo/javaparser/executor/values/variables/ValueFieldContainer.java
+++ src/main/java/it/fulminazzo/javaparser/executor/values/variables/ValueFieldContainer.java
@@ -7,7 +7,6 @@ import it.fulminazzo.javaparser.executor.values.Value;
 import it.fulminazzo.javaparser.executor.values.ValueException;
 import it.fulminazzo.javaparser.visitors.visitorobjects.variables.FieldContainer;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -15,8 +14,8 @@ import java.lang.reflect.Method;
 /**
  * An implementation of {@link FieldContainer} for {@link Value}.
  */
-@SuppressWarnings("unchecked")
-public final class ValueFieldContainer<V> extends FieldContainer<ClassValue<?>, Value<?>, ParameterValues> implements Value<V> {
+public final class ValueFieldContainer<V> extends FieldContainer<ClassValue<?>, Value<?>, ParameterValues>
+        implements Value<V>, ValueVariableContainer<V> {
 
     /**
      * Instantiates a new Field container.
@@ -37,58 +36,58 @@ public final class ValueFieldContainer<V> extends FieldContainer<ClassValue<?>, 
     }
 
     @Override
-    public @Nullable V getValue() {
-        return (V) this.variable.getValue();
+    public V getValue() {
+        return ValueVariableContainer.super.getValue();
     }
 
     @Override
     public boolean isCharacter() {
-        return this.variable.isCharacter();
+        return ValueVariableContainer.super.isCharacter();
     }
 
     @Override
     public boolean isInteger() {
-        return this.variable.isInteger();
+        return ValueVariableContainer.super.isInteger();
     }
 
     @Override
     public boolean isLong() {
-        return this.variable.isLong();
+        return ValueVariableContainer.super.isLong();
     }
 
     @Override
     public boolean isFloat() {
-        return this.variable.isFloat();
+        return ValueVariableContainer.super.isFloat();
     }
 
     @Override
     public boolean isDouble() {
-        return this.variable.isDouble();
+        return ValueVariableContainer.super.isDouble();
     }
 
     @Override
     public boolean isBoolean() {
-        return this.variable.isBoolean();
+        return ValueVariableContainer.super.isBoolean();
     }
 
     @Override
     public boolean isString() {
-        return this.variable.isString();
+        return ValueVariableContainer.super.isString();
     }
 
     @Override
     public @NotNull ClassValue<V> checkClass() {
-        return (ClassValue<V>) this.variable.checkClass();
+        return ValueVariableContainer.super.checkClass();
     }
 
     @Override
     public @NotNull ValueFieldContainer<V> getField(@NotNull Field field) {
-        return (ValueFieldContainer<V>) this.variable.getField(field);
+        return ValueVariableContainer.super.getField(field);
     }
 
     @Override
     public @NotNull Value<?> invokeMethod(@NotNull Method method, @NotNull ParameterValues parameters) throws ValueException {
-        return this.variable.invokeMethod(method, parameters);
+        return ValueVariableContainer.super.invokeMethod(method, parameters);
     }
 
 }
