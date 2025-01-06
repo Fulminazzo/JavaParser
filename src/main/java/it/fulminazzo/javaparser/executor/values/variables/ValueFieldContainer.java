e781e4429b9f4e34b4cc7ec8bb4b519024c9f500
Fulminazzo <fulminazzo21@gmail.com>
2025-01-06 13:43:28 +0100

    Overridden several Value methods

--- src/main/java/it/fulminazzo/javaparser/executor/values/variables/ValueFieldContainer.java
+++ src/main/java/it/fulminazzo/javaparser/executor/values/variables/ValueFieldContainer.java
@@ -3,8 +3,12 @@ package it.fulminazzo.javaparser.executor.values.variables;
 import it.fulminazzo.javaparser.executor.values.ClassValue;
 import it.fulminazzo.javaparser.executor.values.ParameterValues;
 import it.fulminazzo.javaparser.executor.values.Value;
+import it.fulminazzo.javaparser.executor.values.ValueException;
 import it.fulminazzo.javaparser.visitors.visitorobjects.variables.FieldContainer;
 import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 /**
  * An implementation of {@link FieldContainer} for {@link Value}.
@@ -19,6 +23,56 @@ public final class ValueFieldContainer extends FieldContainer<ClassValue<?>, Val
      */
     public ValueFieldContainer(@NotNull ClassValue<?> type, @NotNull Value<?> value) {
         super(type, value);
+    }
+
+    @Override
+    public boolean isCharacter() {
+        return this.value.isCharacter();
+    }
+
+    @Override
+    public boolean isInteger() {
+        return this.value.isInteger();
+    }
+
+    @Override
+    public boolean isLong() {
+        return this.value.isLong();
+    }
+
+    @Override
+    public boolean isFloat() {
+        return this.value.isFloat();
+    }
+
+    @Override
+    public boolean isDouble() {
+        return this.value.isDouble();
+    }
+
+    @Override
+    public boolean isBoolean() {
+        return this.value.isBoolean();
+    }
+
+    @Override
+    public boolean isString() {
+        return this.value.isString();
+    }
+
+    @Override
+    public @NotNull ValueFieldContainer getField(@NotNull Field field) {
+        return this.value.getField(field);
+    }
+
+    @Override
+    public @NotNull Value<?> invokeMethod(@NotNull Method method, @NotNull ParameterValues parameters) throws ValueException {
+        return this.value.invokeMethod(method, parameters);
+    }
+
+    @Override
+    public <T extends Value<?>> @NotNull T to(@NotNull Class<T> value) {
+        return this.value.to(value);
     }
 
 }
