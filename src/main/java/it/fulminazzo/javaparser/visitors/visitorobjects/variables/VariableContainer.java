522477329cb03203a564972510595c2abc4fa275
Fulminazzo <fulminazzo21@gmail.com>
2025-01-06 12:15:27 +0100

    Overridden several VisitorObject methods in VariableContainer to mimic value behavior

--- src/main/java/it/fulminazzo/javaparser/visitors/visitorobjects/variables/VariableContainer.java
+++ src/main/java/it/fulminazzo/javaparser/visitors/visitorobjects/variables/VariableContainer.java
@@ -7,9 +7,6 @@ import it.fulminazzo.javaparser.visitors.visitorobjects.VisitorObjectException;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-
 /**
  * A variable container is an abstract class that contains the information of a variable.
  * It also provides a {@link #set(VisitorObject)} method to update it.
@@ -24,8 +21,8 @@ abstract class VariableContainer<
         O extends VisitorObject<C, O, P>,
         P extends ParameterVisitorObjects<C, O, P>
         > implements VisitorObject<C, O, P> {
-    private final @NotNull C type;
-    private final @NotNull O value;
+    protected final @NotNull C type;
+    protected final @NotNull O value;
 
     /**
      * Instantiates a new Variable container.
@@ -58,18 +55,38 @@ abstract class VariableContainer<
     }
 
     @Override
+    public boolean is(@NotNull Class<?> object) {
+        return this.value.is(object);
+    }
+
+    @Override
+    public boolean is(O @NotNull ... objects) {
+        return this.value.is(objects);
+    }
+
+    @Override
+    public boolean isAssignableFrom(@NotNull ClassVisitorObject<C, O, P> classVisitorObject) {
+        return this.value.isAssignableFrom(classVisitorObject);
+    }
+
+    @Override
     public <T extends VisitorObject<C, O, P>> @NotNull T check(@NotNull Class<T> clazz) {
         return this.value.check(clazz);
     }
 
     @Override
-    public @NotNull FieldContainer<C, O, P> getField(@NotNull Field field) throws VisitorObjectException {
-        return this.value.getField(field);
+    public @NotNull C checkClass() {
+        return this.value.checkClass();
     }
 
     @Override
-    public @NotNull O invokeMethod(@NotNull Method method, @NotNull P parameters) throws VisitorObjectException {
-        return this.value.invokeMethod(method, parameters);
+    public @NotNull FieldContainer<C, O, P> getField(@NotNull String fieldName) throws VisitorObjectException {
+        return this.value.getField(fieldName);
+    }
+
+    @Override
+    public @NotNull O invokeMethod(@NotNull String methodName, @NotNull P parameters) throws VisitorObjectException {
+        return this.value.invokeMethod(methodName, parameters);
     }
 
     @Override
@@ -84,7 +101,112 @@ abstract class VariableContainer<
 
     @Override
     public @NotNull C toClass() {
-        return this.type;
+        return this.value.toClass();
+    }
+
+    @Override
+    public @NotNull O and(@NotNull O other) {
+        return this.value.and(other);
+    }
+
+    @Override
+    public @NotNull O or(@NotNull O other) {
+        return this.value.or(other);
+    }
+
+    @Override
+    public @NotNull O equal(@NotNull O other) {
+        return this.value.equal(other);
+    }
+
+    @Override
+    public @NotNull O notEqual(@NotNull O other) {
+        return this.value.notEqual(other);
+    }
+
+    @Override
+    public @NotNull O lessThan(@NotNull O other) {
+        return this.value.lessThan(other);
+    }
+
+    @Override
+    public @NotNull O lessThanEqual(@NotNull O other) {
+        return this.value.lessThanEqual(other);
+    }
+
+    @Override
+    public @NotNull O greaterThan(@NotNull O other) {
+        return this.value.greaterThan(other);
+    }
+
+    @Override
+    public @NotNull O greaterThanEqual(@NotNull O other) {
+        return this.value.greaterThanEqual(other);
+    }
+
+    @Override
+    public @NotNull O bitAnd(@NotNull O other) {
+        return this.value.bitAnd(other);
+    }
+
+    @Override
+    public @NotNull O bitOr(@NotNull O other) {
+        return this.value.bitOr(other);
+    }
+
+    @Override
+    public @NotNull O bitXor(@NotNull O other) {
+        return this.value.bitXor(other);
+    }
+
+    @Override
+    public @NotNull O lshift(@NotNull O other) {
+        return this.value.lshift(other);
+    }
+
+    @Override
+    public @NotNull O rshift(@NotNull O other) {
+        return this.value.rshift(other);
+    }
+
+    @Override
+    public @NotNull O urshift(@NotNull O other) {
+        return this.value.urshift(other);
+    }
+
+    @Override
+    public @NotNull O add(@NotNull O other) {
+        return this.value.add(other);
+    }
+
+    @Override
+    public @NotNull O subtract(@NotNull O other) {
+        return this.value.subtract(other);
+    }
+
+    @Override
+    public @NotNull O multiply(@NotNull O other) {
+        return this.value.multiply(other);
+    }
+
+    @Override
+    public @NotNull O divide(@NotNull O other) {
+        return this.value.divide(other);
+    }
+
+    @Override
+    public @NotNull O modulo(@NotNull O other) {
+        return this.value.modulo(other);
+    }
+
+    @Override
+    public @NotNull O minus() {
+        return this.value.minus();
+    }
+
+    @Override
+    public @NotNull O not() {
+        return this.value.not();
     }
 
 }
