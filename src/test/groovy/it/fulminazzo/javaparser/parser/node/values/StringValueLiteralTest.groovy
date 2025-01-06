d74d9d1fabd1a42ae81e0cebc91111e2f1524582
Fulminazzo <fulminazzo21@gmail.com>
2025-01-06 15:13:04 +0100

    Updated tests

--- src/test/groovy/it/fulminazzo/javaparser/parser/node/values/StringValueLiteralTest.groovy
+++ src/test/groovy/it/fulminazzo/javaparser/parser/node/values/StringValueLiteralTest.groovy
@@ -8,17 +8,20 @@ class StringValueLiteralTest extends Specification {
         given:
         def expected = 'Hello\tworld\r\nMy \'name\' is \"James\", ' +
                 'and I love \b, \f and \\,' +
-                'but I hate \\a, \\b, \\d, \\e, \\g, \\h, \\i, \\l, \\m, \\n, \\o,' +
+                'but I hate \\a, \\d, \\e, \\g, \\h, \\i, \\l, \\m, \\o,' +
                 '\\p, \\q, \\s, \\u, \\v, \\w, \\x, \\y and \\z'
 
         and:
         def literal = new StringValueLiteral('\"Hello\\tworld\\r\\n' +
                 'My \\\'name\\\' is \\"James\\", and I love \\b, \\f and \\\\' +
-                'but I hate \\a, \\b, \\d, \\e, \\g, \\h, \\i, \\l, \\m, \\n, \\o,' +
+                'but I hate \\a, \\d, \\e, \\g, \\h, \\i, \\l, \\m, \\o,' +
                 '\\p, \\q, \\s, \\u, \\v, \\w, \\x, \\y and \\z\"')
 
-        expect:
-        literal.rawValue == expected
+        when:
+        def value = literal.rawValue
+
+        then:
+        value == expected
     }
 
 }
