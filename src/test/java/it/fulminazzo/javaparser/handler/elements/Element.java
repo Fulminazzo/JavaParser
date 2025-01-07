5708e78d6479c2c243395f501b50020d06162b7d
Fulminazzo <fulminazzo21@gmail.com>
2025-01-07 15:18:31 +0100

    Rewritten Element using IElement

--- src/test/java/it/fulminazzo/javaparser/handler/elements/Element.java
+++ src/test/java/it/fulminazzo/javaparser/handler/elements/Element.java
@@ -2,20 +2,11 @@ package it.fulminazzo.javaparser.handler.elements;
 
 import it.fulminazzo.fulmicollection.utils.ReflectionUtils;
 import it.fulminazzo.javaparser.handler.HandlerException;
-import it.fulminazzo.javaparser.tokenizer.TokenType;
 import it.fulminazzo.javaparser.visitors.visitorobjects.VisitorObject;
-import it.fulminazzo.javaparser.visitors.visitorobjects.VisitorObjectException;
-import it.fulminazzo.javaparser.visitors.visitorobjects.variables.FieldContainer;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.lang.reflect.Executable;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.Arrays;
-
-@SuppressWarnings("unchecked")
-public class Element implements VisitorObject<ClassElement, Element, ParameterElements> {
+public class Element implements VisitorObject<ClassElement, Element, ParameterElements>, IElement {
     protected final @Nullable Object object;
 
     public Element(final @Nullable Object object) {
@@ -33,32 +24,10 @@ public class Element implements VisitorObject<ClassElement, Element, ParameterEl
     }
 
     @Override
-    public <T extends VisitorObject<ClassElement, Element, ParameterElements>> @NotNull T check(@NotNull Class<T> clazz) {
-        if (clazz.isInstance(this)) return (T) this;
-        else throw new HandlerException("%s is not an instance of %s", this, clazz.getCanonicalName());
-    }
-
-    @Override
-    public @NotNull ClassElement checkClass() {
-        if (this instanceof ClassElement) return (ClassElement) this;
-        else throw new HandlerException("%s is not a %s", this, ClassElement.class.getSimpleName());
-    }
-
-    @Override
-    public @NotNull FieldContainer<ClassElement, Element, ParameterElements> getField(@NotNull Field field) throws VisitorObjectException {
-        return null;
-    }
-
-    @Override
-    public @NotNull Element invokeMethod(@NotNull Method method, @NotNull ParameterElements parameters) throws VisitorObjectException {
-        return null;
-    }
-
-    @Override
     public @NotNull Element toPrimitive() {
         if (!isNull() && ReflectionUtils.isPrimitiveOrWrapper(getObjectClass()))
             if (!isPrimitive()) return new Element(ReflectionUtils.getPrimitiveClass(getObjectClass()).cast(this.object));
-        throw new HandlerException("%s is not a wrapper type", this);
+        return IElement.super.toPrimitive();
     }
 
     @Override
@@ -66,7 +35,7 @@ public class Element implements VisitorObject<ClassElement, Element, ParameterEl
         if (isPrimitive()) {
             Object newObject = ReflectionUtils.getWrapperClass(getObjectClass()).cast(this.object);
             return new Element(newObject);
-        } else throw new HandlerException("%s is not a primitive type", this);
+        } else return IElement.super.toWrapper();
     }
 
     @Override
@@ -77,42 +46,6 @@ public class Element implements VisitorObject<ClassElement, Element, ParameterEl
 
     protected @NotNull Class<?> getObjectClass() {
         return (Class<?>) toClass().object;
-    }
-
-    @Override
-    public @NotNull VisitorObjectException fieldNotFound(@NotNull ClassElement classVisitorObject, @NotNull String field) {
-        return new ElementException("Could not find field '%s' in type %s", field, classVisitorObject);
-    }
-
-    @Override
-    public @NotNull VisitorObjectException methodNotFound(@NotNull ClassElement classVisitorObject, @NotNull String method,
-                                                          @NotNull ParameterElements parameters) {
-        return new ElementException("Could not find method %s(%s) in type %s", method, parameters, classVisitorObject);
-    }
-
-    @Override
-    public @NotNull VisitorObjectException typesMismatch(@NotNull ClassElement classVisitorObject, @NotNull Executable method,
-                                                         @NotNull ParameterElements parameters) {
-        return new ElementException("Types mismatch: cannot apply parameters %s to method %s%s in type %s",
-                parameters, method.getName(), Arrays.toString(method.getParameterTypes()), classVisitorObject);
-    }
-
-    @Override
-    public @NotNull RuntimeException noClassType(@NotNull Class<?> type) {
-        return new HandlerException("%s does not have a class", type.getCanonicalName());
-    }
-
-    @Override
-    public @NotNull RuntimeException unsupportedOperation(@NotNull TokenType operator,
-                                                          @NotNull VisitorObject<ClassElement, Element, ParameterElements> left,
-                                                          @NotNull VisitorObject<ClassElement, Element, ParameterElements> right) {
-        return new HandlerException("Operator '%s' cannot be applied to '%s', '%s'", operator, left, right);
-    }
-
-    @Override
-    public @NotNull RuntimeException unsupportedOperation(@NotNull TokenType operator,
-                                                          @NotNull VisitorObject<ClassElement, Element, ParameterElements> operand) {
-        return new HandlerException("Operator '%s' cannot be applied to '%s'", operator, operand);
     }
 
 }
