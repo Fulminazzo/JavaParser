524896a2544695d7ceebb02159be0919ae20409e
Fulminazzo <fulminazzo21@gmail.com>
2025-01-07 20:57:07 +0100

    Added additional tests to obtain encapsulated field

--- src/test/groovy/it/fulminazzo/javaparser/visitors/VisitorLiteralTest.groovy
+++ src/test/groovy/it/fulminazzo/javaparser/visitors/VisitorLiteralTest.groovy
@@ -28,10 +28,11 @@ class VisitorLiteralTest extends Specification {
         element == expected
 
         where:
-        value        | expected
-        'i'          | Element.of(1)
-        'System'     | ClassElement.of(System)
-        'System.out' | Element.of(System.out)
+        value                                       | expected
+        'i'                                         | Element.of(1)
+        'System'                                    | ClassElement.of(System)
+        'System.out'                                | Element.of(System.out)
+        "${SystemWrapper.canonicalName}.system.out" | Element.of(SystemWrapper.system.out)
     }
 
     def 'test visitLiteralImpl of #value should throw exception #exception'() {
@@ -47,6 +48,11 @@ class VisitorLiteralTest extends Specification {
         'System.inexisting'   | new HandlerException(Element.of(null).fieldNotFound(ClassElement.of(System), 'inexisting'))
         'System.not.existing' | new HandlerException(Element.of(null).fieldNotFound(ClassElement.of(System), 'not'))
         'not.existing'        | new Handler(null).cannotResolveSymbol(value)
+    }
+
+    class SystemWrapper {
+        static System system = new System()
+
     }
 
 }
