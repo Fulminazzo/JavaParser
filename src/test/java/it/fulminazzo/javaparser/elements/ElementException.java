69af36b699bb2796e789afa844725960b836027d
Fulminazzo <fulminazzo21@gmail.com>
2025-01-07 14:52:30 +0100

    Created ElementException class

---
+++ src/test/java/it/fulminazzo/javaparser/elements/ElementException.java
@@ -0,0 +1,18 @@
+package it.fulminazzo.javaparser.elements;
+
+import it.fulminazzo.javaparser.visitors.visitorobjects.VisitorObjectException;
+import org.jetbrains.annotations.NotNull;
+
+public class ElementException extends VisitorObjectException {
+
+    /**
+     * Instantiates a new Element exception.
+     *
+     * @param message the message
+     * @param args    the arguments to add in the message format
+     */
+    ElementException(@NotNull String message, Object @NotNull ... args) {
+        super(message, args);
+    }
+
+}
