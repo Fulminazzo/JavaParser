88f47a4005569c0bb2769a2cfbe695865c7134cd
Fulminazzo <fulminazzo21@gmail.com>
2025-01-07 15:18:56 +0100

    Created ParameterElements using IElement

---
+++ src/test/java/it/fulminazzo/javaparser/handler/elements/ParameterElements.java
@@ -0,0 +1,19 @@
+package it.fulminazzo.javaparser.handler.elements;
+
+import it.fulminazzo.javaparser.visitors.visitorobjects.ParameterVisitorObjects;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class ParameterElements extends ParameterVisitorObjects<ClassElement, Element, ParameterElements> implements IElement {
+
+    /**
+     * Instantiates a new Parameter elements.
+     *
+     * @param parameters the parameters
+     */
+    public ParameterElements(@NotNull List<Element> parameters) {
+        super(parameters);
+    }
+
+}
