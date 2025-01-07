9d61b03628a86d69e5ce4c76866cfb4023b93356
Fulminazzo <fulminazzo21@gmail.com>
2025-01-07 01:39:12 +0100

    Added tests for ArrayIndex#getLiteral

---
+++ src/test/groovy/it/fulminazzo/javaparser/parser/node/operators/binary/BinaryOperationTest.groovy
@@ -0,0 +1,25 @@
+package it.fulminazzo.javaparser.parser.node.operators.binary
+
+import it.fulminazzo.javaparser.parser.node.literals.Literal
+import it.fulminazzo.javaparser.parser.node.values.NumberValueLiteral
+import spock.lang.Specification
+
+class BinaryOperationTest extends Specification {
+
+    def 'test array index literal'() {
+        given:
+        def array = Literal.of('arr')
+        def index = new NumberValueLiteral('1')
+
+        and:
+        def arrayIndex = new ArrayIndex(array, index)
+        def expected = "${array}[${index}]"
+
+        when:
+        def literal = arrayIndex.getLiteral()
+
+        then:
+        literal == expected
+    }
+
+}
