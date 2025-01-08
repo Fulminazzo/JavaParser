9321ba48a5c98229c405723f3bb3141e878bd0a8
Fulminazzo <fulminazzo21@gmail.com>
2025-01-08 02:19:20 +0100

    Added test suite for TypeVariableContainer

---
+++ src/test/groovy/it/fulminazzo/javaparser/typechecker/types/variables/TypeVariableContainerTest.groovy
@@ -0,0 +1,46 @@
+package it.fulminazzo.javaparser.typechecker.types.variables
+
+
+import it.fulminazzo.javaparser.typechecker.types.*
+import spock.lang.Specification
+
+import java.lang.reflect.Field
+import java.lang.reflect.Method
+
+class TypeVariableContainerTest extends Specification {
+
+    def 'test container.#method.name(#method.parameterTypes) should call variable.#method.name(#method.parameterTypes)'() {
+        given:
+        def methodName = method.name
+
+        and:
+        def variable = Mock(Type)
+        def container = Mock(TypeVariableContainer)
+        container."${methodName}"(_) >> { callRealMethod() }
+        container.getVariable() >> variable
+
+        and:
+        def parameters = method.parameterTypes.findAll {
+            switch (it) {
+                case Type[] -> new Type[]{PrimitiveType.INT}
+                case ClassType -> PrimitiveClassType.INT
+                case Method -> TestClass.getMethod('publicMethod')
+                case Field -> TestClass.getField('publicField')
+                default -> throw new IllegalArgumentException(it.canonicalName)
+            }
+        }
+
+        when:
+        if (parameters.size() == 0) container."${methodName}"()
+        else container."${methodName}"(*parameters)
+
+        then:
+        if (parameters.size() == 0) 1 * variable."${methodName}"()
+        else 1 * variable."${methodName}"(*parameters)
+
+        where:
+        method << TypeVariableContainer.methods
+                .findAll { it.declaringClass == TypeVariableContainer }
+    }
+
+}
