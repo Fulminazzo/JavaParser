b2d66d656c22b630ac0a582496817cf9d76a8fe6
Fulminazzo <fulminazzo21@gmail.com>
2025-01-08 15:53:40 +0100

    Removed useless methods overrides

--- src/main/java/it/fulminazzo/javaparser/executor/values/variables/ArrayValueVariableContainer.java
+++ src/main/java/it/fulminazzo/javaparser/executor/values/variables/ArrayValueVariableContainer.java
@@ -3,13 +3,9 @@ package it.fulminazzo.javaparser.executor.values.variables;
 import it.fulminazzo.javaparser.executor.values.ClassValue;
 import it.fulminazzo.javaparser.executor.values.ParameterValues;
 import it.fulminazzo.javaparser.executor.values.Value;
-import it.fulminazzo.javaparser.executor.values.ValueException;
 import it.fulminazzo.javaparser.executor.values.arrays.ArrayValue;
 import it.fulminazzo.javaparser.visitors.visitorobjects.variables.VariableContainer;
 import org.jetbrains.annotations.NotNull;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 
 /**
  * Represents the access to an array element with its index.
@@ -43,58 +39,8 @@ public final class ArrayValueVariableContainer<V>
     }
 
     @Override
-    public V getValue() {
-        return ValueVariableContainer.super.getValue();
-    }
-
-    @Override
-    public boolean isCharacter() {
-        return ValueVariableContainer.super.isCharacter();
-    }
-
-    @Override
-    public boolean isInteger() {
-        return ValueVariableContainer.super.isInteger();
-    }
-
-    @Override
-    public boolean isLong() {
-        return ValueVariableContainer.super.isLong();
-    }
-
-    @Override
-    public boolean isFloat() {
-        return ValueVariableContainer.super.isFloat();
-    }
-
-    @Override
-    public boolean isDouble() {
-        return ValueVariableContainer.super.isDouble();
-    }
-
-    @Override
-    public boolean isBoolean() {
-        return ValueVariableContainer.super.isBoolean();
-    }
-
-    @Override
-    public boolean isString() {
-        return ValueVariableContainer.super.isString();
-    }
-
-    @Override
     public @NotNull ClassValue<V> checkClass() {
         return ValueVariableContainer.super.checkClass();
-    }
-
-    @Override
-    public @NotNull ValueFieldContainer<V> getField(@NotNull Field field) {
-        return ValueVariableContainer.super.getField(field);
-    }
-
-    @Override
-    public @NotNull Value<?> invokeMethod(@NotNull Method method, @NotNull ParameterValues parameters) throws ValueException {
-        return ValueVariableContainer.super.invokeMethod(method, parameters);
     }
 
 }
