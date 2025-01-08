830faacc8b355ccd3f8ec74871aeaf40e526faa5
Fulminazzo <fulminazzo21@gmail.com>
2025-01-08 19:53:29 +0100

    Added tests for invalid parseVariables function

--- src/test/groovy/it/fulminazzo/javaparser/MojitoTest.groovy
+++ src/test/groovy/it/fulminazzo/javaparser/MojitoTest.groovy
@@ -1,7 +1,25 @@
 package it.fulminazzo.javaparser
 
+import it.fulminazzo.javaparser.tokenizer.TokenType
 import spock.lang.Specification
 
 class MojitoTest extends Specification {
+
+    def 'test parseVariables of #arguments should throw exception with message: #expected'() {
+        when:
+        Mojito.parseVariables(arguments.toArray(new String[arguments.size()]), 1)
+
+        then:
+        def e = thrown(RunnerException)
+        e.message == expected
+
+        where:
+        arguments                               | expected
+        ['code', 'invalid_variable']            | 'Error for variable \'invalid_variable\' at index 1: Expected \'key:value\' pair'
+        ['code', '1:true', 'invalid key:value'] | 'Error for variable \'invalid key:value\' at index 2: ' +
+                "Invalid value invalid key:value for token${TokenType.STRING_VALUE}(${TokenType.STRING_VALUE.regex()})"
+        ['code', '1:true', '2:false', 'key:;']  | 'Error for variable \'invalid_variable\' at index 3: ' +
+                'No value was returned from key:;'
+    }
 
 }
