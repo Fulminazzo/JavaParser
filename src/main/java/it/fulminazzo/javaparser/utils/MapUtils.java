91ee5f535e5335353623817392efc5f773f08c9d
Fulminazzo <fulminazzo21@gmail.com>
2025-01-08 15:42:26 +0100

    Created MapUtils class

---
+++ src/main/java/it/fulminazzo/javaparser/utils/MapUtils.java
@@ -0,0 +1,12 @@
+package it.fulminazzo.javaparser.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * A collection of utility methods to work with maps.
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class MapUtils {
+
+}
