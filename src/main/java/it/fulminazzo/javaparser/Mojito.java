8a6e193fe44786173b4a1276567920b451ec7a8d
Fulminazzo <fulminazzo21@gmail.com>
2025-01-08 19:31:49 +0100

    Added main function with arguments logic

--- src/main/java/it/fulminazzo/javaparser/Mojito.java
+++ src/main/java/it/fulminazzo/javaparser/Mojito.java
@@ -1,11 +1,30 @@
 package it.fulminazzo.javaparser;
 
+import it.fulminazzo.javaparser.parser.ParserException;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
 
 /**
  * Starting point of the program.
  */
 public final class Mojito {
+
+    public static void main(final String @NotNull [] args) {
+        String code = args.length == 0 ? "-h" : args[0];
+        if (code.equals("-h") || code.equals("--help")) {
+            System.out.println("Usage:");
+            System.out.println("java -jar mojito.jar <filename>");
+            System.out.println("java -jar mojito.jar \"code to run\"");
+        } else {
+            Runner runner = newRunner();
+            try {
+                runner.run(code);
+            } catch (ParserException e) {
+                runner.run(new File(code));
+            }
+        }
+    }
 
     /**
      * Creates a new {@link Runner} with {@link Object} as executor.
