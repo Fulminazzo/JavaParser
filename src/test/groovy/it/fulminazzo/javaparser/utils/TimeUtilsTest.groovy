2003ebee250dc5ae2ef2bc5a075b1b2e303204a2
Fulminazzo <fulminazzo21@gmail.com>
2025-01-08 20:38:25 +0100

    Added test suite for formatTime function

---
+++ src/test/groovy/it/fulminazzo/javaparser/utils/TimeUtilsTest.groovy
@@ -0,0 +1,24 @@
+package it.fulminazzo.javaparser.utils
+
+import spock.lang.Specification
+
+class TimeUtilsTest extends Specification {
+
+    def 'test #time formatTime should return #expected'() {
+        when:
+        def actual = TimeUtils.formatTime(time)
+
+        then:
+        actual == expected
+
+        where:
+        time    | expected
+        3661000 | '1 hours, 1 minutes, 1 seconds'
+        3660000 | '1 hours, 1 minutes'
+        3601000 | '1 hours, 1 seconds'
+        60000   | '1 hours, 1 minutes'
+        1000    | '1 hours, 1 seconds'
+        120     | '0.12 seconds'
+    }
+
+}
