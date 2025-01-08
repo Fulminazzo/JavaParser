578501702def0711cc55ae23a66a57b00c256b0a
Fulminazzo <fulminazzo21@gmail.com>
2025-01-08 20:58:09 +0100

    Totally reworked TimeUtils#formatTime function

--- src/main/java/it/fulminazzo/javaparser/utils/TimeUtils.java
+++ src/main/java/it/fulminazzo/javaparser/utils/TimeUtils.java
@@ -4,30 +4,45 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * A collection of utilities to work with time.
+ */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class TimeUtils {
 
+    /**
+     * Formats the given time in a
+     * <i>&lt;hours&gt; hours, &lt;minutes&gt; minutes, &lt;seconds&gt;.&lt;milliseconds&gt; seconds</i>
+     * format.
+     * If one value is equal to 0, it will not be displayed.
+     *
+     * @param timeInMillis the time in millis
+     * @return the formatted string
+     */
     public static @NotNull String formatTime(long timeInMillis) {
-        double seconds = (double) timeInMillis / 1000;
-        long minutes = (long) (seconds / 60);
-        if (minutes > 0) seconds %= 60;
-        long hours = minutes / 60;
-        if (hours > 0) minutes %= 60;
+        String output = "";
 
-        String format = "";
-        if (hours > 0) format = hours + " hours";
+        long hours = timeInMillis / 3600000;
+        timeInMillis %= 3600000;
+        if (hours > 0) output += hours + " hours";
+
+        long minutes = timeInMillis / 60000;
+        timeInMillis %= 60000;
         if (minutes > 0) {
-            if (!format.isEmpty()) format += ", ";
-            format += minutes + " minutes";
-        }
-        if (seconds > 0 || format.isEmpty()) {
-            if (!format.isEmpty()) format += ", ";
-            if (seconds == Math.round(seconds)) format += (long) seconds;
-            else format += seconds;
-            format += " seconds";
+            if (!output.isEmpty()) output += ", ";
+            output += minutes + " minutes";
         }
 
-        return format;
+        double seconds = (double) timeInMillis / 1000;
+        if (seconds > 0 || output.isEmpty()) {
+            if (!output.isEmpty()) output += ", ";
+            long nonDecimal = (long) seconds;
+            if (seconds - nonDecimal == 0) output += nonDecimal;
+            else output += seconds;
+            output += " seconds";
+        }
+
+        return output;
     }
 
 }
