2d7d1e71bed1fc8ebae412bb8e50395771f3f453
Fulminazzo <fulminazzo21@gmail.com>
2025-01-08 23:46:28 +0100

    !release First release of the project.

--- src/main/java/it/fulminazzo/mojito/parser/node/types/EmptyLiteral.java
+++
@@ -1,9 +0,0 @@
-package it.fulminazzo.mojito.parser.node.types;
-
-import it.fulminazzo.mojito.parser.node.Node;
-
-/**
- * Represents an empty literal.
- */
-public class EmptyLiteral extends Node {
-}
