d40ff5ea4d216cc692a8614e2b8369b0690aab4e
Fulminazzo <fulminazzo21@gmail.com>
2025-01-08 15:22:51 +0100

    Refactored Javadoc

--- src/main/java/it/fulminazzo/javaparser/executor/values/TupleValue.java
+++ src/main/java/it/fulminazzo/javaparser/executor/values/TupleValue.java
@@ -8,16 +8,16 @@ import org.jetbrains.annotations.NotNull;
  * An instance of {@link Tuple} that implements {@link Value}.
  * For internal use only.
  *
- * @param <F> the type parameter
- * @param <S> the type parameter
+ * @param <F> the first type
+ * @param <S> the second type
  */
 public class TupleValue<F, S> extends Tuple<F, S> implements Value<S> {
 
     /**
      * Instantiates a new Tuple value.
      *
-     * @param first  the first
-     * @param second the second
+     * @param first  the first object
+     * @param second the second object
      */
     public TupleValue(@NotNull F first, @NotNull S second) {
         super(first, second);
