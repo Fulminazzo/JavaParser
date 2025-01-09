64f076033d421204d4558dd0dc0c7d94422406ff
Fulminazzo <fulminazzo21@gmail.com>
2025-01-09 13:28:53 +0100

    Refactored NodeException with FormatException

--- src/main/java/it/fulminazzo/mojito/parser/node/NodeException.java
+++ src/main/java/it/fulminazzo/mojito/parser/node/NodeException.java
@@ -1,19 +1,21 @@
 package it.fulminazzo.mojito.parser.node;
 
+import it.fulminazzo.mojito.exceptions.FormatException;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * An exception thrown by {@link TokenizedNode} when reading invalid value.
  */
-public final class NodeException extends Exception {
+public final class NodeException extends FormatException {
 
     /**
      * Instantiates a new Node exception.
      *
      * @param message the message
+     * @param args    the arguments to add in the message format
      */
-    public NodeException(final @NotNull String message) {
-        super(message);
+    public NodeException(final @NotNull String message, final Object @NotNull ... args) {
+        super(String.format(message, args));
     }
 
 }
